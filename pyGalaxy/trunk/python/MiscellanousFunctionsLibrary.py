83fb0ccc71f617160b0e4e685e8dd31747654b79
Johan Comparat <johan.comparat@gmail.com>
2015-10-20 18:06:50 +0200

    s

--- pyGalaxy/trunk/python/MiscellanousFunctionsLibrary.py
+++ pyGalaxy/trunk/python/MiscellanousFunctionsLibrary.py
@@ -3,6 +3,7 @@ This script contains miscellaneous functions.
 """
 
 from scipy.interpolate import interp1d
+import numpy as n
 
 kla=lambda ll :2.659 *(-2.156+1.509/ll-0.198/ll**2+0.011/ll**3 ) + 4.05
 klb=lambda ll :2.659 *(-1.857+1.040/ll)+4.05
