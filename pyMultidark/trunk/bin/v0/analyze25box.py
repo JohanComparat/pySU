ea4413fb612e1ed4f2353b3583a8ab84a6b79106
Johan Comparat <johan.comparat@gmail.com>
2015-11-16 15:11:52 +0100

    new

--- pyMultidark/trunk/bin/v0/analyze25box.py
+++ pyMultidark/trunk/bin/v0/analyze25box.py
@@ -1,10 +1,10 @@
 from MultiDark import *
-box = MultiDarkSimulation(Lbox=2500.0 * uu.Mpc,wdir="/data2/DATA/eBOSS/Multidark-lightcones", boxDir = "MD_2.5Gpc",snl =  n.array(glob.glob(join("/data2/DATA/eBOSS/Multidark-lightcones" , "MD_2.5Gpc" , "hlist_?.?????.list"))) ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc),Melement = 23593750000.0)
+box = MultiDarkSimulation(Lbox=2500.0 * uu.Mpc,wdir="/data2/DATA/eBOSS/Multidark-lightcones", boxDir = "MD_2.5Gpc",snl =  n.array(glob.glob(join("/data2/DATA/eBOSS/Multidark-lightcones" , "MD_2.5Gpc", "snapshots" , "hlist_?.?????.list"))) ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc),Melement = 23593750000.0)
 
-for ii in n.arange(len(box.snl))[40::2]:
+for ii in n.arange(len(box.snl)):
 	box.computeSingleDistributionFunction(ii,'Vpeak', 10**n.arange(0,3.5,0.01))
 
-for ii in n.arange(len(box.snl))[40::2]:
+for ii in n.arange(len(box.snl)):
 	box.combinesSingleDistributionFunction(ii,'Vpeak', 10**n.arange(0,3.5,0.01),type = "Central")
 	box.combinesSingleDistributionFunction(ii,'Vpeak', 10**n.arange(0,3.5,0.01),type = "Satellite")
 
