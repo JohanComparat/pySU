d3c78ff5463cd7f424eb447cb8ae739511750a97
Johan Comparat <jcomparat@taurus.ft.uam.es>
2015-11-23 18:07:06 +0100

    vM

--- pyMultidark/trunk/bin/analyze25box-vpeak.py
+++ pyMultidark/trunk/bin/analyze25box-vpeak.py
@@ -1,7 +1,7 @@
 from MultiDark import *
 box = MultiDarkSimulation(Lbox=2500.0 * uu.Mpc,wdir="/data2/DATA/eBOSS/Multidark-lightcones", boxDir = "MD_2.5Gpc",snl =  n.array(glob.glob(join("/data2/DATA/eBOSS/Multidark-lightcones" , "MD_2.5Gpc", "snapshots" , "hlist_?.?????.list"))) ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc),Melement = 23593750000.0)
 
-for ii in n.arange(len(box.snl)):
+for ii in n.arange(len(box.snl))[45:]:
 	box.computeSingleDistributionFunction(ii,'Vpeak', n.arange(0,3.5,0.01))
 	box.combinesSingleDistributionFunction(ii,'Vpeak', n.arange(0,3.5,0.01),type = "Central")
 	box.combinesSingleDistributionFunction(ii,'Vpeak', n.arange(0,3.5,0.01),type = "Satellite")
