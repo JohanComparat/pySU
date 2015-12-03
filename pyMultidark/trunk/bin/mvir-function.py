1d1f436a29de75d1c786bc9a550ccbd6db2f2f0e
Johan Comparat <johan.comparat@gmail.com>
2015-12-03 19:47:42 +0100

    md vmax

--- pyMultidark/trunk/bin/mvir-function.py
+++ pyMultidark/trunk/bin/mvir-function.py
@@ -1,4 +1,16 @@
 """
+b0,b1,val = n.loadtxt("/Volumes/data/BigMD/Multidark-lightcones/MD_4Gpc/properties/vmax-mvir/hist-Central-mvir-1.00000.dat",unpack=True)
+n.sum(10**((b0+b1)/2.)*val)/(9.6 * 10**(10.))/(4096**3)
+
+b0,b1,val = n.loadtxt("/Volumes/data/BigMD/Multidark-lightcones/MD_2.5Gpc/properties/vmax-mvir/hist-Central-mvir-1.00000.dat",unpack=True)
+n.sum(10**((b0+b1)/2.)*val)/(2.4 * 10**10.)/(3840**3)
+
+b0,b1,val = n.loadtxt("/Volumes/data/BigMD/Multidark-lightcones/MD_1Gpc_new_rockS/properties/vmax-mvir/hist-Central-mvir-1.00000.dat",unpack=True)
+n.sum(10**((b0+b1)/2.)*val)/(1.51 * 10**9.)/(3840**3)
+
+b0,b1,val = n.loadtxt("/Volumes/data/BigMD/Multidark-lightcones/MD_0.4Gpc/properties/vmax-mvir/hist-Central-mvir-1.00000.dat",unpack=True)
+n.sum(10**((b0+b1)/2.)*val)/(9.63 * 10**7.)/(3840**3)
+
 
 ls Multidark-lightcones/MD_*/properties/vmax-mvir/hist-Central*1.0*.dat
 ls Multidark-lightcones/MD_*/properties/vmax-mvir/hist-Central*0.9*.dat
@@ -10,6 +22,15 @@ ls Multidark-lightcones/MD_*/properties/vmax-mvir/hist-Central*0.4*.dat
 ls Multidark-lightcones/MD_*/properties/vmax-mvir/hist-Central*0.3*.dat
 ls Multidark-lightcones/MD_*/properties/vmax-mvir/hist-Central*0.2*.dat
 """
+import astropy.units as uu
+import astropy.cosmology as co
+aa = co.Planck13
+#aah = co.FlatLambdaCDM(H0=100.0 *uu.km / (uu.Mpc *uu.s), Om0=0.307, Tcmb0=2.725 *uu.K, Neff=3.05, m_nu=[ 0.  ,  0. ,   0.06]*uu.eV, Ob0=0.0483)
+#rhom = aa.critical_density0.to(uu.solMass*uu.Mpc**-3).value
+#aa.critical_density0.to(uu.solMass*uu.Mpc**-3).value
+#aah.critical_density0.to(uu.solMass*uu.Mpc**-3).value
+
+from mpl_toolkits.mplot3d import Axes3D
 from scipy.interpolate import interp1d
 import numpy as n
 import matplotlib
@@ -26,11 +47,23 @@ dir = "/Volumes/data/BigMD/mvirFunction/"
 Pdir = "/Volumes/data/BigMD/mvirFunction/plots/"
 
 def getVF(b0, b1, val,volume,label="SMD",completeness = 100, maxV=16,errFactor=1.):
+    """returns the cumulative function n(>X) """
     Nc = n.array([ n.sum(val[ii:]) for ii in range(len(val)) ])
     xData_A = (10**b0+10**b1)/2.
     yData_A = Nc/(volume ) 
     yDataErr_A = errFactor/val**0.5
     sel = (yDataErr_A!=n.inf)&(yData_A>0) &(yData_A * volume >= 1) &(xData_A > completeness)&(xData_A < maxV)
+    xData = xData_A[sel]
+    yData = yData_A[sel]
+    yDataErr = yDataErr_A[sel]*yData_A[sel]
+    return xData,yData,yDataErr,volume
+
+def getDiffVF(b0, b1, val,volume,label="SMD",completeness = 100, maxV=16,errFactor=1.):
+    """returns the differential function dn / dX """
+    xData_A = (10**b0+10**b1)/2.
+    yData_A = val/(volume * (10**b1-10**b0))
+    yDataErr_A = errFactor/val**0.5
+    sel = (yDataErr_A!=n.inf)&(yData_A>0) &(yDataErr_A<1.) & (yDataErr_A>0.) &(xData_A > completeness)&(xData_A < maxV)
     xData = xData_A[sel]
     yData = yData_A[sel]
     yDataErr = yDataErr_A[sel]*yData_A[sel]
@@ -45,11 +78,11 @@ def getVF(b0, b1, val,volume,label="SMD",completeness = 100, maxV=16,errFactor=1
 mf = lambda v, A, v0, alpha, beta : 10**A * (v/10**v0)**beta * n.e**(- (v/10**v0)**alpha )
 
 # for z<3
-limits_04 = [1e7, 1e12]
-limits_10 = [1e11, 1e14]
-limits_25 = [1e12, 1e15]
-limits_40 = [1e13, 1e16]
-zmax = 4.
+limits_04 = [5e10, 1e12]
+limits_10 = [5e11, 1e14]
+limits_25 = [5e12, 1e15]
+limits_40 = [2e13, 1e16]
+zmax = 3.
 
 dir_04 = "/Volumes/data/BigMD/Multidark-lightcones/MD_0.4Gpc/"
 dir_10 = "/Volumes/data/BigMD/Multidark-lightcones/MD_1Gpc_new_rockS/"
@@ -60,10 +93,11 @@ property_dir = "properties/vmax-mvir"
 type = "hist"
 cos = "Central"
 qty = "mvir"
-aa = "1.00000"
+#aa = "1.00000"
 
 fileName = type + "-"+ cos +"-"+ qty +"-*.dat"
 
+############ 0.4 Gpc ##############
 fileList = glob.glob(join(dir_04, property_dir,fileName))
 xData_04,yData_04,yDataErr_04,z_04 = [], [], [], []
 
@@ -85,6 +119,26 @@ yDataErr_04 = n.hstack((yDataErr_04))
 
 n.savetxt(join(dir,"data", type + "-"+ cos +"-"+ qty  +"MD_0.4Gpc"+".dat"),n.transpose([xData_04,z_04,yData_04,yDataErr_04]))
 
+xData_04,yData_04,yDataErr_04,z_04 = [], [], [], []
+for ii in range(len(fileList)):
+    SMDfile = fileList[ii]
+    b0_04, b1_04, val_04 = n.loadtxt(SMDfile,unpack=True)
+    xData_04_ii,yData_04_ii,yDataErr_04_ii,volume_04_ii = getDiffVF(b0_04, b1_04, val_04,400.**3.,completeness = limits_04[0], maxV = limits_04[1])
+    z_04_ii = (1/float(SMDfile.split('-')[-1][:-4])-1)*n.ones_like(xData_04_ii)
+    if z_04_ii[0]<zmax :
+        xData_04.append(xData_04_ii)
+        yData_04.append(yData_04_ii)
+        yDataErr_04.append(yDataErr_04_ii)
+        z_04.append(z_04_ii)
+
+z_04 = n.hstack((z_04))
+xData_04 = n.hstack((xData_04))
+yData_04 = n.hstack((yData_04))
+yDataErr_04 = n.hstack((yDataErr_04))
+
+n.savetxt(join(dir,"data", type + "-"+ cos +"-"+ qty  +"MD_0.4Gpc-diff"+".dat"),n.transpose([xData_04,z_04,yData_04,yDataErr_04]))
+
+############ 1 Gpc ##############
 fileList = glob.glob(join(dir_10, property_dir,fileName))
 xData_10,yData_10,yDataErr_10,z_10 = [], [], [], []
 
@@ -106,6 +160,26 @@ yDataErr_10 = n.hstack((yDataErr_10))
 
 n.savetxt(join(dir,"data", type + "-"+ cos +"-"+ qty  +"MD_1Gpc"+".dat"),n.transpose([xData_10,z_10,yData_10,yDataErr_10]))
 
+xData_10,yData_10,yDataErr_10,z_10 = [], [], [], []
+for ii in range(len(fileList)):
+    SMDfile = fileList[ii]
+    b0_10, b1_10, val_10 = n.loadtxt(SMDfile,unpack=True)
+    xData_10_ii,yData_10_ii,yDataErr_10_ii,volume_10_ii = getDiffVF(b0_10, b1_10, val_10,1000.**3.,completeness = limits_10[0], maxV = limits_10[1])
+    z_10_ii = (1/float(SMDfile.split('-')[-1][:-4])-1)*n.ones_like(xData_10_ii)
+    if z_10_ii[0]<zmax :
+        xData_10.append(xData_10_ii)
+        yData_10.append(yData_10_ii)
+        yDataErr_10.append(yDataErr_10_ii)
+        z_10.append(z_10_ii)
+
+z_10 = n.hstack((z_10))
+xData_10 = n.hstack((xData_10))
+yData_10 = n.hstack((yData_10))
+yDataErr_10 = n.hstack((yDataErr_10))
+
+n.savetxt(join(dir,"data", type + "-"+ cos +"-"+ qty  +"MD_1Gpc-diff"+".dat"),n.transpose([xData_10,z_10,yData_10,yDataErr_10]))
+
+############ 2.5 Gpc ##############
 
 fileList = glob.glob(join(dir_25, property_dir,fileName))
 xData_25,yData_25,yDataErr_25,z_25 = [], [], [], []
@@ -128,6 +202,26 @@ yDataErr_25 = n.hstack((yDataErr_25))
 
 n.savetxt(join(dir,"data", type + "-"+ cos +"-"+ qty  +"MD_2.5Gpc"+".dat"),n.transpose([xData_25,z_25,yData_25,yDataErr_25]))
 
+xData_25,yData_25,yDataErr_25,z_25 = [], [], [], []
+for ii in range(len(fileList)):
+    SMDfile = fileList[ii]
+    b0_25, b1_25, val_25 = n.loadtxt(SMDfile,unpack=True)
+    xData_25_ii,yData_25_ii,yDataErr_25_ii,volume_25_ii = getDiffVF(b0_25, b1_25, val_25,2500.**3.,completeness = limits_25[0], maxV = limits_25[1])
+    z_25_ii = (1/float(SMDfile.split('-')[-1][:-4])-1)*n.ones_like(xData_25_ii)
+    if z_25_ii[0]<zmax :
+        xData_25.append(xData_25_ii)
+        yData_25.append(yData_25_ii)
+        yDataErr_25.append(yDataErr_25_ii)
+        z_25.append(z_25_ii)
+
+z_25 = n.hstack((z_25))
+xData_25 = n.hstack((xData_25))
+yData_25 = n.hstack((yData_25))
+yDataErr_25 = n.hstack((yDataErr_25))
+
+n.savetxt(join(dir,"data", type + "-"+ cos +"-"+ qty  +"MD_2.5Gpc-diff"+".dat"),n.transpose([xData_25,z_25,yData_25,yDataErr_25]))
+
+############ 4 Gpc ##############
 
 fileList = glob.glob(join(dir_40, property_dir,fileName))
 xData_40,yData_40,yDataErr_40,z_40 = [], [], [], []
@@ -149,6 +243,111 @@ yData_40 = n.hstack((yData_40))
 yDataErr_40 = n.hstack((yDataErr_40))
 
 n.savetxt(join(dir, "data", type + "-"+ cos +"-"+ qty  +"MD_4Gpc"+".dat"),n.transpose([xData_40,z_40,yData_40,yDataErr_40]))
+
+
+xData_40,yData_40,yDataErr_40,z_40 = [], [], [], []
+for ii in range(len(fileList)):
+    SMDfile = fileList[ii]
+    b0_40, b1_40, val_40 = n.loadtxt(SMDfile,unpack=True)
+    xData_40_ii,yData_40_ii,yDataErr_40_ii,volume_40_ii = getDiffVF(b0_40, b1_40, val_40,4000.**3.,completeness = limits_40[0], maxV = limits_40[1])
+    z_40_ii = (1/float(SMDfile.split('-')[-1][:-4])-1)*n.ones_like(xData_40_ii)
+    if z_40_ii[0]<zmax :
+        xData_40.append(xData_40_ii)
+        yData_40.append(yData_40_ii)
+        yDataErr_40.append(yDataErr_40_ii)
+        z_40.append(z_40_ii)
+
+z_40 = n.hstack((z_40))
+xData_40 = n.hstack((xData_40))
+yData_40 = n.hstack((yData_40))
+yDataErr_40 = n.hstack((yDataErr_40))
+
+n.savetxt(join(dir, "data", type + "-"+ cos +"-"+ qty  +"MD_4Gpc-diff"+".dat"),n.transpose([xData_40,z_40,yData_40,yDataErr_40]))
+
+
+################################ Plot differential halo mass function  ################################
+
+
+xData_04,z_04,yData_04,yDataErr_04 = n.loadtxt(join(dir,"data", type + "-"+ cos +"-"+ qty  +"MD_0.4Gpc-diff"+".dat"),unpack=True)
+xData_10,z_10,yData_10,yDataErr_10 = n.loadtxt(join(dir,"data", type + "-"+ cos +"-"+ qty  +"MD_1Gpc-diff"+".dat"),unpack=True)
+xData_25,z_25,yData_25,yDataErr_25 = n.loadtxt(join(dir,"data", type + "-"+ cos +"-"+ qty  +"MD_2.5Gpc-diff"+".dat"),unpack=True)
+xData_40,z_40,yData_40,yDataErr_40 = n.loadtxt(join(dir,"data", type + "-"+ cos +"-"+ qty  +"MD_4Gpc-diff"+".dat"),unpack=True)
+
+rhom_04 = n.array([aa.critical_density(zz).to(uu.solMass*uu.Mpc**-3).value/aa.h**2 for zz in z_04])
+rhom_10 = n.array([aa.critical_density(zz).to(uu.solMass*uu.Mpc**-3).value/aa.h**2 for zz in z_10])
+rhom_25 = n.array([aa.critical_density(zz).to(uu.solMass*uu.Mpc**-3).value/aa.h**2 for zz in z_25])
+rhom_40 = n.array([aa.critical_density(zz).to(uu.solMass*uu.Mpc**-3).value/aa.h**2 for zz in z_40])
+
+rhom_04 = n.array([aa.critical_density(zz).to(uu.solMass*uu.Mpc**-3).value for zz in z_04])
+rhom_10 = n.array([aa.critical_density(zz).to(uu.solMass*uu.Mpc**-3).value for zz in z_10])
+rhom_25 = n.array([aa.critical_density(zz).to(uu.solMass*uu.Mpc**-3).value for zz in z_25])
+rhom_40 = n.array([aa.critical_density(zz).to(uu.solMass*uu.Mpc**-3).value for zz in z_40])
+
+dat = n.loadtxt("/Volumes/data/BigMD/mVector_PLANCK_HMD.txt", unpack=True)
+
+p.figure(1,(6,6))
+p.axes([0.17,0.17,0.75,0.75])
+
+p.plot(n.log10(dat[0]),n.log10(dat[0]*dat[0]*dat[5]),'k--',lw=2)
+
+s_04 = (z_04 == 0)
+y_04 = n.log10(yData_04*xData_04**2./rhom_04)
+y_04 = n.log10(yData_04*xData_04*xData_04)
+p.plot(n.log10(xData_04[s_04]), y_04[s_04], marker ='o', mfc='None',mec='r',ls='none', label="SMD", rasterized=True)
+
+s_10 = (z_10 == 0)
+y_10 = n.log10(yData_10*xData_10**2./rhom_10)
+y_10 = n.log10(yData_10*xData_10*xData_10)
+p.plot(n.log10(xData_10[s_10]),y_10[s_10], marker ='v', mfc='None',mec='c',ls='none', label="MDPL", rasterized=True)
+
+s_25 = (z_25 == 0)
+y_25 = n.log10(yData_25*xData_25**2./rhom_25)
+y_25 = n.log10(yData_25*xData_25*xData_25)
+p.plot(n.log10(xData_25[s_25]),y_25[s_25], marker ='s', mfc='None',mec='m',ls='none', label="BigMD", rasterized=True)
+
+s_40 = (z_40 == 0)
+y_40 = n.log10(yData_40*xData_40**2./rhom_40)
+y_40 = n.log10(yData_40*xData_40*xData_40)
+p.plot(n.log10(xData_40[s_40]),y_40[s_40], marker ='p', mfc='None',mec='b',ls='none', label="HMD", rasterized=True)
+
+p.xlabel(r'log$_{10}[M_{vir}/(h^{-1}M_\odot)]$')
+p.ylabel(r'log$_{10}[(M^2/\rho_m) \; dn/dM]')
+p.legend(loc=3)
+p.grid()
+p.savefig(Pdir + "mvir-function2.pdf")
+p.clf()
+
+sys.exit()
+
+fig = p.figure(1,(9,9))
+ax = fig.add_subplot(111, projection='3d')
+
+sc1 = ax.scatter(n.log10(xData_04),z_04,n.log10(yData_04*xData_04**2./rhom_04), s=n.ones_like(z_04)*3, c='r', marker='o',label="SMD", rasterized=True)
+sc1.set_edgecolor('face')
+
+sc1 = ax.scatter(n.log10(xData_10),z_10,n.log10(yData_10*xData_10**2./rhom_10), s=n.ones_like(z_10)*3, c='c', marker='v',label="MDPL", rasterized=True)
+sc1.set_edgecolor('face')
+
+sc1 = ax.scatter(n.log10(xData_25),z_25,n.log10(yData_25*xData_25**2./rhom_25), s=n.ones_like(z_25)*3, c='m', marker='s',label="BigMD", rasterized=True)
+sc1.set_edgecolor('face')
+
+sc1 = ax.scatter(n.log10(xData_40),z_40,n.log10(yData_40*xData_40**2./rhom_40), s=n.ones_like(z_40)*3, c='b', marker='p',label="HMD", rasterized=True)
+sc1.set_edgecolor('face')
+
+ax.legend()
+ax.set_xlabel(r'$log_{10}[M_{vir}/(h^{-1}M_\odot)]$')
+ax.set_ylabel('redshift')
+ax.set_ylim((0,zmax))
+ax.set_zlabel(r'$log_{10}[(M^2/\rho_m) \; dn/dM]$')
+ax.set_zlim((-4,0))
+p.savefig(Pdir + "mvir-function-3d-diff.pdf")
+p.show()
+
+
+
+sys.exit()
+
+################################ Model Fits on the cumulative function ################################
 
 xData_04,z_04,yData_04,yDataErr_04 = n.loadtxt(join(dir,"data", type + "-"+ cos +"-"+ qty  +"MD_0.4Gpc"+".dat"),unpack=True)
 xData_10,z_10,yData_10,yDataErr_10 = n.loadtxt(join(dir,"data", type + "-"+ cos +"-"+ qty  +"MD_1Gpc"+".dat"),unpack=True)
@@ -216,7 +415,6 @@ xData_40,z_40,yData_40,yDataErr_40 = n.loadtxt(join(dir,"data/", type + "-"+ cos
 vmax_mod, z_mod, n_mod = n.loadtxt(join(dir,"data/best_fit.dat"), unpack=True)
 
 #####################
-from mpl_toolkits.mplot3d import Axes3D
 
 fig = p.figure(1,(9,9))
 ax = fig.add_subplot(111, projection='3d')
