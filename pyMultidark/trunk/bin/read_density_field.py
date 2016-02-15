82f94f5d0e4eaf57a90f05838d599c5ac0ebbd65
JohanComparat <johan.comparat@gmail.com>
2016-02-15 13:13:14 +0100

    a

--- pyMultidark/trunk/bin/read_density_field.py
+++ pyMultidark/trunk/bin/read_density_field.py
@@ -1,16 +1,38 @@
-import numpy as np
+import numpy as n
 import os
 from os.path import join
+from astropy.io import fits
 
 dir = join("/data2", "users", "gustavo", "BigMD", "1Gpc_3840_Planck1_New", "DENSFIELDS")
 filename = "dmdens_cic_087.dat"
+
+outDir =  join("/data2", "DATA", "eBOSS", "Multidark-lightcones", "MD_1Gpc_new_rockS", "densityField")
 
 path_to_file = join(dir,filename)
 
 f=open(path_to_file,'r')
  
-data1 =  np.fromfile(f,dtype="float64",count=1 000 000)
-data2 =  np.fromfile(f,dtype="float64",count=1 000 000)
-data3 =  np.fromfile(f,dtype="float64",count=1 000 000)
+ # 64 cubes of 512 on the side ?
+data1 =  n.fromfile(f,dtype="float64",count=134217728) # 512 cube
 
+n.histogram(n.log10(data1))
+
+c0 = fits.Column(name="densityField",format='D', array=data1 )
+c1 = fits.Column(name="ID",format='I', array=n.arange(len(data1)) )
+
+cols = fits.ColDefs([c0, c1])
+
+hdu = fits.BinTableHDU.from_columns(cols)
+hdu.writeto(outputCat)
+
+
+# create fits file
+
+data2 =  np.fromfile(f,dtype="float64",count=1000000)
+data3 =  np.fromfile(f,dtype="float64",count=1000000)
+
+
+data1 =  n.fromfile(f,dtype="float64",count=134217728) # 512 cube
+data1 =  n.fromfile(f,dtype="float64",count=134217728) # 512 cube
+data1 =  n.fromfile(f,dtype="float64",count=134217728) # 512 cube
 
