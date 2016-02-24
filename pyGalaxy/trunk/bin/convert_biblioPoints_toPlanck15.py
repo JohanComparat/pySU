34ba650eb08e38e6cdd5277ece8536cb6abcdaa8
JohanComparat <johan.comparat@gmail.com>
2016-02-24 13:06:56 +0100

    vipers

--- pyGalaxy/trunk/bin/convert_biblioPoints_toPlanck15.py
+++ pyGalaxy/trunk/bin/convert_biblioPoints_toPlanck15.py
@@ -1,36 +1,95 @@
 from os.path import join
 import astropy.cosmology as co
-co.Planck13
-co.Planck15
-co.FlatLambdaCDM(H0=70,Om0=0.3)
-co.WMAP7
+#co.Planck13
+#co.Planck15
+co0307 = co.FlatLambdaCDM(H0=70,Om0=0.3)
+#co.WMAP7
 
 import numpy as n
 
 """
+Conversion of bibliography points to Planck 2015 and to the format of data to input the fitting routine.
+
 Needs to match the format:
 # z_mean z_min z_max L_mean L_min L_max phi_mean phi_min phi_max  Ngalaxy
+
 No logarithmic values
 
 """
 
 bibPtDir = join("..", "biblioPoints")
 dataDir = join("..", "data")
-fV = lambda zMean : co.Planck13.comoving_volume(zMean) / co.Planck15.comoving_volume(zMean)
 
-fL = lambda zMean : co.Planck15.luminosity_distance(zMean) / co.Planck13.luminosity_distance(zMean)
+# 
+# load hetdex data. Ciardullo et al. 2013. 0.3 0.7 => Planck 15
+names=n.array(["Hetdex-points-z015.dat", "Hetdex-points-z026.dat", "Hetdex-points-z038.dat", "Hetdex-points-z050.dat"])
+zmins = n.array([0.0, 0.2, 0.325, 0.45])
+zmaxs = n.array([0.2,  0.325, 0.45, 0.56])
+zmeans = n.array([0.12,  0.26, 0.38, 0.49])
+
+def convertC13_0(name,zmin,zmax,zmean):
+	L,philow,phiup=n.loadtxt(join(bibPtDir,name),unpack=True)
+	fV = lambda zMean : co0307.comoving_volume(zMean) / co.Planck15.comoving_volume(zMean)
+	fL = lambda zMean : co.Planck15.luminosity_distance(zMean) / co0307.luminosity_distance(zMean)
+	z_mean = n.ones_like(philow) * zmean
+	z_min = n.ones_like(philow) * zmin
+	z_max  = n.ones_like(philow) * zmax
+	cL = fL(z_mean)
+	L_mean = 10**L * cL
+	L_min = 10**(L-0.1) * cL
+	L_max = 10**(L+0.1) * cL
+	cV = fV(z_mean)
+	phi_mean = (phiup*0.001+philow*0.001)/2. * cV
+	phi_min = philow*0.001 * cV
+	phi_max = phiup*0.001* cV
+	Ngalaxy = -1.*n.ones_like(philow) 
+	head= "z_mean z_min z_max L_mean L_min L_max phi_mean phi_min phi_max  Ngalaxy"
+	f=open(join(dataDir , "O2_3728", "O2_3728-"+name[:-4]+"-Planck15.txt"),'w')
+	n.savetxt(f, n.transpose([z_mean, z_min, z_max, L_mean, L_min, L_max, phi_mean, phi_min, phi_max, Ngalaxy]), header= head)
+	f.close()
+
+	
+def convertC13(name,zmin,zmax,zmean):
+	L, phi, philow, phiup=n.loadtxt(join(bibPtDir,name),unpack=True)
+	fV = lambda zMean : co0307.comoving_volume(zMean) / co.Planck15.comoving_volume(zMean)
+	fL = lambda zMean : co.Planck15.luminosity_distance(zMean) / co0307.luminosity_distance(zMean)
+	z_mean = n.ones_like(philow) * zmean
+	z_min = n.ones_like(philow) * zmin
+	z_max  = n.ones_like(philow) * zmax
+	cL = fL(z_mean)
+	L_mean = 10**L * cL
+	L_min = 10**(L-0.1) * cL
+	L_max = 10**(L+0.1) * cL
+	cV = fV(z_mean)
+	phi_mean = phi*0.001* cV
+	phi_min = philow*0.001 * cV
+	phi_max = phiup*0.001* cV
+	Ngalaxy = -1.*n.ones_like(philow) 
+	head= "z_mean z_min z_max L_mean L_min L_max phi_mean phi_min phi_max  Ngalaxy"
+	f=open(join(dataDir , "O2_3728", "O2_3728-"+name[:-4]+"-Planck15.txt"),'w')
+	n.savetxt(f, n.transpose([z_mean, z_min, z_max, L_mean, L_min, L_max, phi_mean, phi_min, phi_max, Ngalaxy]), header= head)
+	f.close()
+	
+for ii, name in enumerate(names):
+	print name, ii
+	if ii==0:
+		convertC13_0(name,zmins[ii],zmaxs[ii],zmeans[ii])
+	if ii>0:
+		convertC13(name,zmins[ii],zmaxs[ii],zmeans[ii])
+
 
 # comparat et al. 2015. Planck 13 => Planck 15
 #  Lmin Lmax Lmean phi phiErrTotal jkErrShare poissonErrShare weightErrShare Ngalaxy
-names = n.array([ "comparat2014-LF-0.100-z-0.24.dat", "comparat2014-LF-0.240-z-0.4.dat", "comparat2014-LF-0.500-z-0.695.dat", "comparat2014-LF-0.695-z-0.88.dat", "comparat2014-LF-0.880-z-1.09.dat", "comparat2014-LF-1.090-z-1.34.dat", "comparat2014-LF-1.340-z-1.65.dat"])
 
 def convertC15(name):
+	fV = lambda zMean : co.Planck13.comoving_volume(zMean) / co.Planck15.comoving_volume(zMean)
+	fL = lambda zMean : co.Planck15.luminosity_distance(zMean) / co.Planck13.luminosity_distance(zMean)
 	zmin = float(name[:-4].split('-')[2])
 	zmax = float(name[:-4].split('-')[4])
 	data=n.loadtxt(join(bibPtDir,name),unpack=True)
-	z_mean = n.ones_like(data[0]) * (0.1+0.24)/2.
-	z_min = n.ones_like(data[0]) * 0.1
-	z_max  = n.ones_like(data[0]) * 0.24 
+	z_mean = n.ones_like(data[0]) * (zmin + zmax)/2.
+	z_min = n.ones_like(data[0]) * zmin
+	z_max  = n.ones_like(data[0]) * zmax
 	cL = fL(z_mean)
 	L_mean = 10**data[2] * cL
 	L_min = 10**data[0] * cL
@@ -45,22 +104,15 @@ def convertC15(name):
 	n.savetxt(f, n.transpose([z_mean, z_min, z_max, L_mean, L_min, L_max, phi_mean, phi_min, phi_max, Ngalaxy]), header= head)
 	f.close()
 
-
+names = n.array([ "comparat2014-LF-0.100-z-0.24.dat", "comparat2014-LF-0.240-z-0.4.dat", "comparat2014-LF-0.500-z-0.695.dat", "comparat2014-LF-0.695-z-0.88.dat", "comparat2014-LF-0.880-z-1.09.dat", "comparat2014-LF-1.090-z-1.34.dat", "comparat2014-LF-1.340-z-1.65.dat"])
 for name in names:
 	convertC15(name)
 
 
 
-# load SDSS
+# load SDSS from Gilbank 2010
 sdss01=n.loadtxt("../biblioPoints/Sdss-points-z00.dat",unpack=True)
 
-# load hetdex data.
-hd01=n.loadtxt("../biblioPoints/Hetdex-points-z01.dat",unpack=True)
-hd02=n.loadtxt("../biblioPoints/Hetdex-points-z026.dat",unpack=True)
-hd03=n.loadtxt("../biblioPoints/Hetdex-points-z038.dat",unpack=True)
-hd05=n.loadtxt("../biblioPoints/Hetdex-points-z05.dat",unpack=True)
-hdRes=n.array([hd01,hd02,hd03,hd05])
-zHd=n.array([0.15,0.26,0.38,0.5])
 
 # the DEEP2 points
 dp2=n.loadtxt("../biblioPoints/DEEP2-apj299359t1.txt",unpack=True) # 084 1 12 135
