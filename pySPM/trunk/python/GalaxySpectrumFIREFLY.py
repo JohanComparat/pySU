e1c5346a66a633d18fc016c4b44b1b32d32214d8
JohanComparat <johan.comparat@gmail.com>
2016-03-02 10:56:58 +0100

    spm

---
+++ pySPM/trunk/python/GalaxySpectrumFIREFLY.py
@@ -0,0 +1,167 @@
+"""
+.. moduleauthor:: Johan Comparat <johan.comparat__at__gmail.com>
+
+*General purpose*:
+
+The class GalaxySpectrumFIREFLY is dedicated to handling spectra to be fed to FIREFLY for fitting its stellar population
+
+*Imports*::
+
+	import numpy as np
+	import astropy.io.fits as pyfits
+	import glob
+	from firefly_instrument import *
+	from firefly_dust import *
+	from firefly_fitter import *
+	from firefly_library import *
+
+
+"""
+import numpy as np
+import astropy.io.fits as pyfits
+import glob
+
+from firefly_instrument import *
+from firefly_dust import *
+from firefly_fitter import *
+from firefly_library import *
+
+
+class GalaxySpectrumFIREFLY:
+	"""
+	Loads the environnement to transform observed spectra into the input for FIREFLY. 
+	
+	Currently SDSS spectra, speclite format is handled as well as stacks from the VVDS and the DEEP2 galaxy surveys.
+
+	:param path_to_spectrum: path to the spectrum
+	:param hpf_mode: models the dust attenuation observed in the spectrum using high pass filter.
+	:param milky_way_reddening: True if you want to correct from the Milky way redenning using the Schlegel 98 dust maps.
+	:param stack_type: Optional. If you are fitting stack, choose the origins of the stacks : "DEEP2" or "VVDS"
+	"""
+	def __init__(self,path_to_spectrum, milky_way_reddening=True , hpf_mode = 'on', stack_type = "DEEP2"):
+		self.path_to_spectrum=path_to_spectrum
+		self.milky_way_reddening = milky_way_reddening
+		self.stack_type = stack_type 
+
+	def openObservedSDSSSpectrum(self):
+		"""
+		It reads an SDSS spectrum and provides the input for the firefly fitting routine.
+		:param path_to_spectrum:
+		:param sdss_dir: directory with the observed spectra
+		:param milky_way_reddening: True or False if you want to correct the redenning of the Milky way.
+		:param hpf_mode: 'on' high pass filters the data to correct from dust in the galaxy.
+
+		In this aims, it stores the following data in the object :
+		* hdu list from the spec lite
+		* SED data : wavelength (in angstrom), flux, error on the flux (in 10^{-17} erg/cm2/s/Angstrom, like the SDSS spectra)
+		* Metadata :
+			* ra : in degrees J2000
+			* dec : in degrees J2000
+			* redshift : best fit
+			* vdisp : velocity dispersion in km/s
+			* r_instrument : resolution of the instrument at each wavelength observed
+			* trust_flag : 1 or True if trusted 
+			* bad_flags : ones as long as the wavelength array, filters the pixels with bad data
+			* objid : object id optional : set to 0
+		"""
+		self.hdulist = pyfits.open(self.path_to_spectrum)
+		self.ra = self.hdulist[0].header['RA']
+		self.dec = self.hdulist[0].header['DEC']
+
+		self.wavelength = 10**self.hdulist[1].data['loglam']
+		self.flux = self.hdulist[1].data['flux']
+		self.error = self.hdulist[1].data['ivar']**(-0.5)
+		self.bad_flags = np.ones(len(self.wavelength))
+		bad_data = np.isnan(self.flux) | np.isinf(self.flux) | (self.flux <= 0.0) | np.isnan(self.error) | np.isinf(self.error)
+		# removes the bad data from the spectrum 
+		self.flux[bad_data] 	= 0.0
+		self.error[bad_data] 	= np.max(self.flux) * 99999999999.9
+		self.bad_flags[bad_data] = 0
+
+		self.r_instrument = np.zeros(len(self.wavelength))
+		for wi,w in enumerate(self.wavelength):
+			if w<6000:
+				self.r_instrument[wi] = (2270.0-1560.0)/(6000.0-3700.0)*w + 420.0 
+			else:
+				self.r_instrument[wi] = (2650.0-1850.0)/(9000.0-6000.0)*w + 250.0 
+
+		self.redshift = self.hdulist[2].data['Z'][0]
+		self.vdisp = self.hdulist[2].data['VDISP'][0]
+		self.restframe_wavelength = self.wavelength / (1.0+self.redshift)
+
+		self.trust_flag = 1
+		self.objid = 0
+
+		if self.milky_way_reddening :
+			# gets the amount of MW reddening on the models
+			self.ebv_mw = get_dust_radec(self.ra,self.dec,'ebv')
+		else:
+			self.ebv_mw = 0.0
+
+
+	def openObservedStack(self):
+		"""
+		It reads an Stack spectrum from the LF analysis and provides the input for the firefly fitting routine.
+		:param path_to_spectrum:
+		:param sdss_dir: directory with the observed spectra
+		:param milky_way_reddening: True or False if you want to correct the redenning of the Milky way.
+		:param hpf_mode: 'on' high pass filters the data to correct from dust in the galaxy.
+
+		In this aims, it stores the following data in the object :
+		* hdu list from the spec lite
+		* SED data : wavelength (in angstrom), flux, error on the flux (in 10^{-17} erg/cm2/s/Angstrom, like the SDSS spectra)
+		* Metadata :
+			* ra : in degrees J2000
+			* dec : in degrees J2000
+			* redshift : best fit
+			* vdisp : velocity dispersion in km/s
+			* r_instrument : resolution of the instrument at each wavelength observed
+			* trust_flag : 1 or True if trusted 
+			* bad_flags : ones as long as the wavelength array, filters the pixels with bad data
+			* objid : object id optional : set to 0
+		"""
+		self.hdulist = pyfits.open(self.path_to_spectrum)
+		self.ra = 0. #self.hdulist[0].header['RA']
+		self.dec = 0. #self.hdulist[0].header['DEC']
+		self.redshift = float(self.path_to_spectrum.split('-')[-1].split('_')[0][1:])
+		self.restframe_wavelength = self.hdulist[1].data['wavelength']
+
+		self.wavelength = self.restframe_wavelength * (1. + self.redshift)
+		self.flux = self.hdulist[1].data['meanWeightedStack'] * 1e17
+		self.error = self.hdulist[1].data['jackknifStackErrors'] * 1e17
+		self.bad_flags = np.ones(len(self.restframe_wavelength))
+		Nstacked = float(self.path_to_spectrum.split('-')[-1].split('_')[3])
+
+		N_angstrom_masked = 20
+		lines_mask = ((self.restframe_wavelength > 3738 - N_angstrom_masked) & (self.restframe_wavelength < 3738 + N_angstrom_masked)) | ((self.restframe_wavelength > 5007 - N_angstrom_masked) & (self.restframe_wavelength < 5007 + N_angstrom_masked)) | ((self.restframe_wavelength > 4861 - N_angstrom_masked) & (self.restframe_wavelength < 4861 + N_angstrom_masked)) | ((self.restframe_wavelength > 6564 - N_angstrom_masked) & (self.restframe_wavelength < 6564 + N_angstrom_masked)) | ( self.hdulist[1].data['NspectraPerPixel'] < Nstacked * 0.8 ) | (self.flux==-9999.99)
+
+		self.restframe_wavelength = self.restframe_wavelength[(lines_mask==False)] 
+		self.wavelength = self.wavelength[(lines_mask==False)] 
+		self.flux = self.flux[(lines_mask==False)] 
+		self.error = self.error[(lines_mask==False)] 
+		self.bad_flags = self.bad_flags[(lines_mask==False)] 
+
+		bad_data 	= np.isnan(self.flux) | np.isinf(self.flux) | (self.flux <= 0.0) | np.isnan(self.error) | np.isinf(self.error) 
+		# removes the bad data from the spectrum 
+		self.flux[bad_data] 	= 0.0
+		self.error[bad_data] 	= np.max(self.flux) * 99999999999.9
+		self.bad_flags[bad_data] = 0
+
+		if self.stack_type == "DEEP":
+			self.r_instrument = np.ones_like(np.arange(len(self.restframe_wavelength))) * 6000.*2
+		if self.stack_type == "VVDS":
+			self.r_instrument = np.ones_like(np.arange(len(self.restframe_wavelength))) * 250.*2
+
+		self.vdisp = 70. # km/s
+
+		self.trust_flag = 1
+		self.objid = 0
+
+		if self.milky_way_reddening :
+			# gets the amount of MW reddening on the models
+			self.ebv_mw = get_dust_radec(self.ra,self.dec,'ebv')
+		else:
+			self.ebv_mw = 0.0
+
+		print"there are", len(self.wavelength),"data points at redshift",self.redshift," between", np.min(self.wavelength[bad_data==False]), np.max(self.wavelength[bad_data==False]), "Angstrom.", np.min(self.restframe_wavelength[bad_data==False]), np.max(self.restframe_wavelength[bad_data==False]), "Angstrom in the rest frame."
+
---
+++ pySPM/trunk/python/StellarPopulationModel.py
@@ -0,0 +1,412 @@
+"""
+.. moduleauthor:: Johan Comparat <johan.comparat__at__gmail.com>
+
+General purpose:
+................
+
+The class StellarPopulationModel is a wrapper dedicated to handling the fit of stellar population models on observed spectra. 
+It gathers all inputs : from the model and from the data.
+
+*Imports*::
+
+	import numpy as np
+	import astropy.io.fits as pyfits
+	import astropy.units as u
+	import glob
+	import pandas as pd
+	import os
+	from firefly_instrument import *
+	from firefly_dust import *
+	from firefly_fitter import *
+	from firefly_library import *
+
+"""
+import numpy as np
+import astropy.io.fits as pyfits
+import astropy.units as u
+import glob
+import pandas as pd
+import os
+from os.path import join
+#from scipy.stats import sigmaclip
+
+from firefly_instrument import *
+from firefly_dust import *
+from firefly_fitter import *
+from firefly_library import *
+
+
+class StellarPopulationModel:
+	"""
+	:param specObs: specObs observed spectrum object initiated with the  GalaxySpectrumFIREFLY class.
+	:param models: choose between 'm11', 'bc03' or 'm09'. 
+
+		* m11 corresponds to all the models compared in `Maraston and Stromback 2011  <http://adsabs.harvard.edu/abs/2011MNRAS.418.2785M>`_. 
+		* m09 to `Maraston et al. 2009 <http://adsabs.harvard.edu/abs/2009A%26A...493..425M>`_. 
+		* bc03 to the `Bruzual and Charlot 2003 models <http://adsabs.harvard.edu/abs/2003MNRAS.344.1000B>`_.
+
+	:param model_libs: only necessary if using m11. 
+	Choose between `MILES <http://adsabs.harvard.edu/abs/2011A%26A...532A..95F>`_, MILES revisednearIRslope, MILES UVextended, `STELIB <http://adsabs.harvard.edu/abs/2003A%26A...402..433L>`_, `ELODIE <http://adsabs.harvard.edu/abs/2007astro.ph..3658P>`_, `MARCS <http://adsabs.harvard.edu/abs/2008A%26A...486..951G>`_. 
+
+		* MILES, MILES revisednearIRslope, MILES UVextended, STELIB, ELODIE are empirical libraries. 
+		* MARCS is a theoretical library.
+
+	:param imfs: choose the `initial mass function <https://en.wikipedia.org/wiki/Initial_mass_function>`_:
+
+		* 'ss' for `Salpeter <http://adsabs.harvard.edu/abs/1955ApJ...121..161S>`_or 
+		* 'kr' for `Kroupa <http://adsabs.harvard.edu/cgi-bin/bib_query?arXiv:1112.3340>`_ or 
+		* 'cha' for `Chabrier <http://adsabs.harvard.edu/abs/2003PASP..115..763C>`_.
+
+	 Notes
+	 -----
+
+	.. note::
+		*This is how it proceeds :*
+		 #. reads the parameter file by using parameters_obtain(parameters.py)
+		 #. It opens the data file, model files, then it matches their resolutions by downgrading the models to instrumental and velocity dispersion resolution
+		 #. Determines dust attenuation curve to be applied to the models. Two options : through HPF fitting (3.1.) or through filtered values to determing SP properties (3.2.).
+		 #. It fits the models to the data
+		 #. Gets mass-weighted SSP contributions using saved M/L ratio.
+		 #. Convert chis into probabilities and calculates all average properties and errors (assuming the number of degrees of freedom is the number of wavelength points)
+		 #. Optionally produces a plot
+		 #. Finally, it writes the output files
+
+	"""
+	def __init__(self, specObs, outputFile, cosmo, models = 'm11', model_libs = ['MILES_UVextended'], imfs = ['ss','kr'], hpf_mode = 'on', age_limits = [6,10.1], downgrade_models = True, dust_law = 'calzetti', max_ebv = 1.0, num_dust_vals = 100, dust_smoothing_length = 200, max_iterations = 10, fit_per_iteration_cap = 1000, pdf_sampling = 300, data_wave_medium = 'vacuum', Z_limits = [-0.1,0.1], wave_limits = [0,99999990], suffix = "-fireflyFits.fits",use_downgraded_models = True):
+		self.cosmo = cosmo
+		self.specObs = specObs
+		self.outputFile = outputFile
+		#################### STARTS HERE #################### 
+		# sets the models
+		self.models = models # m11/bc03 / m09
+		self.model_libs = model_libs
+		self.suffix = suffix
+		self.deltal_libs = []
+		self.vdisp_round = int(round(self.specObs.vdisp/5.0)*5.0) # rounding vDisp for the models
+		self.use_downgraded_models = use_downgraded_models
+		if self.models == 'm11':
+			for m in self.model_libs:
+				if m == 'MILES' or m == 'MILES_revisednearIRslope' or m == 'MILES_UVextended':
+					self.deltal_libs.append(2.55)
+				elif m == 'STELIB':
+					self.deltal_libs.append(3.40)
+				elif m == 'ELODIE':
+					self.deltal_libs.append(0.55)
+				elif m == 'MARCS':
+					self.deltal_libs.append(0.1)
+
+		elif self.models=='bc03':
+			self.model_libs = ['STELIB_BC03']
+			imfs        = ['cha']
+			self.deltal_libs = [3.00]
+
+		elif self.models == 'm09':
+			self.model_libs = ['M09']
+			if downgrade_models:
+				self.deltal_libs = [0.4]
+			else:
+				self.deltal_libs = [3.6]
+		# sets the Initial mass function
+		self.imfs = imfs
+		self.hpf_mode = hpf_mode
+		self.age_limits = age_limits
+
+		self.downgrade_models = downgrade_models
+		self.dust_law = dust_law
+		self.max_ebv = max_ebv
+		self.num_dust_vals = num_dust_vals
+		self.dust_smoothing_length = dust_smoothing_length
+		# Specific fitting options
+		self.max_iterations = max_iterations
+		self.fit_per_iteration_cap = fit_per_iteration_cap
+		# Sampling size when calculating the maximum pdf (100=recommended)
+		self.pdf_sampling = pdf_sampling
+		# Default is air, unless manga is used
+		self.data_wave_medium = data_wave_medium
+		self.Z_limits = Z_limits
+		self.wave_limits = wave_limits
+
+	def get_model(self, model_used, imf_used, deltal, vdisp, wave_instrument, r_instrument, ebv_mw):
+
+		"""
+		Retrieves all relevant model files, in their downgraded format.
+		If they aren't downgraded to the correct resolution / velocity dispersion,
+		takes the base models in their native form and converts to downgraded files.
+
+		:param model_used: list of models to be used, for example ['m11', 'bc03', 'm09'].
+		:param imf_used: list of imf to be used, for example ['ss', 'cha'].
+		:param deltal: delta lambda in the models.
+		:param vdisp: velocity dispersion observed in the galaxy.
+		:param wave_instrument: wavelength array from the observations
+		:param r_instrument: resolution array from the observations
+		:param  ebv_mw: E(B-V) from the dust maps for the galaxy.
+		"""
+		# first the m11 case
+		if self.models == 'm11':
+			first_file  = True 
+			model_files = []
+			if self.use_downgraded_models :
+				if model_used == 'MILES_UVextended' or model_used == 'MILES_revisedIRslope':
+					model_path 		= join(os.environ['STELLARPOPMODELS_DIR'],'data','SSP_M11_MILES_downgraded','ssp_M11_' + model_used+ '.' + imf_used)				
+				else:
+					model_path 		= join(os.environ['STELLARPOPMODELS_DIR'],'data','SSP_M11_'+ model_used + '_downgraded', 'ssp_M11_' +model_used +'.' + imf_used)
+			else:
+				if model_used == 'MILES_UVextended' or model_used == 'MILES_revisedIRslope':
+					model_path 		= join(os.environ['STELLARPOPMODELS_DIR'],'data','SSP_M11_MILES', 'ssp_M11_'+model_used+'.'+imf_used)
+				else:
+					model_path 		= join(os.environ['STELLARPOPMODELS_DIR'],'data','SSP_M11_'+model_used ,'ssp_M11_' +model_used +'.' + imf_used)
+
+			# Constructs the metallicity array of models :
+			all_metal_files = glob.glob(model_path+'*')
+			metal_files 	= []
+			metal 			= []
+			for z in range(len(all_metal_files)):
+				zchar = all_metal_files[z][len(model_path):]
+				if zchar == 'z001':
+					znum = -0.3
+				elif zchar == 'z002':
+					znum = 0.0
+				elif zchar == 'z004':
+					znum = 0.3
+				elif zchar == 'z0001.bhb':
+					znum = -1.301
+				elif zchar == 'z0001.rhb':
+					znum = -1.302
+				elif zchar == 'z10m4.bhb':
+					znum = -2.301
+				elif zchar == 'z10m4.rhb':
+					znum = -2.302
+				elif zchar == 'z10m4':
+					znum = -2.300
+				else:
+					raise NameError('Unrecognised metallicity! Check model file names.')
+
+				if znum>self.Z_limits[0] and znum<self.Z_limits[1]:
+					metal_files.append(all_metal_files[z])
+					metal.append(znum)
+			# constructs the model array
+			model_flux, age_model, metal_model = [],[],[]
+			for zi,z in enumerate(metal_files):
+				print "Retrieving and downgrading models for "+z
+				model_table = pd.read_table(z,converters={'Age':np.float64}, header=None ,usecols=[0,2,3], names=['Age','wavelength_model','flux_model'], delim_whitespace=True)
+				age_data = np.unique(model_table['Age'].values.ravel())
+				for a in age_data:
+					logyrs_a = np.log10(a)+9.0
+					#print "age model selection:", self.age_limits[0], logyrs_a, self.age_limits[1]
+					if logyrs_a < self.age_limits[0] or logyrs_a > self.age_limits[1]:
+						continue
+
+					spectrum = model_table.loc[model_table.Age == a, ['wavelength_model', 'flux_model'] ].values
+					wavelength_int,flux = spectrum[:,0],spectrum[:,1]
+
+					# converts to air wavelength
+					if self.data_wave_medium == 'vacuum':
+						wavelength = airtovac(wavelength_int)
+					else:
+						wavelength = wavelength_int
+
+					# downgrades the model
+					if self.downgrade_models:
+						mf = downgrade(wavelength,flux,deltal,self.vdisp_round, wave_instrument, r_instrument)
+					else:
+						mf = copy.copy(flux)			
+			
+					# Reddens the models
+					if ebv_mw != 0:
+						attenuations = unred(wavelength,ebv=0.0-ebv_mw)
+						model_flux.append(mf*attenuations)
+					else:
+						model_flux.append(mf)
+
+					age_model.append(a)
+					metal_model.append(metal[zi])
+					first_model = False
+
+			print "Retrieved all models!"
+			self.model_wavelength, self.model_flux, self.age_model, self.metal_model = wavelength, model_flux, age_model, metal_model
+			return wavelength, model_flux, age_model, metal_model
+
+		elif self.models == 'm09':
+			first_file  = True 
+			model_files = []
+			if self.downgrade_models:
+				model_path 		= os.environ['STELLARPOPMODELS_DIR']+'/data/UVmodels_Marastonetal08b/'
+			else:
+				model_path 		= os.environ['STELLARPOPMODELS_DIR']+'/data/UVmodels_Marastonetal08b_downgraded/'
+			# Gathers the list of models with metallicities and ages of interest:
+			all_metal_files = glob.glob(model_path+'*')
+			metal_files 	= []
+			metal 			= []
+			for z in range(len(all_metal_files)):
+				zchar = all_metal_files[z].split('.')[1][2:]
+				if zchar == 'z001':
+					znum = -0.3
+				elif zchar == 'z002':
+					znum = 0.0
+				elif zchar == 'z004':
+					znum = 0.3
+				elif zchar == 'z0001':
+					znum = -1.300
+				else:
+					raise NameError('Unrecognised metallicity! Check model file names.')
+
+				if znum>self.Z_limits[0] and znum<self.Z_limits[1]:
+					metal_files.append(all_metal_files[z])
+					metal.append(znum)
+
+			# constructs the model array
+			model_flux, age_model, metal_model = [],[],[]
+			for zi,z in enumerate(metal_files):
+				print "Retrieving and downgrading models for "+z
+				model_table = pd.read_table(z,converters={'Age':np.float64}, header=None ,usecols=[0,2,3], names=['Age','wavelength_model','flux_model'], delim_whitespace=True)
+				age_data = np.unique(model_table['Age'].values.ravel())
+				for a in age_data:
+					logyrs_a = np.log10(a)+9.0
+					#print "age model selection:", self.age_limits[0], logyrs_a, self.age_limits[1]
+					if logyrs_a < self.age_limits[0] or logyrs_a > self.age_limits[1]:
+						continue
+
+					spectrum = model_table.loc[model_table.Age == a, ['wavelength_model', 'flux_model'] ].values
+					wavelength_int,flux = spectrum[:,0],spectrum[:,1]
+
+					# converts to air wavelength
+					if self.data_wave_medium == 'vacuum':
+						wavelength = airtovac(wavelength_int)
+					else:
+						wavelength = wavelength_int
+
+					# downgrades the model
+					if self.downgrade_models:
+						mf = downgrade(wavelength,flux,deltal,self.vdisp_round, wave_instrument, r_instrument)
+					else:
+						mf = copy.copy(flux)			
+			
+					# Reddens the models
+					if ebv_mw != 0:
+						attenuations = unred(wavelength,ebv=0.0-ebv_mw)
+						model_flux.append(mf*attenuations)
+					else:
+						model_flux.append(mf)
+
+					age_model.append(a)
+					metal_model.append(metal[zi])
+					first_model = False
+
+			print "Retrieved all models!"
+			self.model_wavelength, self.model_flux, self.age_model, self.metal_model = wavelength, model_flux, age_model, metal_model
+			return wavelength, model_flux, age_model, metal_model
+
+
+	def fit_models_to_data(self):
+		"""
+		Once the data and models are loaded, then execute this function to find the best model. It loops overs the models to be fitted on the data:
+		 #. gets the models
+		 #. matches the model and data to the same resolution
+		 #. normalises the spectra
+		"""
+		for mi,mm in enumerate(self.model_libs):
+			# loop over the models
+			for ii in self.imfs:
+				# loop over the IMFs
+				# A. gets the models
+				print "getting the models"
+				deltal = self.deltal_libs[mi]
+				model_wave_int, model_flux_int, age,metal = self.get_model( mm, ii, deltal, self.specObs.vdisp, self.specObs.restframe_wavelength, self.specObs.r_instrument, self.specObs.ebv_mw)
+				# B. matches the model and data to the same resolution
+				print "Matching models to data"
+				wave,data_flux,error_flux,model_flux_raw = match_data_models( self.specObs.restframe_wavelength, self.specObs.flux, self.specObs.bad_flags, self.specObs.error, model_wave_int, model_flux_int, self.wave_limits[0], self.wave_limits[1], saveDowngradedModel = False)
+				# C. normalises the models to the median value of the data
+				print "Normalising the models"
+				model_flux,mass_factors = normalise_spec(data_flux,model_flux_raw)
+
+			# 3. Corrects from dust attenuation
+			if self.hpf_mode=='on':
+				# 3.1. Determining attenuation curve through HPF fitting, apply attenuation curve to models and renormalise spectra
+				best_ebv, attenuation_curve = determine_attenuation(wave,data_flux, error_flux, model_flux, self, age, metal)
+				model_flux_atten = np.zeros(np.shape(model_flux_raw))
+				for m in range(len(model_flux_raw)):
+					model_flux_atten[m] = attenuation_curve * model_flux_raw[m]
+
+				model_flux,mass_factors = normalise_spec(data_flux,model_flux_atten)
+				# 4. Fits the models to the data
+				light_weights,chis,branch = fitter(wave,data_flux,error_flux,model_flux ,self)
+			
+			elif self.hpf_mode == 'hpf_only':
+
+				# 3.2. Uses filtered values to determing SP properties only."
+				smoothing_length = self.dust_smoothing_length
+				hpf_data    = hpf(data_flux)
+				hpf_models  = np.zeros(np.shape(model_flux))
+				for m in range(len(model_flux)):
+					hpf_models[m] = hpf(model_flux[m])
+
+				zero_dat = np.where( (np.isnan(hpf_data)) & (np.isinf(hpf_data)) )
+				hpf_data[zero_dat] = 0.0
+				for m in range(len(model_flux)):
+					hpf_models[m,zero_dat] = 0.0
+				hpf_error    = np.zeros(len(error_flux))
+				hpf_error[:] = np.median(error_flux)/np.median(data_flux) * np.median(hpf_data)
+				hpf_error[zero_dat] = np.max(hpf_error)*999999.9
+
+				best_ebv = -99
+				hpf_models,mass_factors = normalise_spec(hpf_data,hpf_models)
+				# 4. Fits the models to the data
+				light_weights,chis,branch = fitter(wave, hpf_data,hpf_error, hpf_models, self)
+			
+			# 5. Get mass-weighted SSP contributions using saved M/L ratio.
+			unnorm_mass,mass_weights = light_weights_to_mass(light_weights,mass_factors)
+
+			print "Fitting complete! Calculating average properties and outputting."
+			# 6. Convert chis into probabilities and calculates all average properties and errors
+			dof = len(wave)
+			probs = convert_chis_to_probs(chis,dof)
+			dist_lum	= self.cosmo.luminosity_distance( self.specObs.redshift).to( u.cm ).value
+			averages = calculate_averages_pdf(probs,light_weights,mass_weights, unnorm_mass, age,metal, self.pdf_sampling, dist_lum)
+			unique_ages 				= np.unique(age)
+			marginalised_age_weights 	= np.zeros(np.shape(unique_ages))
+			marginalised_age_weights_int = np.sum(mass_weights.T,1)
+			for ua in range(len(unique_ages)):
+				marginalised_age_weights[ua] = np.sum(marginalised_age_weights_int[np.where(age==unique_ages[ua])])
+
+			best_fit_index = [np.argmin(chis)]
+			best_fit = np.dot(light_weights[best_fit_index],model_flux)[0]
+
+			self.wave = wave
+			self.best_fit = best_fit
+			self.averages = averages
+
+			# 8. It writes the output file
+			waveCol = pyfits.Column(name="wavelength",format="D", unit="Angstrom", array= wave)
+			best_fitCol = pyfits.Column(name="firefly_model",format="D", unit="1e-17erg/s/cm2/Angstrom", array= best_fit)
+			cols = pyfits.ColDefs([ waveCol, best_fitCol]) 
+			tbhdu = pyfits.BinTableHDU.from_columns(cols)
+
+			tbhdu.header['light_age'] = averages['light_age'] # log(Gyrs)
+			tbhdu.header['light_age_up'] = averages['light_age_1_sig_plus'] # log(Gyrs)
+			tbhdu.header['light_age_low'] = averages['light_age_1_sig_minus'] # log(Gyrs)
+			tbhdu.header['light_metallicity'] = averages['light_metal']
+			tbhdu.header['light_metallicity_up'] = averages['light_metal_1_sig_plus']
+			tbhdu.header['light_metallicity_low'] = averages['light_metal_1_sig_minus']
+			tbhdu.header['mass_age'] = averages['mass_age'] # log(Gyrs)
+			tbhdu.header['mass_age_up'] = averages['mass_age_1_sig_plus'] # log(Gyrs)
+			tbhdu.header['mass_age_low'] = averages['mass_age_1_sig_minus'] # log(Gyrs)
+			tbhdu.header['mass_metallicity'] = averages['mass_metal']
+			tbhdu.header['mass_metallicity_up'] = averages['mass_metal_1_sig_plus']
+			tbhdu.header['mass_metallicity_low'] = averages['mass_metal_1_sig_minus']
+			tbhdu.header['EBV'] = best_ebv
+			tbhdu.header['stellar_mass'] = averages['stellar_mass']
+			tbhdu.header['stellar_mass_up'] = averages['stellar_mass_1_sig_plus']
+			tbhdu.header['stellar_mass_low'] = averages['stellar_mass_1_sig_minus']
+
+			prihdr = pyfits.Header()
+			prihdr['file'] = self.specObs.path_to_spectrum
+			prihdr['model'] = self.models
+			prihdr['ageMin'] = self.age_limits[0]
+			prihdr['ageMax'] = self.age_limits[1]
+			prihdr['Zmin'] = self.Z_limits[0]
+			prihdr['Zmax'] = self.Z_limits[1]
+			prihdu = pyfits.PrimaryHDU(header=prihdr)
+
+			thdulist = pyfits.HDUList([prihdu, tbhdu])
+			os.system('rm '+self.outputFile + self.suffix )
+			thdulist.writeto(self.outputFile + self.suffix )
