2faacc89d4d6daf71a528f87ecb939d13264f5f2
JohanComparat <johan.comparat@gmail.com>
2016-03-03 18:05:46 +0100

    s

--- pyGalaxy/trunk/bin/plot_results.py
+++ pyGalaxy/trunk/bin/plot_results.py
@@ -12,7 +12,11 @@ from os.path import join
 
 path_to_summary_table = join(os.environ['SPECTRASTACKS_DIR'], "results", "table_v0.data")
 
-lineWavelength,Survey,Redshift,L_MIN,L_MAX,L_MEAN,N_in_stack,R_stack,spm_light_age,spm_light_age_err_plus,spm_light_age_err_minus,spm_light_metallicity,spm_light_metallicity_err_plus,spm_light_metallicity_err_minus,spm_stellar_mass,spm_stellar_mass_err_plus,spm_stellar_mass_err_minus,spm_EBV,gp_EBV_4862_4341,gp_EBV_4862_4341_err,gp_EBV_4862_4102,gp_EBV_4862_4102_err,gp_BD_4102_4341,gp_BD_4102_4341_err,gp_SFR_O2_3728,gp_SFR_O2_3728_err,gp_SFR_H1_4862,gp_SFR_H1_4862_err,gp_12logOH_tremonti04,gp_12logOH_tremonti04_err,gp_12logOH_tremonti04_intrinsic,gp_12logOH_tremonti04_intrinsic_err = n.loadtxt(path_to_summary_table)	
+data = n.loadtxt(path_to_summary_table)	
+
+ok = (data[2]<0.85)&(data[2]>0.7)
+
+lineWavelength,Survey,Redshift,L_MIN,L_MAX,L_MEAN,N_in_stack,R_stack,spm_light_age,spm_light_age_err_plus,spm_light_age_err_minus,spm_light_metallicity,spm_light_metallicity_err_plus,spm_light_metallicity_err_minus,spm_stellar_mass,spm_stellar_mass_err_plus,spm_stellar_mass_err_minus,spm_EBV,gp_EBV_4862_4341,gp_EBV_4862_4341_err,gp_EBV_4862_4102,gp_EBV_4862_4102_err,gp_BD_4102_4341,gp_BD_4102_4341_err,gp_SFR_O2_3728,gp_SFR_O2_3728_err,gp_SFR_H1_4862,gp_SFR_H1_4862_err,gp_12logOH_tremonti04,gp_12logOH_tremonti04_err,gp_12logOH_tremonti04_intrinsic,gp_12logOH_tremonti04_intrinsic_err = data.T[ok].T
 
 # EBV comparison
 #spm_EBV
@@ -109,22 +113,22 @@ p.clf()
 # Mannucci et al. 2010 :
 y_rel = lambda x_rel : 8.90 + 0.39*x_rel - 0.20*x_rel*x_rel - 0.077*x_rel*x_rel*x_rel + 0.064*x_rel*x_rel*x_rel*x_rel
 
-x_obs_1 = spm_stellar_mass - 0.32 * gp_SFR_O2_3728 - 10
-x_obs_2 = spm_stellar_mass - 0.32 * gp_SFR_H1_4862 - 10
+x_obs_1 = spm_stellar_mass - 0.32 * n.log10(gp_SFR_O2_3728) - 10
+x_obs_2 = spm_stellar_mass - 0.32 * n.log10(gp_SFR_H1_4862) - 10
 y_obs_1 = spm_light_metallicity
-y_obs_2 = gp_12logOH_tremonti04
-y_obs_3 = gp_12logOH_tremonti04_intrinsic
 
 x_rel = n.arange(8.5,11.5,0.05) - 10 
 y_pr = y_rel(x_rel)
 
+ok = (gp_SFR_O2_3728>0)& (gp_SFR_O2_3728_err>0)&(spm_stellar_mass >0)
+
 p.figure(0,(6,6))
 p.axes([0.17,0.17,0.8,0.8])
 p.plot(x_obs_1, y_obs_1, )
-p.errorbar(x_obs_1[ok], spm_light_metallicity[ok], yerr=[spm_light_metallicity_err_plus[ok], spm_light_metallicity_err_minus[ok]],fmt='o',elinewidth=2, mfc='none')
+p.errorbar(x_obs_1[ok], 12+spm_light_metallicity[ok], yerr=[spm_light_metallicity_err_plus[ok], spm_light_metallicity_err_minus[ok]],fmt=None, mfc='none')
 p.plot(x_rel, y_pr, 'k--')
 p.ylabel(r'$12 \log(O/H)$')
 p.xlabel('$\log(M_*)-0.32 \log(SFR)-10$')
-p.xlim((9,11.5))
+p.xlim((-2,2.5))
 p.savefig( join(os.environ['SPECTRASTACKS_DIR'], "plots", "sfr-mass-z.png"))
 p.clf()
