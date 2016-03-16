2fe409f8d1bc3b762e77d6a41482db21dbdda435
JohanComparat <johan.comparat@gmail.com>
2016-03-16 17:49:42 +0100

    a

--- pyMultidark/trunk/python/MultiDark.py
+++ pyMultidark/trunk/python/MultiDark.py
@@ -87,16 +87,16 @@ class MultiDarkSimulation :
 		if self.boxDir == "MD_1Gpc_new_rockS" :
 			self.columnDictHlist = {'id': 0, 'desc_id': 1, 'mvir': 2, 'vmax': 3, 'vrms': 4, 'rvir': 5, 'rs': 6, 'Np': 7, 'x': 8, 'y': 9, 'z': 10, 'vx': 11, 'vy': 12, 'vz': 13, 'Jx': 14, 'Jy': 15, 'Jz': 16, 'Spin':17, 'Rs_Klypin': 18, 'Mmvir_all': 19, 'M200b': 20, 'M200c': 21, 'M500c': 22, 'M2500c': 23, 'Xoff': 24, 'Voff': 25, 'Spin_Bullock': 26, 'b_to_a': 27, 'c_to_a': 28, 'Ax': 29, 'Ay': 30, 'Az': 31, 'b_to_a_500c': 32, 'c_to_a_500c': 33, 'Ax_500c': 34, 'Ay_500c': 35, 'Az_500c': 36, 'TU': 37, 'M_pe_Behroozi': 38, 'M_pe_Diemer': 39, 'pid': 40}
 
-	def writePositionCatalog(self, ii, vmin=70, vmax=3500):
+	def writePositionCatalog(self, ii, vmin=70, vmax=3500, NperBatch = 1000000):
 		"""
 		Extracts the positions and velocity out of a snapshot of the Multidark simulation.        
 		:param ii: index of the snapshot in the list self.snl
 		:param vmin: name of the quantity of interest, mass, velocity.
 		:param vmax: of the quantity of interest in the snapshots.
+		:param NperBatch: number of line per fits file, default: 1000000
 		 """		
 		fl = fileinput.input(self.snl[ii])
 		nameSnapshot = self.snl[ii].split('/')[-1][:-5]
-		NperBatch = 10000000
 		Nb = 0
 		count = 0
 		output = n.empty((NperBatch,5))
