8ed17b1b3fd95168a3ea17e673aa156852913815
JohanComparat <johan.comparat@gmail.com>
2016-03-18 10:28:08 +0100

    s

--- pyMultidark/trunk/bin/MD04box-write-positionFiles.py
+++ pyMultidark/trunk/bin/MD04box-write-positionFiles.py
@@ -1,7 +1,7 @@
 from MultiDark import *
 
 
-snList=  n.array(glob.glob(" /data2/DATA/eBOSS/Multidark-lightcones/MD_0.4Gpc/snapshots/hlist_*.list"))
+snList=  n.array(glob.glob("/data2/DATA/eBOSS/Multidark-lightcones/MD_0.4Gpc/snapshots/hlist_*.list"))
 
 box = MultiDarkSimulation(Lbox=400.0 * uu.Mpc, boxDir = "MD_0.4Gpc",snl =snList   ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
 
