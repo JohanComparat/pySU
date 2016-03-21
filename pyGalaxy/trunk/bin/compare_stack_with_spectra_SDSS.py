d8e5a2ac27e130d1e46d1c621b98afd07ab16bfc
JohanComparat <johan.comparat@gmail.com>
2016-03-21 18:13:07 +0100

    s

--- pyGalaxy/trunk/bin/compare_stack_with_spectra_SDSS.py
+++ pyGalaxy/trunk/bin/compare_stack_with_spectra_SDSS.py
@@ -23,28 +23,22 @@ for ii, el in enumerate(list_of_stacks):
 	outPutFileName = join("/uufs/chpc.utah.edu/common/home/u0936736/stack_eBOSSELG",el[:-5].split('/')[-1] + "_stackComparison.dat")
 	hdu = fits.open(stackName)[1].data
 	sel= (hdu['NspectraPerPixel']>0.9*n.max(hdu['NspectraPerPixel']))
+	chi2median = n.empty_like(PLATE) 
+	chi2mean = n.empty_like(PLATE) 
 	for jj in range(len(PLATE)):
-		chi2median = n.empty_like(PLATE) 
-		chi2mean = n.empty_like(PLATE) 
-
 		ObsPlate = HandleReducedELGPlate(int(PLATE[ii]),int(MJD[ii]))
 		ObsPlate.loadSpec(int(FIBERID[ii]))
-
 		wlmin=n.min(hdu['wavelength'][sel]*(1+REDSHIFT[jj]))
 		wlmax=n.max(hdu['wavelength'][sel]*(1+REDSHIFT[jj]))
-
 		medianStack = interp1d(hdu['wavelength'][sel]*(1+REDSHIFT[jj]),hdu['meanWeightedStack'][sel])
 		meanStack =interp1d(hdu['wavelength'][sel]*(1+REDSHIFT[jj]),hdu['medianStack'][sel])
-
 		overlap=(ObsPlate.wavelength>wlmin)&(ObsPlate.wavelength<wlmax)
 		x = ObsPlate.wavelength[overlap]
 		y = ObsPlate.flux[overlap]
 		yerr = ObsPlate.fluxErr[overlap]
-
 		chi2median[jj] = n.sum(((y - medianStack(x))/yerr)**2.)/len(x)
 		chi2mean[jj] = n.sum(((y - meanStack(x))/yerr)**2.)/len(x)
 
-		PLATE ,   MJD  ,  FIBERID ,   REDSHIFT   , gmag ,   rzcol  ,  grcol = n.loadtxt(el, unpack=True)
-		header = " PLATE MJD FIBERID REDSHIFT gmag rzcol grcol chi2median chi2mean"
-		n.savetxt(outPutFileName, n.transpose([PLATE ,   MJD  ,  FIBERID ,   REDSHIFT   , gmag ,   rzcol  ,  grcol , chi2median, chi2mean]), header = header)
+	header = " PLATE MJD FIBERID REDSHIFT gmag rzcol grcol chi2median chi2mean"
+	n.savetxt(outPutFileName, n.transpose([PLATE ,   MJD  ,  FIBERID ,   REDSHIFT   , gmag ,   rzcol  ,  grcol , chi2median, chi2mean]), header = header)
 
