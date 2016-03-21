ebbefa39f133f37217086a5ddf2211889c9cbc4d
JohanComparat <johan.comparat@gmail.com>
2016-03-21 08:56:59 +0100

    d

--- pyMultidark/trunk/bin/MD04box-compute2PCF-vbins.py
+++ pyMultidark/trunk/bin/MD04box-compute2PCF-vbins.py
@@ -3,7 +3,7 @@ from MultiDark import *
 box = MultiDarkSimulation(Lbox=400.0 * uu.Mpc, boxDir = "MD_0.4Gpc")
 
 all=n.array([ 0.31800, 0.50000, 0.59240, 0.74230, 0.90430, 1.00000 ])
-for ii in range(len(all):
+for ii in range(len(all)):
 	a= all[ii]
 	ll = n.array( glob.glob( "/data2/DATA/eBOSS/Multidark-lightcones/MD_0.4Gpc/snapshots/*"+str(a)+"*.fits" ) )
 	box.compute2PCF(ll)
--- pyMultidark/trunk/bin/MD10box-compute2PCF-vbins.py
+++ pyMultidark/trunk/bin/MD10box-compute2PCF-vbins.py
@@ -3,7 +3,7 @@ from MultiDark import *
 box = MultiDarkSimulation(Lbox=1000.0 * uu.Mpc, boxDir = "MD_1Gpc_new_rockS")
 
 all=n.array([ 0.25320, 0.33030, 0.36090, 0.40320, 0.44060, 0.49220 ])
-for ii in range(len(all):
+for ii in range(len(all)):
 	a= all[ii]
 	ll = n.array( glob.glob( "/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc_new_rockS/snapshots/*"+str(a)+"*.fits" ) )
 	box.compute2PCF(ll)
