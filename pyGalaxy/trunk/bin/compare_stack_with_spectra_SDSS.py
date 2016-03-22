0f2f1523996b4f9aae0ae19b7b0ad3ac2c4961bc
JohanComparat <johan.comparat@gmail.com>
2016-03-22 20:05:23 +0100

    s

--- pyGalaxy/trunk/bin/compare_stack_with_spectra_SDSS.py
+++ pyGalaxy/trunk/bin/compare_stack_with_spectra_SDSS.py
@@ -4,41 +4,65 @@ from os.path import join
 from SpectraStackingSDSSOnly import *
 from HandleSdssPlate import *
 import glob	
-specDirSdssMain = "/uufs/chpc.utah.edu/common/home/sdss00/sdsswork/sdss/spectro/redux/26/spectra"
-list_of_stacks_eb67 = glob.glob(join("/uufs/chpc.utah.edu/common/home/u0992342/eboss67/grz_stacks/","*.asc"))
-list_of_stacks_eb17 = glob.glob(join("/uufs/chpc.utah.edu/common/home/u0992342/eboss17/grz_stacks/","*.asc"))
 
-list_of_stacks = n.hstack((list_of_stacks_eb67,list_of_stacks_eb17))
-list_of_stacks.sort()
+stackDir = "/uufs/chpc.utah.edu/common/home/u0936736/stack_eBOSSELG"
+ggrid  = [21.8,22.5,22.8]
+rzgrid = [0.0,0.8,1.0,2.0]
+grgrid = [0.0,0.4,0.6,1.0]
 
-for ii, el in enumerate(list_of_stacks):
-	PLATE ,   MJD  ,  FIBERID ,   REDSHIFT   , gmag ,   rzcol  ,  grcol = n.loadtxt(el, unpack=True, usecols=(0,1,2,3,4,5,6))
-	g_min = n.min(gmag)
-	g_max = n.max(gmag)
-	gr_min = n.min(grcol)
-	gr_max = n.max(grcol)
-	rz_min = n.min(grcol)
-	rz_max = n.max(grcol)
-	stackName = join("/uufs/chpc.utah.edu/common/home/u0936736/stack_eBOSSELG",el[:-4].split('/')[-1] + "_stack.fits")
-	outPutFileName = join("/uufs/chpc.utah.edu/common/home/u0936736/stack_eBOSSELG",el[:-4].split('/')[-1] + "_stackComparison.dat")
-	hdu = fits.open(stackName)[1].data
-	sel= (hdu['NspectraPerPixel']>0.9*n.max(hdu['NspectraPerPixel']))
-	chi2median = n.empty_like(PLATE) 
-	chi2mean = n.empty_like(PLATE) 
-	for jj in range(len(PLATE)):
-		ObsPlate = HandleReducedELGPlate(int(PLATE[ii]),int(MJD[ii]))
-		ObsPlate.loadSpec(int(FIBERID[ii]))
-		wlmin=n.min(hdu['wavelength'][sel]*(1+REDSHIFT[jj]))
-		wlmax=n.max(hdu['wavelength'][sel]*(1+REDSHIFT[jj]))
-		medianStack = interp1d(hdu['wavelength'][sel]*(1+REDSHIFT[jj]),hdu['meanWeightedStack'][sel])
-		meanStack =interp1d(hdu['wavelength'][sel]*(1+REDSHIFT[jj]),hdu['medianStack'][sel])
-		overlap=(ObsPlate.wavelength>wlmin)&(ObsPlate.wavelength<wlmax)
-		x = ObsPlate.wavelength[overlap]
-		y = ObsPlate.flux[overlap]
-		yerr = ObsPlate.fluxErr[overlap]
-		chi2median[jj] = n.sum(((y - medianStack(x))/yerr)**2.)/len(x)
-		chi2mean[jj] = n.sum(((y - meanStack(x))/yerr)**2.)/len(x)
+def compareSpectrumToStack(entry, nameRoot="elg270_eboss17_", ggrid  = [21.8,22.5,22.8], rzgrid = [0.0,0.8,1.0,2.0], grgrid = [0.0,0.4,0.6,1.0]):
+	# gets the spectrum
+	ObsPlate = HandleReducedELGPlate(entry['PLATE'],entry['MJD'])
+	ObsPlate.loadSpec(entry['FIBER'])
+	# gets the stack
+	if entry['index_g']>=0:
+		suffix = "_g_"+str(n.round(ggrid[entry['index_g']],1))+"_rz_"+str(n.round(rzgrid[entry['index_rz']],1))+"_gr_"+str(n.round(grgrid[entry['index_gr']],1))
+		stackName = join(stackDir, nameRoot + suffix + "_stack.fits")
+		hdu = fits.open(stackName)[1].data
+		sel = (hdu['NspectraPerPixel']>0.9*n.max(hdu['NspectraPerPixel']))
+		# compares stakc and spectrum at REDSHIFT
+		def getchi2(REDSHIFT):
+			wlmin=n.min(hdu['wavelength'][sel]*(1+REDSHIFT))
+			wlmax=n.max(hdu['wavelength'][sel]*(1+REDSHIFT))
+			meanStack =interp1d(hdu['wavelength'][sel]*(1+REDSHIFT),hdu['medianStack'][sel])
+			overlap=(ObsPlate.wavelength>wlmin)&(ObsPlate.wavelength<wlmax)
+			x = ObsPlate.wavelength[overlap]
+			y = ObsPlate.flux[overlap]
+			yerr = ObsPlate.fluxErr[overlap]
+			chi2mean = n.sum(((y - meanStack(x))/yerr)**2.)/len(x)
+			return chi2mean
+			
+		return getchi2(entry['Z_1']), getchi2(entry['Z_2']), getchi2(entry['Z_3'])
+	else:
+		return -1,-1,-1
 
-	header = " PLATE MJD FIBERID REDSHIFT gmag rzcol grcol chi2median chi2mean"
-	n.savetxt(outPutFileName, n.transpose([PLATE ,   MJD  ,  FIBERID ,   REDSHIFT   , gmag ,   rzcol  ,  grcol , chi2median, chi2mean]), header = header)
 
+def compareSpectraAndStack(nameRoot):
+	summaryTableName =join(stackDir, nameRoot + "summaryTable_stack.fits")
+	table = fits.open(summaryTableName)
+	chi1 = n.empty(len(table['PLATE']))
+	chi2 = n.empty(len(table['PLATE']))
+	chi3 = n.empty(len(table['PLATE']))
+
+	for ii in range(len(table['PLATE'])):
+		print entry
+		entry = table[1].data[ii]
+		chi1[ii], chi2[ii], chi3[ii] = compareSpectrumToStack(entry, nameRoot=nameRoot, ggrid  = ggrid, rzgrid = rzgrid, grgrid = grgrid)
+
+	summaryTableName =join(stackDir, nameRoot + "summaryTable_stack_comparison.fits")
+	col_chi1 = fits.Column(name="chi2_Z1",format="D", array= chi1)
+	col_chi2 = fits.Column(name="chi2_Z2",format="D", array= chi2)
+	col_chi3 = fits.Column(name="chi2_Z3",format="D", array= chi3)
+	cols = table.columns + col_chi1 + col_chi2 + col_chi3
+	tbhdu = fits.BinTableHDU.from_columns(cols)
+	prihdr = fits.Header()
+	prihdr['chunk'] = nameRoot
+	prihdu = fits.PrimaryHDU(header=prihdr)
+	thdulist = fits.HDUList([prihdu, tbhdu])
+	os.system('rm '+summaryTableName)
+	thdulist.writeto(summaryTableName)
+
+nameRoot="elg270_eboss17"
+compareSpectraAndStack(nameRoot)
+nameRoot="elg270_eboss67"
+compareSpectraAndStack(nameRoot)
