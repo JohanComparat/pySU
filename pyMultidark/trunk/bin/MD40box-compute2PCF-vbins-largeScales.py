a632d6f7e8afd39ee67e2fdc572e05146aa1baed
JohanComparat <johan.comparat@gmail.com>
2016-03-23 19:04:36 +0100

    a

---
+++ pyMultidark/trunk/bin/MD40box-compute2PCF-vbins-largeScales.py
@@ -0,0 +1,12 @@
+from MultiDark import *
+
+box = MultiDarkSimulation(Lbox4000.0 * uu.Mpc, boxDir = "MD_4Gpc")
+
+all=n.array([ 105, 112, 124, 128, 66, 79, 82, 87, 91, 97 ])
+for ii in range(len(all)):
+	a= all[ii]
+	ll = n.array( glob.glob( "/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/hlist_"+str(a)+"_Nb*.fits" ) )
+	box.compute2PCF(ll, rmax=200, Nmax=800000)
+
+
+
---
+++ pyMultidark/trunk/bin/MD40box-write-positionFiles.py
@@ -0,0 +1,8 @@
+from MultiDark import *
+snList =  n.array(glob.glob(join("/data2/DATA/eBOSS/Multidark-lightcones" , "MD_4Gpc", "snapshots" , "hlist_*.list")))
+box = MultiDarkSimulation(Lbox=4000.0 * uu.Mpc, boxDir = "MD_4Gpc",snl = snList ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
+
+for ii in n.arange(len(box.snl)):
+	box.writePositionCatalog(ii, vmin = 400.)
+
+
