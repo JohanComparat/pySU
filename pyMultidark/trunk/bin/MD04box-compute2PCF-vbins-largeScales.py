336252e2372a1e8ae875a7429b35d93f98741619
JohanComparat <johan.comparat@gmail.com>
2016-03-27 15:17:23 +0200

    s

--- pyMultidark/trunk/bin/MD04box-compute2PCF-vbins-largeScales.py
+++ pyMultidark/trunk/bin/MD04box-compute2PCF-vbins-largeScales.py
@@ -2,9 +2,9 @@ from MultiDark import *
 
 box = MultiDarkSimulation(Lbox=400.0 * uu.Mpc, boxDir = "MD_0.4Gpc")
 
-all=n.array([ 0.31800, 0.50000, 0.59240, 0.74230, 0.90430, 1.00000 ])
+all=n.array([ 0.31800, 0.40700, 0.50000, 0.59240, 0.74230, 0.90430, 1.00000 ])
 for ii in range(len(all)):
 	a= all[ii]
 	ll = n.array( glob.glob( "/data2/DATA/eBOSS/Multidark-lightcones/MD_0.4Gpc/snapshots/*"+str(a)+"*.fits" ) )
-	box.compute2PCF(ll, rmax=30, Nmax=4000000)
+	box.compute2PCF(ll, rmax=20, Nmax=4000000)
 
