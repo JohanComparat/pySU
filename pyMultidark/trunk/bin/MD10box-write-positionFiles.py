7650fd71b30ff2f8338de85eafb090074128db4d
JohanComparat <johan.comparat@gmail.com>
2016-04-01 16:45:10 +0200

    d

--- pyMultidark/trunk/bin/MD10box-write-positionFiles.py
+++ pyMultidark/trunk/bin/MD10box-write-positionFiles.py
@@ -5,5 +5,5 @@ snList= n.array(["/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc_new_rockS/snaps
 box = MultiDarkSimulation(Lbox=1000.0 * uu.Mpc, boxDir = "MD_1Gpc_new_rockS",snl =snList   ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
 
 for ii in n.arange(len(box.snl)):
-	box.writePositionCatalog(ii)
+	box.writePositionCatalog(ii, vmin = 65)
 
--- pyMultidark/trunk/bin/MD25box-write-positionFiles.py
+++ pyMultidark/trunk/bin/MD25box-write-positionFiles.py
@@ -3,6 +3,6 @@ snList =  n.array(glob.glob(join("/data2/DATA/eBOSS/Multidark-lightcones" , "MD_
 box = MultiDarkSimulation(Lbox=2500.0 * uu.Mpc, boxDir = "MD_2.5Gpc",snl = snList ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
 
 for ii in n.arange(len(box.snl)):
-	box.writePositionCatalog(ii)
+	box.writePositionCatalog(ii, vmin = 100)
 
 
