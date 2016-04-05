e32321b3e086cc708c87cdd149daae6c94326bfc
JohanComparat <johan.comparat@gmail.com>
2016-04-05 14:01:52 +0200

    qa

---
+++ pyMultidark/trunk/bin/MD04box-write-positionFiles-hydra.py
@@ -0,0 +1,10 @@
+from MultiDarkHydra import *
+
+snList=  n.array(glob.glob("/lustre/jcomparat/Multidark/MD_0.4Gpc_Rockstar/snapshots/out*.list.bz2"))
+
+box = MultiDarkSimulation(Lbox=400.0 * uu.Mpc, boxDir = "MD_0.4Gpc",snl =snList )
+
+for ii in n.arange(len(box.snl)):
+	box.writePositionCatalog(ii, vmin=65)
+
+
