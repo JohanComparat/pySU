92d19db1a4a7288d3c70ff6369e71cc6286dc0c4
JohanComparat <johan.comparat@gmail.com>
2016-04-07 18:49:20 +0200

    s

--- pyMultidark/trunk/bin/MD10box-compute2PCF-vbins.py
+++ pyMultidark/trunk/bin/MD10box-compute2PCF-vbins.py
@@ -2,7 +2,7 @@ from MultiDark import *
 
 box = MultiDarkSimulation(Lbox=1000.0 * uu.Mpc, boxDir = "MD_1Gpc_new_rockS")
 
-all=n.array([ 1.00000, 0.50320, 0.60080, 0.74980, 0.89510 ])
+all=n.array([ 1.00000, 0.50320, 0.89510, 0.74980 ])
 for ii in range(len(all)):
         a= all[ii]
         ll = n.array( glob.glob( "/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc_new_rockS/snapshots/*"+str(a)+"*.fits" ) )
