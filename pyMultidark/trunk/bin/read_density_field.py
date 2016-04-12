9fb2b305936fdd4d2fdfdfda10d3a29a08024f58
JohanComparat <johan.comparat@gmail.com>
2016-04-12 17:27:11 +0200

    s

--- pyMultidark/trunk/bin/read_density_field.py
+++ pyMultidark/trunk/bin/read_density_field.py
@@ -3,88 +3,98 @@ import os
 from os.path import join
 from astropy.io import fits
 import time
+import fortranfile
+DFdir = join("/data2", "users", "gustavo", "BigMD", "1Gpc_3840_Planck1_New", "DENSFIELDS")
+mockDir = "/data1/DATA/eBOSS/Multidark-box-mocks/parts/"
 
-dir = join("/data2", "users", "gustavo", "BigMD", "1Gpc_3840_Planck1_New", "DENSFIELDS")
-filename = "dmdens_cic_087.dat"
+def writeDFMock(dataCat, DFfile, Lbox = 1000.):
+	print dataCat, DFfile
+	x, y, z, vx, vy ,vz ,Vmax, Vpeak ,Mvir, parent_id, snap_id, kind, z_dis = n.loadtxt(dataCat ,unpack=True)
+	path_to_outputCat = dataCat[:-4] + ".DF.fits.gz"
+	# opens the DF file
+	f = fortranfile.FortranFile(DFfile)
+	gridx, gridy, gridz = f.readInts()
+	dx = Lbox/gridx
+	# convert QSO positions into indexes
+	i = (x/dx).astype(int) + 1
+	j = (y/dx).astype(int) + 1
+	k= (z/dx).astype(int) + 1
+	#init the output array :
+	delta = n.empty_like(x)
+	delta1 = n.empty_like(x)
+	delta2 = n.empty_like(x)
+	# now loops over k (every line of the file) and assigns delta values.
+	for kk in range(gridx):
+		sel = (k==kk+1)
+		N = i[sel] * j[sel] -1 #* k[sel]
+		DF = f.readReals()
+		delta[sel] = DF[N]
+		# distance 1 mean density field in the plane
+		sel1 = (sel)&(i>1)&(i<gridx)&(j>1)&(j<gridx)
+		N1 = n.transpose([ (i[sel1]-1) * (j[sel1] -1), (i[sel1]) * (j[sel1] -1), (i[sel1]-1) * (j[sel1]), (i[sel1]+1) * (j[sel1] +1), (i[sel1]+1) * (j[sel1] ), (i[sel1]) * (j[sel1] +1), (i[sel1]+1) * (j[sel1] -1), (i[sel1]-1) * (j[sel1] +1) ]) - 1
+		delta1[sel1] = n.array([ n.mean(DF[el]) for el in N1 ]) 
+		# assign -1 err value to points on the boundary
+		border1 = (sel)&(sel1==False)
+		delta1[border1] = n.ones_like(delta1[border1])*-1.
+		# distance 2 mean density field in the plane
+		sel2 = (sel)&(i>3)&(i<gridx-3)&(j>3)&(j<gridx-3)
+		N2 = n.transpose([ (i[sel2]-2) * (j[sel2] -2), (i[sel2]-2) * (j[sel2] -1), (i[sel2]-2) * (j[sel2] ), (i[sel2]-2) * (j[sel2] +1), (i[sel2]-2) * (j[sel2] +2), (i[sel2]-1) * (j[sel2] + 2), (i[sel2]) * (j[sel2] +2), (i[sel2]+11) * (j[sel2] +2), (i[sel2] + 2) * (j[sel2] +2), (i[sel2] + 2) * (j[sel2] +1), (i[sel2] + 2) * (j[sel2] ), (i[sel2] + 2) * (j[sel2] -1), (i[sel2] + 2) * (j[sel2] -2), (i[sel2] + 1) * (j[sel2] -2),  (i[sel2] ) * (j[sel2] -2),  (i[sel2] - 1) * (j[sel2] -2) ]) -1 
+		delta2[sel2] = n.array([ n.mean(DF[el]) for el in N2 ])
+		# assign -1 err value to points on the boundary
+		border2 = (sel)&(sel2==False)
+		delta2[border2] = n.ones_like(delta2[border2])*-1.
 
-outDir =  join("/data2", "DATA", "eBOSS", "Multidark-lightcones", "MD_1Gpc_new_rockS", "densityField")
+	f.close()
 
-# first rewrite the density field 
-
-path_to_file = join(dir,filename)
-f=open(path_to_file,'r')
-for i in n.arange(1,65,1):
-	print i, time.time()
-	path_to_outputCat = join(outDir, "dmdens_cic_087_DF_"+str(i).zfill(2)+".fits.gz")
-	data1 =  n.fromfile(f,dtype="float64",count=134217728) # 512 cube
-	c0 = fits.Column(name="densityField",format='D', array=data1 )
-	cols = fits.ColDefs([c0])
+	c0 = fits.Column(name="DF",format='D', array=delta )
+	c01 = fits.Column(name="DF_N1",format='D', array=delta1 )
+	c02 = fits.Column(name="DF_N2",format='D', array=delta2 )
+	c1 = fits.Column(name="x",format='D', array=x )
+	c2 = fits.Column(name="y",format='D', array=y )
+	c3 = fits.Column(name="z",format='D', array=z )
+	c4 = fits.Column(name="vx",format='D', array=vx )
+	c5 = fits.Column(name="vy",format='D', array=vy )
+	c6 = fits.Column(name="vz",format='D', array=vz )
+	c7 = fits.Column(name="Vmax",format='D', array=Vmax )
+	c8 = fits.Column(name="Vpeak",format='D', array=Vpeak )
+	c9 = fits.Column(name="Mvir",format='D', array=Mvir )
+	c10 = fits.Column(name="parent_id",format='D', array=parent_id )
+	c11 = fits.Column(name="snap_id",format='D', array=snap_id )
+	c12 = fits.Column(name="kind",format='D', array=kind )
+	c13 = fits.Column(name="z_dis",format='D', array=z_dis )
+	# now writes the catalog
+	cols = fits.ColDefs([c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c0, c01, c02 ])
 	hdu = fits.BinTableHDU.from_columns(cols)
-	os.system("rm -rf "+path_to_outputCat)
 	hdu.writeto(path_to_outputCat)
 
 
-# secondly look for the values of interest
+DFfile = join(DFdir,"dmdens_cic_104.dat")
+writeDFMock(join( mockDir,"Box_HAM_z0.701838_nbar1.000000e-04_LRG.dat"), DFfile)
+writeDFMock(join( mockDir,"Box_HAM_z0.701838_nbar1.350000e-05_QSO.dat"), DFfile)
+writeDFMock(join( mockDir,"Box_HAM_z0.701838_nbar2.400000e-04_ELG.dat"), DFfile)
+DFfile = join(DFdir,"dmdens_cic_101.dat")
+writeDFMock(join( mockDir,"Box_HAM_z0.818843_nbar1.000000e-04_LRG.dat"), DFfile)
+writeDFMock(join( mockDir,"Box_HAM_z0.818843_nbar1.680000e-05_QSO.dat"), DFfile)
+writeDFMock(join( mockDir,"Box_HAM_z0.818843_nbar3.200000e-04_ELG.dat"), DFfile)
+DFfile = join(DFdir,"dmdens_cic_097.dat")
+writeDFMock(join( mockDir,"Box_HAM_z0.987281_nbar1.720000e-05_QSO.dat"), DFfile)
+writeDFMock(join( mockDir,"Box_HAM_z0.987281_nbar2.400000e-04_ELG.dat"), DFfile)
+DFfile = join(DFdir,"dmdens_cic_087.dat")
+writeDFMock(join( mockDir,"Box_HAM_z1.480160_nbar1.930000e-05_QSO.dat"), DFfile)
 
-x, y, z, vx, vy ,vz ,Vmax, Vpeak ,Mvir, parent_id, snap_id, kind, z_dis = n.loadtxt("/data1/DATA/eBOSS/Multidark-box-mocks/parts/Box_HAM_z1.480160_nbar1.930000e-05_QSO.dat",unpack=True)
-path_to_outputCat = "/data1/DATA/eBOSS/Multidark-box-mocks/parts/Box_HAM_z1.480160_nbar1.930000e-05_QSO.DF.try2.fits.gz"
-dx = 1000.0/2048
 
-i = (x/dx).astype(int) + 1
-j = (y/dx).astype(int) + 1
-k= (z/dx).astype(int) + 1
-N = i*j*k
-N1 = n.transpose([ 
-(i-1)*(j)*(k), 
-(i-1)*(j-1)*(k),  (i-1)*(j)*(k-1), 
-(i-1)*(j+1)*(k), (i-1)*(j)*(k+1), 
-(i-1)*(j-1)*(k-1), (i-1)*(j-1)*(k+1),  (i-1)*(j+1)*(k-1), (i-1)*(j+1)*(k+1), 
-(i+1)*(j)*(k), 
-(i+1)*(j-1)*(k),  (i+1)*(j)*(k-1), 
-(i+1)*(j+1)*(k), (i+1)*(j)*(k+1), 
-(i+1)*(j-1)*(k-1), (i+1)*(j-1)*(k+1),  (i+1)*(j+1)*(k-1), (i+1)*(j+1)*(k+1), 
-(i)*(j)*(k), 
-(i)*(j-1)*(k),  (i)*(j)*(k-1), 
-(i)*(j+1)*(k), (i)*(j)*(k+1), 
-(i)*(j-1)*(k-1), (i)*(j-1)*(k+1),  (i)*(j+1)*(k-1), (i)*(j+1)*(k+1), 
-])
 
-delta = n.empty_like(x)
-delta1 = n.empty_like(x)
+sys.exit()
 
-for i in n.arange(1,65,1):
-	print i, time.time()
-	path_to_DFCat = join(outDir, "dmdens_cic_087_"+str(i).zfill(2)+".fits.gz")
-	hd1 = fits.open( path_to_DFCat )
-	sel = (N< i* 512**3)  & (N> (i-1) * 512**3) 
-	index = N[ sel ]
-	indexD = n.arange(len(x))[sel]
-	delta[indexD] = hd1[1].data['densityField'][index - (i-1) * 512**3]
-	for ii, el in enumerate(N1):
-		sel2 = (el< i* 512**3)  & (el> (i-1) * 512**3) 
-		if len(sel2.nonzero()[0])==27:
-			delta1[ii] = n.mean(hd1[1].data['densityField'][el - (i-1) * 512**3])
 
-c0 = fits.Column(name="DF",format='D', array=delta )
-c01 = fits.Column(name="DF_N1",format='D', array=delta1 )
-c1 = fits.Column(name="x",format='D', array=x )
-c2 = fits.Column(name="y",format='D', array=y )
-c3 = fits.Column(name="z",format='D', array=z )
-c4 = fits.Column(name="vx",format='D', array=vx )
-c5 = fits.Column(name="vy",format='D', array=vy )
-c6 = fits.Column(name="vz",format='D', array=vz )
-c7 = fits.Column(name="Vmax",format='D', array=Vmax )
-c8 = fits.Column(name="Vpeak",format='D', array=Vpeak )
-c9 = fits.Column(name="Mvir",format='D', array=Mvir )
-c10 = fits.Column(name="parent_id",format='D', array=parent_id )
-c11 = fits.Column(name="snap_id",format='D', array=snap_id )
-c12 = fits.Column(name="kind",format='D', array=kind )
-c13 = fits.Column(name="z_dis",format='D', array=z_dis )
 
-cols = fits.ColDefs([c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c0, c01 ])
-hdu = fits.BinTableHDU.from_columns(cols)
-hdu.writeto(path_to_outputCat)
 
+ok = (delta > 0)
+lgDF = n.log10(delta[ok])
+
+0.9830 has a 0 density field value ?
+
+n.median(lgDF), n.mean(lgDF ), n.min(lgDF), n.max(lgDF)
 
 
 87 0.403200
