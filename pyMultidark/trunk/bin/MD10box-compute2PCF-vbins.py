6c64741cf85ff6cab26c3896585de2b56334961b
JohanComparat <johan.comparat@gmail.com>
2016-04-13 10:07:28 +0200

    s

--- pyMultidark/trunk/bin/MD10box-compute2PCF-vbins.py
+++ pyMultidark/trunk/bin/MD10box-compute2PCF-vbins.py
@@ -6,5 +6,6 @@ all=n.array([ 1.00000, 0.50320, 0.89510, 0.74980 ])
 for ii in range(len(all)):
         a= all[ii]
         ll = n.array( glob.glob( "/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc_new_rockS/snapshots/*"+str(a)+"*.fits" ) )
-	box.compute2PCF(ll, rmax=140, Nmax=4000000, vmin=65, dr = 2., name="rmax_140")
-	box.compute2PCF(ll, rmax=15, Nmax=4000000, vmin=65, dr = 0.1, name="rmax_015")
+	#box.compute2PCF(ll, rmax=140, Nmax=4000000, vmin=65, dr = 2., name="rmax_140")
+	#box.compute2PCF(ll, rmax=15, Nmax=4000000, vmin=65, dr = 0.1, name="rmax_015")
+	box.compute2PCF(ll, rmax=50, Nmax=4000000, vmin=65, dr = 1., name="rmax_050")
