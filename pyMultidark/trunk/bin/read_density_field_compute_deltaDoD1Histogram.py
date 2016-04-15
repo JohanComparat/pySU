8a14ccdd5e8b9666a40467fb5675090a2c52539a
JohanComparat <johan.comparat@gmail.com>
2016-04-15 15:10:51 +0200

    s

--- pyMultidark/trunk/bin/read_density_field_compute_deltaDoD1Histogram.py
+++ pyMultidark/trunk/bin/read_density_field_compute_deltaDoD1Histogram.py
@@ -11,7 +11,7 @@ DFdir = join("/data2", "users", "gustavo", "BigMD", "1Gpc_3840_Planck1_New", "DE
 mockDir = "/data1/DATA/eBOSS/Multidark-box-mocks/parts/"
 
 inFiles = n.array(["dmdens_cic_104.dat", "dmdens_cic_101.dat", "dmdens_cic_097.dat", "dmdens_cic_087.dat"])
-bins = n.hstack((0,n.logspace(-3, 4, 10000)))
+bins = n.hstack((0,n.logspace(-3, 4, 1000)))
 
 for infi in inFiles:
 	DFfile = join(DFdir,infi)
