230f98c59ae72fb61bcee91e5e9a018eae32ed97
JohanComparat <johan.comparat@gmail.com>
2016-04-28 15:37:29 +0200

    d

--- pyGalaxy/trunk/bin/compute_TSR_VVDS.py
+++ pyGalaxy/trunk/bin/compute_TSR_VVDS.py
@@ -50,90 +50,93 @@ def createMask(path_to_mask):
 fields = [02, 10, 14, 22] 
 magLim =[24., 22.5, 22.5, 22.5]
 summaryCat = ["VVDS_DEEP_summary.LFcatalog.Planck15.fits", "VVDS_WIDE_summary.LFcatalog.Planck15.fits", "VVDS_WIDE_summary.LFcatalog.Planck15.fits", "VVDS_WIDE_summary.LFcatalog.Planck15.fits"]
-summaryCatOut = ["VVDS_DEEP_summary.LFcatalog.Planck15.v2.fits", "VVDS_WIDE_summary.LFcatalog.Planck15.v2.fits", "VVDS_WIDE_summary.LFcatalog.Planck15.v2.fits", "VVDS_WIDE_summary.LFcatalog.Planck15.v2.fits"]
+summaryCatOut = ["VVDS_DEEP_summary.LFcatalog.Planck15.v2.fits", "VVDS_WIDE_F10_summary.LFcatalog.Planck15.v2.fits", "VVDS_WIDE_F14_summary.LFcatalog.Planck15.v2.fits", "VVDS_WIDE_F22_summary.LFcatalog.Planck15.v2.fits"]
 depth = ["DEEP", "WIDE", "WIDE", "WIDE"]
 dMag = 0.5
 
 ii = 0
+for ii in range(len(fields)):
+	print fields[ii]
+	# loads the catalog for the field
+	# spectroscopy + mask
+	survey = GalaxySurveyVVDS( redshift_catalog = summaryCat[ii] )
+	survey.vvds_photo_dir = join(os.environ['VVDS_DIR'],'photometry')
+	field = (survey.catalog['NUM']/1e7).astype(int)
+	allSpec = (field==fields[ii])
+	speccat = survey.catalog[allSpec]
+	path_to_spec_mask = join(survey.vvds_catalog_dir, "F"+str(fields[ii]).zfill(2)+"_specmask.reg")
+	Nspec_total = len(speccat)
 
-# loads the catalog for the field
-# spectroscopy + mask
-survey = GalaxySurveyVVDS( redshift_catalog = summaryCat[ii] )
-survey.vvds_photo_dir = join(os.environ['VVDS_DIR'],'photometry')
-field = (survey.catalog['NUM']/1e7).astype(int)
-allSpec = (field==fields[ii])
-speccat = survey.catalog[allSpec]
-path_to_spec_mask = join(survey.vvds_catalog_dir, "F"+str(fields[ii]).zfill(2)+"_specmask.reg")
-Nspec_total = len(speccat)
+	TSR = n.zeros_like(speccat['ALPHA'])
+	TSR_ERR = n.zeros_like(speccat['ALPHA'])
 
-TSR = n.zeros_like(speccat['ALPHA'])
-TSR_ERR = n.zeros_like(speccat['ALPHA'])
+	# photometry
+	# catalog of galaxies + mask with the VVDS selection applied
+	path_to_photo_Cat = join(survey.vvds_photo_dir, "cesam_vvds_photoF"+str(fields[ii]).zfill(2)+"_"+depth[ii]+".fits")
+	photocat_all = fits.open(path_to_photo_Cat)[1].data
+	selection=(photocat_all['MAGI_CFH12K']<magLim[ii])&(photocat_all['MAGI_CFH12K']>17.5)
+	photocat = photocat_all[selection]
+	path_to_photo_mask = join(survey.vvds_photo_dir, "F"+str(fields[ii]).zfill(2)+"_photmask.reg")
 
-# photometry
-# catalog of galaxies + mask with the VVDS selection applied
-path_to_photo_Cat = join(survey.vvds_photo_dir, "cesam_vvds_photoF"+str(fields[ii]).zfill(2)+"_"+depth[ii]+".fits")
-photocat_all = fits.open(path_to_photo_Cat)[1].data
-selection=(photocat_all['MAGI_CFH12K']<magLim[ii])&(photocat_all['MAGI_CFH12K']>17.5)
-photocat = photocat_all[selection]
-path_to_photo_mask = join(survey.vvds_photo_dir, "F"+str(fields[ii]).zfill(2)+"_photmask.reg")
+	pixelSelection=(raPixAll < n.max(photocat_all['ALPHA']) + 1 ) & (raPixAll > n.min(photocat_all['ALPHA']) - 1 ) & (decPixAll < n.max(photocat_all['DELTA']) + 1 ) & (decPixAll > n.min(photocat_all['DELTA']) -1 ) 
+	decPix = decPixAll[pixelSelection]
+	raPix = raPixAll[pixelSelection]
 
-pixelSelection=(raPixAll < n.max(photocat_all['ALPHA']) + 1 ) & (raPixAll > n.min(photocat_all['ALPHA']) - 1 ) & (decPixAll < n.max(photocat_all['DELTA']) + 1 ) & (decPixAll > n.min(photocat_all['DELTA']) -1 ) 
-decPix = decPixAll[pixelSelection]
-raPix = raPixAll[pixelSelection]
+	# creates random arrays
+	randRA_all = n.random.uniform(n.min(speccat['ALPHA']) - .1, n.max(speccat['ALPHA']) + .1, Nspec_total*100)
+	randDEC_all = n.random.uniform(n.min(speccat['DELTA']) - .1, n.max(speccat['DELTA']) + .1, Nspec_total*100)
+	area_all = (n.max(speccat['ALPHA']) + .1 - n.min(speccat['ALPHA']) + .1)*(n.max(speccat['DELTA']) + .1 - n.min(speccat['DELTA']) + .1)
 
-# creates random arrays
-randRA_all = n.random.uniform(n.min(speccat['ALPHA']) - .1, n.max(speccat['ALPHA']) + .1, Nspec_total*100)
-randDEC_all = n.random.uniform(n.min(speccat['DELTA']) - .1, n.max(speccat['DELTA']) + .1, Nspec_total*100)
-area_all = (n.max(speccat['ALPHA']) + .1 - n.min(speccat['ALPHA']) + .1)*(n.max(speccat['DELTA']) + .1 - n.min(speccat['DELTA']) + .1)
+	# loads the actual co-added photo NOT USED SO FAR
+	path_to_photo_Iband = join(survey.vvds_photo_dir, "F"+str(fields[ii]).zfill(2)+"i.fits")
+	fitsfile = fits.open(path_to_photo_Iband)
+	iband_header = fitsfile[0].header
+	iband_data = fitsfile[0].data
 
-# loads the actual co-added photo NOT USED SO FAR
-path_to_photo_Iband = join(survey.vvds_photo_dir, "F"+str(fields[ii]).zfill(2)+"i.fits")
-fitsfile = fits.open(path_to_photo_Iband)
-iband_header = fitsfile[0].header
-iband_data = fitsfile[0].data
+	# creates the masks
+	spec_mask_filter, spec_mask_polygon = createMask(path_to_spec_mask)
+	#photo_mask_filter, photo_mask_polygon = createMask(path_to_photo_mask)
 
-# creates the masks
-spec_mask_filter, spec_mask_polygon = createMask(path_to_spec_mask)
-#photo_mask_filter, photo_mask_polygon = createMask(path_to_photo_mask)
+	print "N in mask:", len(speccat['ALPHA'][spec_mask_filter.inside(n.transpose([speccat['ALPHA'], speccat['DELTA']]))]), ", N in cat: ", len(speccat['ALPHA'])
 
-print "N in mask:", len(speccat['ALPHA'][spec_mask_filter.inside(n.transpose([speccat['ALPHA'], speccat['DELTA']]))]), ", N in cat: ", len(speccat['ALPHA'])
+	bins = n.hstack((17.5, n.arange(18.5, magLim[ii]+dMag, dMag) ))
 
-bins = n.hstack((17.5, n.arange(18.5, magLim[ii]+dMag, dMag) ))
+	areaIn = spec_mask_filter.inside(n.transpose([raPix, decPix]))
+	NPIX = len(areaIn.nonzero()[0])
+	areaEffective = NPIX * areaPerPixel
+	print "effective area = ", areaEffective, "square degrees"
+	specIn = spec_mask_filter.inside(n.transpose([speccat['ALPHA'], speccat['DELTA']]))
+	ND = len(specIn.nonzero()[0])
+	photIn = spec_mask_filter.inside(n.transpose([photocat['ALPHA'], photocat['DELTA']]))
+	NR = len(photIn.nonzero()[0])
+	randIn = spec_mask_filter.inside(n.transpose([randRA_all, randDEC_all]))
+	#downSelectRD = (n.random.uniform(0,1,len(randRA_all[randIn]))< float(ND)/float(NR) )
 
-areaIn = spec_mask_filter.inside(n.transpose([raPix, decPix]))
-NPIX = len(areaIn.nonzero()[0])
-areaEffective = NPIX * areaPerPixel
-print "effective area = ", areaEffective, "square degrees"
-specIn = spec_mask_filter.inside(n.transpose([speccat['ALPHA'], speccat['DELTA']]))
-ND = len(specIn.nonzero()[0])
-photIn = spec_mask_filter.inside(n.transpose([photocat['ALPHA'], photocat['DELTA']]))
-NR = len(photIn.nonzero()[0])
-randIn = spec_mask_filter.inside(n.transpose([randRA_all, randDEC_all]))
-downSelectRD = (n.random.uniform(0,1,len(randRA_all[randIn]))< float(ND)/float(NR) )
+	# print ND, NR
+	NDpb = n.histogram(speccat['MAGI'][specIn], bins=bins)[0]
+	NRpb = n.histogram(photocat['MAGI_CFH12K'][photIn], bins=bins)[0]
+	# piecewise interpolation of the TSR :
+	tsr_eval = lambda x : n.piecewise(x, n.array([ (x > bins[kk])&(x<bins[kk+1]) for kk in range(len(NDpb)) ]), NDpb.astype(float)/NRpb)
+	tsr_err_eval = lambda x : n.piecewise(x, n.array([ (x > bins[kk])&(x<bins[kk+1]) for kk in range(len(NDpb)) ]), NDpb**(-0.5) * NDpb.astype(float)/NRpb)
 
-# print ND, NR
-NDpb = n.histogram(speccat['MAGI'][specIn], bins=bins)[0]
-NRpb = n.histogram(photocat['MAGI_CFH12K'][photIn], bins=bins)[0]
-# piecewise interpolation of the TSR :
-tsr_eval = lambda x : n.piecewise(x, n.array([ (x > bins[kk])&(x<bins[kk+1]) for kk in range(len(NDpb)) ]), NDpb.astype(float)/NRpb)
-tsr_err_eval = lambda x : n.piecewise(x, n.array([ (x > bins[kk])&(x<bins[kk+1]) for kk in range(len(NDpb)) ]), NDpb**(-0.5) * NDpb.astype(float)/NRpb)
+	TSR[specIn] = tsr_eval(speccat['MAGI'][specIn])
+	TSR_ERR[specIn] = tsr_err_eval(speccat['MAGI'][specIn])
 
-TSR[specIn] = tsr_eval(speccat['MAGI'][specIn])
-TSR_ERR[specIn] = tsr_err_eval(speccat['MAGI'][specIn])
+	# writes the new catalog
+	c0 = fits.Column(name="TSR_new",format="D", array= TSR )
+	c1 = fits.Column(name="TSR_ERR_new",format="D", array= TSR_ERR )
+	new_columns = survey.catalog.columns + c0 + c1
+	hdu = fits.BinTableHDU.from_columns(new_columns)
+	os.system("rm -rf "+join(os.environ['VVDS_DIR'], 'catalogs', summaryCatOut[ii]) )
+	hdu.writeto(join(os.environ['VVDS_DIR'], 'catalogs', summaryCatOut[ii]))
 
-c0 = fits.Column(name="TSR_new",format="D", array= TSR )
-c1 = fits.Column(name="TSR_ERR_new",format="D", array= TSR_ERR )
-
-new_columns = survey.catalog.columns + c0 + c1
-
-hdu = fits.BinTableHDU.from_columns(new_columns)
-hdu.writeto(join(os.environ['VVDS_DIR'], 'catalogs', summaryCatOut[ii]))
-
-col0 = fits.Column(name="RA",format='D', array= randRA_all[randIn][downSelectRD])
-col1 = fits.Column(name="DEC",format='D', array= randDEC_all[randIn][downSelectRD])
-fullSpec_cols  = fits.ColDefs([col0, col1])
-fullSpec_tb_hdu = fits.BinTableHDU.from_columns(fullSpec_cols)
-fullSpec_tb_hdu.writeto(join(os.environ['VVDS_DIR'], 'catalogs', summaryCatOut[ii][:-5]+"random.fits"))
+	# writes the randoms 
+	col0 = fits.Column(name="RA",format='D', array= randRA_all[randIn]) #[downSelectRD])
+	col1 = fits.Column(name="DEC",format='D', array= randDEC_all[randIn]) #[downSelectRD])
+	fullSpec_cols  = fits.ColDefs([col0, col1])
+	fullSpec_tb_hdu = fits.BinTableHDU.from_columns(fullSpec_cols)
+	os.system("rm -rf "+join(os.environ['VVDS_DIR'], 'catalogs', summaryCatOut[ii][:-5]+".random.fits") )
+	fullSpec_tb_hdu.writeto(join(os.environ['VVDS_DIR'], 'catalogs', summaryCatOut[ii][:-5]+".random.fits") )
 
 """
 Eventually toimplement per small units of mask, but beware of overlaps between masks
@@ -172,16 +175,12 @@ for jj, mask in enumerate(spec_mask_filter):
 randRA = n.hstack(( randRA ))
 randDEC = n.hstack(( randDEC ))
 areaEffective = nInReg.T[2].sum()
-"""
-
 	
 for kk in range(len(tsr_A)):
 	p.plot((bins[1:]+bins[:-1])/2., tsr_A[kk])
 
 p.savefig(join(os.environ['DATA_DIR'],'indev','tsr'+str(fields[ii])+".png"))
 
-
-"""
 p.plot(photocat['ALPHA'][photIn], photocat['DELTA'][photIn], 'b,')
 p.plot(speccat['ALPHA'][specIn], speccat['DELTA'][specIn], 'r,')
 p.savefig('ra-dec-2'+str(fields[ii])+".png")
