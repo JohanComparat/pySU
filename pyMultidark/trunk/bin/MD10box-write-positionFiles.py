807b68adf610487efb9a62d3df6341e9252096e1
JohanComparat <johan.comparat@gmail.com>
2016-07-04 15:37:35 +0200

    sd

--- pyMultidark/trunk/bin/MD10box-write-positionFiles.py
+++ pyMultidark/trunk/bin/MD10box-write-positionFiles.py
@@ -1,6 +1,8 @@
 from MultiDark import *
 
-snList= n.array([ "/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc_new_rockS/snapshots/hlist_1.00000.list" , 
+snList= n.array([ "/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc_new_rockS/snapshots/hlist_0.50320.list"])
+"""
+"/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc_new_rockS/snapshots/hlist_1.00000.list" , 
 "/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc_new_rockS/snapshots/hlist_0.89510.list",
 "/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc_new_rockS/snapshots/hlist_0.74980.list", 
 "/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc_new_rockS/snapshots/hlist_0.60080.list", 
@@ -10,7 +12,8 @@ snList= n.array([ "/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc_new_rockS/snap
 "/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc_new_rockS/snapshots/hlist_0.36090.list", 
 "/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc_new_rockS/snapshots/hlist_0.33030.list", 
 "/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc_new_rockS/snapshots/hlist_0.25320.list"
-]) #"/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc_new_rockS/snapshots/hlist_0.50320.list",
+]) 
+"""
 
 box = MultiDarkSimulation(Lbox=1000.0 * uu.Mpc, boxDir = "MD_1Gpc_new_rockS",snl =snList   ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
 
