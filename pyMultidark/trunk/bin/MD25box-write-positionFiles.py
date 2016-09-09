da27478a880d01055fb602a5a05710f0ae331bef
JohanComparat <johan.comparat@gmail.com>
2016-09-09 16:36:34 +0200

    s

--- pyMultidark/trunk/bin/MD25box-write-positionFiles.py
+++ pyMultidark/trunk/bin/MD25box-write-positionFiles.py
@@ -1,12 +1,13 @@
 from MultiDark import *
-
-snList =  n.array(["/data2/DATA/eBOSS/Multidark-lightcones/MD_2.5Gpc/snapshots/hlist_10.list",
+"""
+"/data2/DATA/eBOSS/Multidark-lightcones/MD_2.5Gpc/snapshots/hlist_10.list",
 "/data2/DATA/eBOSS/Multidark-lightcones/MD_2.5Gpc/snapshots/hlist_74.list",  
 "/data2/DATA/eBOSS/Multidark-lightcones/MD_2.5Gpc/snapshots/hlist_7.list",  
 "/data2/DATA/eBOSS/Multidark-lightcones/MD_2.5Gpc/snapshots/hlist_9.list",
 "/data2/DATA/eBOSS/Multidark-lightcones/MD_2.5Gpc/snapshots/hlist_11.list",
 "/data2/DATA/eBOSS/Multidark-lightcones/MD_2.5Gpc/snapshots/hlist_22.list",
-"/data2/DATA/eBOSS/Multidark-lightcones/MD_2.5Gpc/snapshots/hlist_46.list",
+"""
+snList =  n.array(["/data2/DATA/eBOSS/Multidark-lightcones/MD_2.5Gpc/snapshots/hlist_46.list",
 "/data2/DATA/eBOSS/Multidark-lightcones/MD_2.5Gpc/snapshots/hlist_80.list" ])
 box = MultiDarkSimulation(Lbox=2500.0 * uu.Mpc, boxDir = "MD_2.5Gpc",snl = snList ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
 
--- pyMultidark/trunk/bin/MD40box-write-positionFiles.py
+++ pyMultidark/trunk/bin/MD40box-write-positionFiles.py
@@ -1,5 +1,13 @@
 from MultiDark import *
-snList =  n.array(glob.glob(join("/data2/DATA/eBOSS/Multidark-lightcones" , "MD_4Gpc", "snapshots" , "hlist_*.list")))
+
+
+snList =  n.array(["/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/hlist_66.list", 
+"/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/hlist_79.list", 
+"/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/hlist_82.list", 
+"/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/hlist_87.list", 
+"/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/hlist_91.list", 
+"/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/hlist_97.list"])
+
 box = MultiDarkSimulation(Lbox=4000.0 * uu.Mpc, boxDir = "MD_4Gpc",snl = snList ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
 
 for ii in n.arange(len(box.snl)):
