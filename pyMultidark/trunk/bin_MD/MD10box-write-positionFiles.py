5d2b4df4672028d750a10437ebeaf98f6bbdb7e3
JohanComparat <johan.comparat@gmail.com>
2016-09-24 09:16:22 +0200

    s

--- pyMultidark/trunk/bin_MD/MD10box-write-positionFiles.py
+++ pyMultidark/trunk/bin_MD/MD10box-write-positionFiles.py
@@ -1,11 +1,18 @@
 from MultiDark import *
+import sys
 
-#"/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc/snapshots/hlist_0.25320.list",  
-#"/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc/snapshots/hlist_0.36090.list",  
-#"/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc/snapshots/hlist_0.44060.list",  
-#"/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc/snapshots/hlist_0.49220.list",  
-#"/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc/snapshots/hlist_0.60080.list",  
+snList= n.array([ "/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc/snapshots/hlist_0.25320.list",  
+"/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc/snapshots/hlist_0.36090.list",  
+"/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc/snapshots/hlist_0.44060.list",  
+"/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc/snapshots/hlist_0.49220.list",  
+"/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc/snapshots/hlist_0.60080.list"]) 
 
+box = MultiDarkSimulation(Lbox=1000.0 * uu.Mpc, boxDir = "MD_1Gpc",snl =snList   ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
+
+for ii in n.arange(len(box.snl)):
+	box.writePositionCatalogPM(ii,  vmin=50, mmin=2*10**10)
+
+sys.exit()
 snList= n.array([ 
 "/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc/snapshots/hlist_1.00000.list", 
 "/data2/DATA/eBOSS/Multidark-lightcones/MD_1Gpc/snapshots/hlist_0.89510.list",
--- pyMultidark/trunk/bin_MD/mvir-MD10box-JK.py
+++ pyMultidark/trunk/bin_MD/mvir-MD10box-JK.py
@@ -1,5 +1,5 @@
 from MultiDark import *
-box = MultiDarkSimulation(Lbox=1000.0 * uu.Mpc,wdir="/data2/DATA/eBOSS/Multidark-lightcones", boxDir = "MD_1Gpc_new_rockS",snl =  n.array(glob.glob(join("/data2/DATA/eBOSS/Multidark-lightcones" , "MD_1Gpc_new_rockS", "snapshots", "hlist_*.list"))) ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc),Melement = 1510000000.0)
+box = MultiDarkSimulation(Lbox=1000.0 * uu.Mpc,wdir="/data2/DATA/eBOSS/Multidark-lightcones", boxDir = "MD_1Gpc",snl =  n.array(glob.glob(join("/data2/DATA/eBOSS/Multidark-lightcones" , "MD_1Gpc", "snapshots", "hlist_*.list"))) ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc),Melement = 1510000000.0)
 
 
 for ii in n.arange(len(box.snl)):
