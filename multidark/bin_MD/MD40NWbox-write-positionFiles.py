c93f957018b5cde97c9ce0d8975f2e62ca939023
JohanComparat <johan.comparat@gmail.com>
2016-10-05 17:56:38 +0200

    d

--- multidark/bin_MD/MD40NWbox-write-positionFiles.py
+++ multidark/bin_MD/MD40NWbox-write-positionFiles.py
@@ -1,22 +1,6 @@
 from MultiDark import *
 
-snList=  n.array([
-"/home2/jcomparat/eBOSS-LC/Multidark-lightcones/MD_4GpcNW/snapshots/out_1.list",
-"/home2/jcomparat/eBOSS-LC/Multidark-lightcones/MD_4GpcNW/snapshots/out_2.list", 
-"/home2/jcomparat/eBOSS-LC/Multidark-lightcones/MD_4GpcNW/snapshots/out_3.list", 
-"/home2/jcomparat/eBOSS-LC/Multidark-lightcones/MD_4GpcNW/snapshots/out_4.list", 
-"/home2/jcomparat/eBOSS-LC/Multidark-lightcones/MD_4GpcNW/snapshots/out_5.list",
-"/home2/jcomparat/eBOSS-LC/Multidark-lightcones/MD_4GpcNW/snapshots/out_6.list"])
-
-snList=  n.array(["/home2/jcomparat/eBOSS-LC/Multidark-lightcones/MD_4GpcNW/snapshots/out_7.list", 
-"/home2/jcomparat/eBOSS-LC/Multidark-lightcones/MD_4GpcNW/snapshots/out_8.list", 
-"/home2/jcomparat/eBOSS-LC/Multidark-lightcones/MD_4GpcNW/snapshots/out_9.list",
-"/home2/jcomparat/eBOSS-LC/Multidark-lightcones/MD_4GpcNW/snapshots/out_10.list"])
-
-snList=  n.array(["/home2/jcomparat/eBOSS-LC/Multidark-lightcones/MD_4GpcNW/snapshots/out_11.list","/home2/jcomparat/eBOSS-LC/Multidark-lightcones/MD_4GpcNW/snapshots/out_12.list", 
-"/home2/jcomparat/eBOSS-LC/Multidark-lightcones/MD_4GpcNW/snapshots/out_13.list", 
-"/home2/jcomparat/eBOSS-LC/Multidark-lightcones/MD_4GpcNW/snapshots/out_14.list", 
-"/home2/jcomparat/eBOSS-LC/Multidark-lightcones/MD_4GpcNW/snapshots/out_15.list"])
+snList=  n.array(["/home2/jcomparat/eBOSS-LC/Multidark-lightcones/MD_4GpcNW/snapshots/out_*.list"])
 
 box = MultiDarkSimulation(Lbox=4000.0 * uu.Mpc, boxDir = "MD_4GpcNW",snl = snList ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
 
