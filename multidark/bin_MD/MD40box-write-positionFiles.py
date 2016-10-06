7e5e2658e362dddbb76bc7ff4ae7d4dc9e31a569
JohanComparat <johan.comparat@gmail.com>
2016-10-06 16:17:24 +0200

    a

--- multidark/bin_MD/MD40box-write-positionFiles.py
+++ multidark/bin_MD/MD40box-write-positionFiles.py
@@ -1,25 +1,41 @@
 from MultiDark import *
 
 
-snList =  n.array(["/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/hlist_128.list",
-"/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/hlist_66.list", 
-"/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/hlist_79.list", 
-"/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/hlist_82.list", 
-"/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/hlist_87.list", 
-"/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/hlist_91.list", 
-"/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/hlist_97.list",
-"/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/hlist_105.list", 
-"/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/hlist_112.list", 
-"/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/hlist_124.list"])
-
-snList =  n.array(["/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/out_108.list",
-"/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/out_115.list"])
+snList =  n.array(glob.glob("/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/out_4?.list"))
 
 box = MultiDarkSimulation(Lbox=4000.0 * uu.Mpc, boxDir = "MD_4Gpc",snl = snList ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
 
+for ii in n.arange(len(box.snl)):
+	box.writePositionCatalogPM(ii, vmin = 200., mmin=9*10**11)
 
-box.columnDict = {'id': 0, 'desc_id': 1, 'mvir': 2, 'vmax': 3, 'vrms': 4, 'rvir': 5, 'rs': 6, 'Np': 7, 'x': 8, 'y': 9, 'z': 10, 'vx': 11, 'vy': 12, 'vz': 13, 'Jx': 14, 'Jy': 15, 'Jz': 16, 'Spin':17, 'Rs_Klypin': 18, 'Mmvir_all': 19, 'M200b': 20, 'M200c': 21, 'M500c': 22, 'M2500c': 23, 'Xoff': 24, 'Voff': 25, 'Spin_Bullock': 26, 'b_to_a': 27, 'c_to_a': 28, 'Ax': 29, 'Ay': 30, 'Az': 31, 'b_to_a_500c': 32, 'c_to_a_500c': 33, 'Ax_500c': 34, 'Ay_500c': 35, 'Az_500c': 36, 'TU': 37, 'M_pe_Behroozi': 38, 'M_pe_Diemer': 39, 'pid': 40}
 
+snList =  n.array(glob.glob("/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/out_5?.list"))
+
+box = MultiDarkSimulation(Lbox=4000.0 * uu.Mpc, boxDir = "MD_4Gpc",snl = snList ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
+
+for ii in n.arange(len(box.snl)):
+	box.writePositionCatalogPM(ii, vmin = 200., mmin=9*10**11)
+
+
+snList =  n.array(glob.glob("/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/out_6?.list"))
+
+box = MultiDarkSimulation(Lbox=4000.0 * uu.Mpc, boxDir = "MD_4Gpc",snl = snList ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
+
+for ii in n.arange(len(box.snl)):
+	box.writePositionCatalogPM(ii, vmin = 200., mmin=9*10**11)
+
+
+snList =  n.array(glob.glob("/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/out_7?.list"))
+
+box = MultiDarkSimulation(Lbox=4000.0 * uu.Mpc, boxDir = "MD_4Gpc",snl = snList ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
+
+for ii in n.arange(len(box.snl)):
+	box.writePositionCatalogPM(ii, vmin = 200., mmin=9*10**11)
+
+
+snList =  n.array(glob.glob("/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/out_8?.list"))
+
+box = MultiDarkSimulation(Lbox=4000.0 * uu.Mpc, boxDir = "MD_4Gpc",snl = snList ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
 
 for ii in n.arange(len(box.snl)):
 	box.writePositionCatalogPM(ii, vmin = 200., mmin=9*10**11)
