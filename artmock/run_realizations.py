35978a324ce1042088f0c332ab8a5c6f989ae391
JohanComparat <johan.comparat@gmail.com>
2016-10-31 17:28:18 +0100

    d

--- artmock/run_realizations.py
+++ artmock/run_realizations.py
@@ -1,13 +1,11 @@
 import os
+from os.path import join
 
 # define params
-def runPMP(LBOX = "650.000", NROW = "1612", NGRID = "1612", DTHR = "120.000", NORM = "0.0001", nrm= "N1em4", SLOPE= "-1.000", slp = "am100", DMAX = "240.000", RR=1):
+def runPMP(LBOX = "650.000", NROW = "1612", NGRID = "1612", DTHR = "120.000", NORM = "0.0001", nrm= "N1em4", SLOPE= "-1.000", slp = "am100", DMAX = "240.000", RR=1, start=0, topdir = os.environ['PM_DIR']):
 	# create outdir
-	topdir = "/data2/DATA/eBOSS/PM_runs/PM_C"
-	os.chdir(topdir)
-	outdir = "PM_Nr"+NROW+"_L"+LBOX+"_g"+NGRID+"_Dgt"+DTHR[:4]+"_"+slp+"_Dlt"+DMAX[:4] +"_"+nrm
-	os.mkdir(outdir)
-
+	
+	os.chdir(os.environ['ARTMOCK_DIR'])
 	os.remove("Init.dat")
 
 	f=open("Init.dat", 'w')
@@ -19,7 +17,7 @@ def runPMP(LBOX = "650.000", NROW = "1612", NGRID = "1612", DTHR = "120.000", NO
 	f.write("""step da  =  4.0000E-04"""  + "\n")
 	f.write("""z_final  =     0.700"""  + "\n")
 	f.write("""#outputs =         8"""  + "\n")
-	f.write("""2.00 1.70 1.40 1.20 1.10 1.00 0.9 0.8 """  + "\n")
+	f.write("""2.00 1.70 1.40 1.20 1.10 1.00 0.9 0.8 0.1 0.0"""  + "\n")
 	f.write("""dens_thr =   """ + DTHR + "\n")
 	f.write("""Vrms     =     0.000"""  + "\n")
 	f.write("""#Params  =        10"""  + "\n")
@@ -42,15 +40,17 @@ def runPMP(LBOX = "650.000", NROW = "1612", NGRID = "1612", DTHR = "120.000", NO
 	os.system("make PMPanalysis")
 	os.system("./PMP2init.exe")
 
+	outdir = join(topdir, "PM_Nr"+NROW+"_L"+LBOX+"_g"+NGRID+"_Dgt"+DTHR[:4]+"_"+slp+"_Dlt"+DMAX[:4] +"_"+nrm)
+	os.mkdir(outdir)
 	os.chdir(outdir)
-	for ii in range(RR):
-                os.system("touch begin"+str(ii+1))
-                os.system("echo "+str(ii+1)+" | ../PMP2start.exe")
-                os.system("echo 1000000 | ../PMP2main.exe")
-                os.system("rm -rf PMcr*.DAT")
-                os.system("touch fin"+str(ii+1))
+	
+	for ii in range(start, start+RR, 1):
+		os.system("touch begin"+str(ii) )
+		os.system("echo " + str(ii) + " | " + join( os.environ['ARTMOCK_DIR'], "PMP2start.exe") )
+		os.system("echo 1000000 | " + join( os.environ['ARTMOCK_DIR'], "PMP2main.exe") )
+		os.system("rm -rf PMcr*.DAT" )
+		
 
-
-runPMP(LBOX="1040.000", NROW="1300", NGRID="2600", DTHR="10.000", NORM="0.0003", nrm= "N3em4", SLOPE= "0.110", slp="ap011", DMAX="400.000", RR = 100)
+runPMP(LBOX="1040.000", NROW="1300", NGRID="2600", DTHR="10.000", NORM="0.0003", nrm= "N3em4", SLOPE= "0.110", slp="ap011", DMAX="400.000", RR = 10, start=300)
 
 
