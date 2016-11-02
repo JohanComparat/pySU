bfd95a9a3d71537a8fdc14a8761f8affa8193914
JohanComparat <johan.comparat@gmail.com>
2016-11-02 11:37:02 +0100

    s

--- spm/bin/write_run_scripts.py
+++ spm/bin/write_run_scripts.py
@@ -1,28 +1,27 @@
 import os
-plate = "7645"
-rootName = join(os.environ['HOME'], "batchscripts_firefly", plate)
+from os.path import join
+import numpy as n
+def writeScript(rootName, plate):
+	f=open(rootName+".sh",'w')
+	f.write("#!/bin/bash \n")
+	f.write("#PBS -l walltime=48:00:00 \n")
+	f.write("#PBS -o "+plate+".o.$PBS_JOBID \n")
+	f.write("#PBS -e "+plate+".e$PBS_JOBID \n")
+	f.write("#PBS -M johan.comparat@gmail.com \n")
+	f.write("module load apps/anaconda/2.4.1 \n")
+	f.write("module load apps/python/2.7.8/gcc-4.4.7 \n")
+	f.write("export PYTHONPATH=$PYTHONPATH:/users/comparat/pySU/galaxy/python/ \\n")
+	f.write("export PYTHONPATH=$PYTHONPATH:/users/comparat/pySU/simulations/python/ \n")
+	f.write("export PYTHONPATH=$PYTHONPATH:/users/comparat/pySU/multidark/python/ \n")
+	f.write("export PYTHONPATH=$PYTHONPATH:/users/comparat/pySU/spm/python/ \n")
+	f.write("export PYTHONPATH=$PYTHONPATH:/users/comparat/pySU/targetselection/python/ \n")
+	f.write(" \n")
+	f.write("cd /users/comparat/pySU/spm/bin \n")
+	f.write("python stellarpop_sdss_single "+plate+" \n")
+	f.write(" \n")
+	f.close()
 
-f=open(rootName+".sh",'w')
-
-str2write = """
-#!/bin/bash 
-#PBS -l walltime=48:00:00 
-#PBS -o """+plate+""".o.$PBS_JOBID 
-#PBS -e """+plate+""".e$PBS_JOBID 
-#PBS -M johan.comparat@gmail.com 
-
-# Configure modules and load modules 
-module load apps/anaconda/2.4.1
-module load apps/python/2.7.8/gcc-4.4.7
-
-export PYTHONPATH=$PYTHONPATH:/users/comparat/pySU/galaxy/python/
-export PYTHONPATH=$PYTHONPATH:/users/comparat/pySU/simulations/python/
-export PYTHONPATH=$PYTHONPATH:/users/comparat/pySU/multidark/python/
-export PYTHONPATH=$PYTHONPATH:/users/comparat/pySU/spm/python/
-export PYTHONPATH=$PYTHONPATH:/users/comparat/pySU/targetselection/python/
-
-cd /users/comparat/pySU/spm/bin
-python stellarpop_sdss_single """+plate
-
-f.write(str2write)
-f.close()
+plates = n.loadtxt( join(os.environ['SDSSDR12_DIR'], "plateNumberList"), unpack=True, dtype='str')
+for plate in plates:
+	rootName = join(os.environ['HOME'], "batchscripts_firefly", plate)
+	writeScript(rootName, plate)
