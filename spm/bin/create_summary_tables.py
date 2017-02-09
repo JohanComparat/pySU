78b37afd0e9cca9be377617944b4468e2f9d6ce0
JohanComparat <johan.comparat@gmail.com>
2017-02-10 00:17:27 +0100

    salpeter

--- spm/bin/create_summary_tables.py
+++ spm/bin/create_summary_tables.py
@@ -8,7 +8,7 @@ import time
 import astropy.io.fits as fits
 
 plate = sys.argv[1]
-dir ='stellarpop-m11-kroupa'
+dir ='stellarpop-m11-salpeter'
 
 hdus = fits.open( join( os.environ['SDSSDR12_DIR'], "catalogs", "specObj-dr12.fits") )
 
