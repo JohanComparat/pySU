a41e77534bae07419ff31b32f30884edda008f23
JohanComparat <johan.comparat@gmail.com>
2017-03-13 07:36:36 +0100

    no dust run

--- spm/bin/run_summary_tables.py
+++ spm/bin/run_summary_tables.py
@@ -8,16 +8,21 @@ plates = n.loadtxt( join(os.environ['SDSSDR12_DIR'], "catalogs", "plateNumberLis
 tts=[]
 for plate in plates[10:]:
 	t0 = time.time()
-	os.system("python test.py "+plate+" stellarpop-m11-kroupa")
+	os.system("python create_summary_tables_sdss_dr12.py "+plate+" stellarpop-m11-kroupa")
 	tts.append(time.time()-t0)
 	print tts[-1], "seconds"
+
+for plate in plates:
+	os.system("python create_summary_tables_sdss_dr12.py "+plate+" stellarpop-m11-kroupa-nodust")
+	os.system("python create_summary_tables_sdss_dr12.py "+plate+" stellarpop-m11-salpeter")
+	os.system("python create_summary_tables_sdss_dr12.py "+plate+" stellarpop-m11-salpeter-nodust")
 	
 
 plates = n.loadtxt( join(os.environ['EBOSSDR14_DIR'], "catalogs", "plateNumberList"), unpack=True, dtype='str')
 
-tts=[]
-for plate in plates[4:]:
-	t0 = time.time()
-	os.system("python test2.py "+plate+" stellarpop-m11-kroupa")
-	tts.append(time.time()-t0)
-	print tts[-1], "seconds"
+for plate in plates:
+	os.system("python create_summary_tables_eboss_dr14.py "+plate+" stellarpop-m11-salpeter")
+	os.system("python create_summary_tables_eboss_dr14.py "+plate+" stellarpop-m11-salpeter-nodust")
+	os.system("python create_summary_tables_eboss_dr14.py "+plate+" stellarpop-m11-kroupa-nodust")
+	
+	
