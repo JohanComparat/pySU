fbcdccd2dfc3cbab1cc84bd876331b9e858569a3
JohanComparat <johan.comparat@gmail.com>
2017-03-13 08:22:08 +0100

    summary_tables dr12

--- spm/bin/run_summary_tables.py
+++ spm/bin/run_summary_tables.py
@@ -5,18 +5,12 @@ import time
 
 plates = n.loadtxt( join(os.environ['SDSSDR12_DIR'], "catalogs", "plateNumberList"), unpack=True, dtype='str')
 
-tts=[]
-for plate in plates[10:]:
-	t0 = time.time()
-	os.system("python create_summary_tables_sdss_dr12.py "+plate+" stellarpop-m11-kroupa")
-	tts.append(time.time()-t0)
-	print tts[-1], "seconds"
-
 for plate in plates:
 	os.system("python create_summary_tables_sdss_dr12.py "+plate+" stellarpop-m11-kroupa-nodust")
 	os.system("python create_summary_tables_sdss_dr12.py "+plate+" stellarpop-m11-salpeter")
 	os.system("python create_summary_tables_sdss_dr12.py "+plate+" stellarpop-m11-salpeter-nodust")
 	
+sys.exit()
 
 plates = n.loadtxt( join(os.environ['EBOSSDR14_DIR'], "catalogs", "plateNumberList"), unpack=True, dtype='str')
 
