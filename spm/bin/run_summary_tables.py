de5805ca88108be145d117f9d74c9a36eeb751d4
JohanComparat <johan.comparat@gmail.com>
2017-04-03 11:50:56 +0200

    run tables

--- spm/bin/run_summary_tables.py
+++ spm/bin/run_summary_tables.py
@@ -6,17 +6,15 @@ import time
 plates = n.loadtxt( join(os.environ['SDSSDR12_DIR'], "catalogs", "plateNumberList"), unpack=True, dtype='str')
 
 for plate in plates:
-	os.system("python create_summary_tables_sdss_dr12.py "+plate+" stellarpop-m11-kroupa-nodust")
+	os.system("python create_summary_tables_sdss_dr12.py "+plate+" stellarpop-m11-kroupa")
 	os.system("python create_summary_tables_sdss_dr12.py "+plate+" stellarpop-m11-salpeter")
-	os.system("python create_summary_tables_sdss_dr12.py "+plate+" stellarpop-m11-salpeter-nodust")
+	#os.system("python create_summary_tables_sdss_dr12.py "+plate+" stellarpop-m11-salpeter-nodust")
 	
-sys.exit()
-
 plates = n.loadtxt( join(os.environ['EBOSSDR14_DIR'], "catalogs", "plateNumberList"), unpack=True, dtype='str')
 
 for plate in plates:
 	os.system("python create_summary_tables_eboss_dr14.py "+plate+" stellarpop-m11-salpeter")
-	os.system("python create_summary_tables_eboss_dr14.py "+plate+" stellarpop-m11-salpeter-nodust")
-	os.system("python create_summary_tables_eboss_dr14.py "+plate+" stellarpop-m11-kroupa-nodust")
+	os.system("python create_summary_tables_eboss_dr14.py "+plate+" stellarpop-m11-kroupa")
+	#os.system("python create_summary_tables_eboss_dr14.py "+plate+" stellarpop-m11-kroupa-nodust")
 	
 	
