81b92461b97b7124c22f8e8741b0b394fb96f4e2
JohanComparat <johan.comparat@gmail.com>
2017-05-12 20:11:04 +0200

    message

--- spm/bin/check_status_SDSS.py
+++ spm/bin/check_status_SDSS.py
@@ -24,19 +24,18 @@ suffixes = n.array([
 "-kr.fits", "-kr.fits", "-kr.fits", 
 "-ss.fits", "-ss.fits", "-ss.fits"])
 
-done=n.zeros((len(data), len(dirs)))
-
 bds = n.arange(0,len(data),100000)
 for jj in n.arange(0,len(data)+100000,100000)[:-1]:
+	out_file = os.environ['DATA_DIR'], 'status', 'status-SDSS-'+str(bds[jj])+'.txt'
+	f = open(out_file, 'w')
 	for ii, el in enumerate(data[bds[jj]:bds[jj+1]]) :
 		plate = str(int(el['PLATE'])).zfill(4)
 		mjd = str(int(el['MJD']))
 		fiber = str(int(el['FIBERID'])).zfill(4)
 		flyName = "spFly-"+plate+"-"+mjd+"-"+fiber
-		done[ii] = 1.*n.array([ os.path.isfile(join(env, dir, 'stellarpop', plate, flyName + suff)) for dir, suff in zip(dirs, suffixes) ])
-		print done[ii]
-
-	outData = n.vstack((data['PLATE'], data['MJD'], data['FIBERID'], done.T)).astype('int')
-
-	n.savetxt(os.environ['DATA_DIR'], 'status', 'status-SDSS-'+str(bds[jj])+'.txt.gz', outData[selection], fmt='%i', header=' plate mjd fiberid summary cm cs ce km ks ke sm ss se ' )
+		done = 1.*n.array([ os.path.isfile(join(env, dir, 'stellarpop', plate, flyName + suff)) for dir, suff in zip(dirs, suffixes) ])
+		f.write(n.vstack((data['PLATE'], data['MJD'], data['FIBERID'], done.T)).astype('int'))
+		f.write('\n')
+		
+	f.close()
 
