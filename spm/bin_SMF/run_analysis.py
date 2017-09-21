79b4431dcf0d6e518e5ee61508c8624efcfa3c66
JohanComparat <johan.comparat@gmail.com>
2017-09-21 09:10:32 +0200

    update analysis flow

--- spm/bin_SMF/run_analysis.py
+++ spm/bin_SMF/run_analysis.py
@@ -13,8 +13,29 @@
 python create_table1.py
 # writes here os.environ['OBS_REPO'], 'spm', 'results', "table_1.tex" and "table_2.tex"
 
-# creates figure 1
+# figure 1 is an example spectrum created by firefly code
+
+# Creates figure 2 about sigma_m and SNR per pixel
 python pdf_SM_error.py
+
+# Figure 3 
+plot M vs Z for all accurate stellar masses
+ - SDSS
+ - DEEP2
+ - COSMOS: or not
+
+z_bins = n.arange(0, 1.7, 0.05)
+m_bins = n.arange(7, 13, 0.1)
+weight = area 
+area_boss = 10000.
+area_sdss = 7900.
+area_deep2 = xxx
+n.histogram2d(redshift, mass, weight, z_bins, m_bins)
+
+density_bins_per_deg2 = n.array([0.1, 1, 10, 100, 1000])
+
+ 
+N(z) for all accurate stellar masses
 
 sh run_smf_plots.sh
 python smf_plot.py 0.78 0.83 41.8
