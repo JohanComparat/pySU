cdc0e41962f1be2191f6e167a060db9aecb5d15e
JohanComparat <johan.comparat@gmail.com>
2017-10-04 16:40:34 +0200

    table 1 firefly paper

--- spm/bin_SMF/lib_spm.py
+++ spm/bin_SMF/lib_spm.py
@@ -53,13 +53,17 @@ path_2_vvdsD_cat = os.path.join( vvds_dir, "catalogs", "VVDS_DEEP_summary.v1.spm
 # path_2_F16_cat = os.path.join( sdss_dir, "RA_DEC_z_w_fluxOII_Mstar_grcol_Mr_lumOII.dat" )
 
 # OPENS THE CATALOGS
+print("Loads deep surveys")
 deep2   = fits.open(path_2_deep2_cat)[1].data
-vvdsD   = fits.open(path_2_vvdsD_cat)[1].data
-vvdsW   = fits.open(path_2_vvdsW_cat)[1].data
-vipers   = fits.open(path_2_vipers_cat)[1].data
-sdss   = fits.open(path_2_sdss_cat)[1].data
-boss   = fits.open(path_2_eboss_cat)[1].data
-cosmos = fits.open(path_2_cosmos_cat)[1].data
+#vvdsD   = fits.open(path_2_vvdsD_cat)[1].data
+#vvdsW   = fits.open(path_2_vvdsW_cat)[1].data
+#vipers   = fits.open(path_2_vipers_cat)[1].data
+print("Loads sdss")
+#sdss   = fits.open(path_2_sdss_cat)[1].data
+print("Loads boss")
+#boss   = fits.open(path_2_eboss_cat)[1].data
+#print("Loads cosmos")
+#cosmos = fits.open(path_2_cosmos_cat)[1].data
 
 #sdss_12_portSF_kr   = fits.open(path_2_pS_kroupa_cat)[1].data
 #boss_12_portSF_kr   = fits.open(path_2_pB_kroupa_cat)[1].data
@@ -73,23 +77,34 @@ cosmos = fits.open(path_2_cosmos_cat)[1].data
 #RA, DEC, z, weigth, O2flux, M_star, gr_color, Mr_5logh, O2luminosity = n.loadtxt(path_2_F16_cat, unpack=True)
 
 # stat functions
-ld = lambda selection : len(selection.nonzero()[0])
+ld = lambda selection : 1.*len(selection.nonzero()[0])
 sld = lambda selection : str(len(selection.nonzero()[0]))
+
+SNlimit = 5
+lineSelection = lambda catalog, lineName : (catalog[lineName+'_flux']>0.)& (catalog[lineName+'_fluxErr'] >0.) & (catalog[lineName+'_flux'] > SNlimit * catalog[lineName+'_fluxErr']) # & (catalog[lineName+'_luminosity']>0)& (catalog[lineName+'_luminosity']<1e50)
 
 def get_basic_stat_firefly_DR14(catalog, z_name, z_err_name, class_name, zwarning, name, zflg_val, prefix):
     catalog_zOk =(catalog[z_err_name] > 0.) & (catalog[z_name] > catalog[z_err_name])  & (catalog[class_name]=='GALAXY')  & (catalog[zwarning]==zflg_val) & (catalog[z_name] > z_min) & (catalog[z_name] < z_max) 
-    catalog_sel = (catalog_zOk) & (catalog[prefix+'stellar_mass'] < 10**14. ) & (catalog[prefix+'stellar_mass'] > 0 )  & (catalog[prefix+'stellar_mass'] > catalog[prefix+'stellar_mass_low'] ) & (catalog[prefix+'stellar_mass'] < catalog[prefix+'stellar_mass_up'] ) & ( - n.log10(catalog[prefix+'stellar_mass_low'])  + n.log10(catalog[prefix+'stellar_mass_up']) < 0.4 )
+    converged = (catalog_zOk)&(catalog[prefix+'stellar_mass'] < 10**13. ) & (catalog[prefix+'stellar_mass'] > 10**4 )  & (catalog[prefix+'stellar_mass'] > catalog[prefix+'stellar_mass_low'] ) & (catalog[prefix+'stellar_mass'] < catalog[prefix+'stellar_mass_up'] ) 
+    dex04 = (converged) & (catalog[prefix+'stellar_mass'] < 10**14. ) & (catalog[prefix+'stellar_mass'] > 0 )  & (catalog[prefix+'stellar_mass'] > catalog[prefix+'stellar_mass_low'] ) & (catalog[prefix+'stellar_mass'] < catalog[prefix+'stellar_mass_up'] ) & ( - n.log10(catalog[prefix+'stellar_mass_low'])  + n.log10(catalog[prefix+'stellar_mass_up']) < 0.8 )
+    dex02 = (dex04) & ( - n.log10(catalog[prefix+'stellar_mass_low'])  + n.log10(catalog[prefix+'stellar_mass_up']) < 0.4 )
     m_catalog = n.log10(catalog[prefix+'stellar_mass'])
     w_catalog =  n.ones_like(catalog[prefix+'stellar_mass'])
-    return name + '& $' +str(len(catalog))+ "$ & $"+ sld(catalog_zOk)+"$ & $"+ sld(catalog_sel)+"$ \\\\"
+    return name + " & $"+ sld(converged)+"$ ("+str(n.round(ld(converged)/ld(catalog_zOk)*100.,1))+") & $"+ sld(dex04)+"$ ("+str(n.round(ld(dex04)/ld(catalog_zOk)*100.,1))+") & $"+ sld(dex02)+ "$ ("+str(n.round(ld(dex02)/ld(catalog_zOk)*100.,1))+") \\\\"
     #return catalog_sel, m_catalog, w_catalog
 
-def get_basic_stat_deep2(catalog, z_name, z_flg, name, zflg_min, prefix):
+def get_basic_stat_deep2(catalog, z_name, z_flg, name, zflg_min, prefix, o2=False):
     catalog_zOk = (catalog[z_name] > z_min) & (catalog[z_flg]>=zflg_min) & (catalog[z_name] > z_min) & (catalog[z_name] < z_max) & (catalog['SSR']>0) & (catalog['TSR']>0) & (catalog['SSR']<=1.0001) & (catalog['TSR']<=1.0001)
-    catalog_sel = (catalog_zOk) & (catalog[prefix+'stellar_mass'] < 10**14. ) & (catalog[prefix+'stellar_mass'] > 0. )  & (catalog[prefix+'stellar_mass'] > catalog[prefix+'stellar_mass_low'] ) & (catalog[prefix+'stellar_mass'] < catalog[prefix+'stellar_mass_up'] ) & ( - n.log10(catalog[prefix+'stellar_mass_low'])  + n.log10(catalog[prefix+'stellar_mass_up']) < 0.4 )
+    converged = (catalog_zOk)&(catalog[prefix+'stellar_mass'] < 10**13. ) & (catalog[prefix+'stellar_mass'] > 10**4 )  & (catalog[prefix+'stellar_mass'] > catalog[prefix+'stellar_mass_low'] ) & (catalog[prefix+'stellar_mass'] < catalog[prefix+'stellar_mass_up'] ) 
+    dex04 = (converged) & (catalog[prefix+'stellar_mass'] < 10**14. ) & (catalog[prefix+'stellar_mass'] > 0 )  & (catalog[prefix+'stellar_mass'] > catalog[prefix+'stellar_mass_low'] ) & (catalog[prefix+'stellar_mass'] < catalog[prefix+'stellar_mass_up'] ) & ( - n.log10(catalog[prefix+'stellar_mass_low'])  + n.log10(catalog[prefix+'stellar_mass_up']) < 0.8 )
+    dex02 = (dex04) & ( - n.log10(catalog[prefix+'stellar_mass_low'])  + n.log10(catalog[prefix+'stellar_mass_up']) < 0.4 )
     m_catalog = n.log10(catalog[prefix+'stellar_mass'])
     w_catalog = 1. / (catalog['TSR'] * catalog['SSR'])
-    return name + '& $' +str(len(catalog))+ "$ & $"+ sld(catalog_zOk)+"$ & $"+ sld(catalog_sel)+"$ \\\\"
+    if o2:
+      l_o2 = lineSelection(catalog, "O2_3728") & catalog_zOk
+      return name +' & $'+ sld(converged & l_o2)+"$ ("+str(n.round(ld(converged & l_o2)/ld(catalog_zOk & l_o2)*100.,1))+") & $"+ sld(dex04 & l_o2)+"$ ("+str(n.round(ld(dex04 & l_o2)/ld(catalog_zOk & l_o2)*100.,1))+") & $"+ sld(dex02 & l_o2)+"$ ("+str(n.round(ld(dex02 & l_o2)/ld(catalog_zOk & l_o2)*100.,1))+r") \\\\"
+    else:
+      return name + '& $'+ sld(converged)+"$ ("+str(n.round(ld(converged)/ld(catalog_zOk)*100.,1))+") & $"+ sld(dex04)+"$ ("+str(n.round(ld(dex04)/ld(catalog_zOk)*100.,1))+") & $"+ sld(dex02)+"$ ("+str(n.round(ld(dex02)/ld(catalog_zOk)*100.,1))+r") \\\\"
     #return catalog_sel, m_catalog, w_catalog
 
 def get_basic_stat_DR12(catalog, z_name, z_err_name, name, zflg_val):
@@ -97,20 +112,22 @@ def get_basic_stat_DR12(catalog, z_name, z_err_name, name, zflg_val):
     catalog_sel = (catalog_zOk) & (catalog['LOGMASS'] < 14. ) & (catalog['LOGMASS'] > 0 ) & (catalog['MAXLOGMASS'] - catalog['MINLOGMASS'] <0.4) & (catalog['LOGMASS'] < catalog['MAXLOGMASS'] ) & (catalog['LOGMASS'] > catalog['MINLOGMASS'] )
     m_catalog = catalog['LOGMASS']
     w_catalog =  n.ones_like(catalog['LOGMASS'])
-    return name + '& $' +str(len(catalog))+ "$ & $"+ sld(catalog_zOk)+"$ & $"+ sld(catalog_sel)+"$ \\\\"
+    return name + '& $' +str(len(catalog))+ "$ & $"+ sld(catalog_zOk)+"$ & $"+ sld(catalog_sel)+r"$ \\\\"
     #return catalog_sel, m_catalog, w_catalog
-
-SNlimit = 5
-lineSelection = lambda catalog, lineName : (catalog[lineName+'_flux']>0.)& (catalog[lineName+'_fluxErr'] >0.) & (catalog[lineName+'_flux'] > SNlimit * catalog[lineName+'_fluxErr']) # & (catalog[lineName+'_luminosity']>0)& (catalog[lineName+'_luminosity']<1e50)
 
 def get_line_stat_deep2(catalog, z_name, z_flg, name, zflg_min, prefix):
     catalog_zOk = (catalog[z_name] > z_min) & (catalog[z_flg]>=zflg_min) & (catalog[z_name] > z_min) & (catalog[z_name] < z_max) & (catalog['SSR']>0) & (catalog['TSR']>0) & (catalog['SSR']<=1.0001) & (catalog['TSR']<=1.0001)
-    catalog_sel = (catalog_zOk) & (catalog[prefix+'stellar_mass'] < 10**14. ) & (catalog[prefix+'stellar_mass'] > 10**5. )  & (catalog[prefix+'stellar_mass'] < catalog[prefix+'stellar_mass_up'] ) & (catalog[prefix+'stellar_mass'] > catalog[prefix+'stellar_mass_low'] ) & (-n.log10(catalog[prefix+'stellar_mass_low'])  + n.log10(catalog[prefix+'stellar_mass_up']) < 1.)
+    converged = (catalog_zOk) & (catalog[prefix+'stellar_mass'] < 10**14. ) & (catalog[prefix+'stellar_mass'] > 10**5. )  & (catalog[prefix+'stellar_mass'] < catalog[prefix+'stellar_mass_up'] ) & (catalog[prefix+'stellar_mass'] > catalog[prefix+'stellar_mass_low'] ) & (-n.log10(catalog[prefix+'stellar_mass_low'])  + n.log10(catalog[prefix+'stellar_mass_up']) < 1.)
+    dex04 = (converged) & (catalog[prefix+'stellar_mass'] < 10**14. ) & (catalog[prefix+'stellar_mass'] > 0 )  & (catalog[prefix+'stellar_mass'] > catalog[prefix+'stellar_mass_low'] ) & (catalog[prefix+'stellar_mass'] < catalog[prefix+'stellar_mass_up'] ) & ( - n.log10(catalog[prefix+'stellar_mass_low'])  + n.log10(catalog[prefix+'stellar_mass_up']) < 0.8 )
+    dex02 = (dex04) & ( - n.log10(catalog[prefix+'stellar_mass_low'])  + n.log10(catalog[prefix+'stellar_mass_up']) < 0.4 )
+    m_catalog = n.log10(catalog[prefix+'stellar_mass'])
+
     l_o2 = lineSelection(catalog, "O2_3728") & catalog_zOk
     l_o3 = lineSelection(catalog, "O3_5007") & catalog_zOk
     l_hb = lineSelection(catalog, "H1_4862") & catalog_zOk
     m_catalog = n.log10(catalog[prefix+'stellar_mass'])
     w_catalog = 1. / (catalog['TSR'] * catalog['SSR'])
-    return name +  '& $' + str(len(catalog)) +  "$ & $" +  sld(catalog_zOk) + "$ & $" +  "\\;(" +  sld(catalog_sel) + ")$ & $" +  sld(l_o2) +  "\\;(" +  sld(catalog_sel & l_o2) + ")$ & $" +  sld(l_o3) +  "\\;(" +  sld(catalog_sel & l_o3) + ")$ & $" +  sld(l_hb) +  "\\;(" +  sld(catalog_sel & l_hb) + ")$ \\\\"
+    return name +  '& $' +sld(converged) +"$ ("+str(n.round(ld(converged)/ld(catalog_zOk)*100.,1))+") & $"+  "\\;(" +  sld(catalog_sel) + ")$ & $" +  sld(l_o2) +  "\\;(" +  sld(catalog_sel & l_o2) + r")$  \\\\"
+    #return name +  '& $' sld(converged) + "$ & $" +  "\\;(" +  sld(catalog_sel) + ")$ & $" +  sld(l_o2) +  "\\;(" +  sld(catalog_sel & l_o2) + ")$ & $" +  sld(l_o3) +  "\\;(" +  sld(catalog_sel & l_o3) + ")$ & $" +  sld(l_hb) +  "\\;(" +  sld(catalog_sel & l_hb) + ")$ \\\\"
     #return catalog_sel, m_catalog, w_catalog, l_o2, l_o3, l_hb
     
