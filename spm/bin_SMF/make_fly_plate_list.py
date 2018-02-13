80a16278fe8bc258d36084912d6a42c9fc392e50
JohanComparat <johan.comparat@gmail.com>
2018-02-13 20:02:28 +0100

    make list

--- spm/bin_SMF/make_fly_plate_list.py
+++ spm/bin_SMF/make_fly_plate_list.py
@@ -1,22 +1,28 @@
+from astropy.io import fits
 import numpy as n
 import glob
 import os
+import sys
+
+vv = sys.argv[1]
 
 #plates = n.array(os.listdir("/home/comparat/SDSS/26/stellarpop"))
-top_dir = "/home/comparat/SDSS/26/stellarpop"
+top_dir = "/home/comparat/SDSS/"+vv+"/stellarpop"
 
 #NN = n.array([len(os.listdir('../stellarpop/'+pl)) for pl in plates ])
-flyall = n.array([glob.glob(top_dir + "/spFlyPl*.fits"))
+flyall = n.array(glob.glob(top_dir + "/spFlyPl*.fits"))
 flyall.sort()
-
+print(flyall)
+print(len(flyall))
 #pll = n.array([el.split('/')[-2] for el in flyall])
 
-NN = n.array([len(fit.open(pl)[1].data) for pl in flyall ])
-NH = n.array([len(fit.open(pl)[1].data.dtype) for pl in flyall ])
+NN = n.array([len(fits.open(pl)[1].data) for pl in flyall ])
+print(NN)
+NH = n.array([len(fits.open(pl)[1].data.dtype) for pl in flyall ])
 print(set(NH))
 
-n.savetxt('flyAllList', flyall[NN>0], fmt='%s')
-n.savetxt('flyAllList_N0', flyall[NN>0], fmt='%s')
-n.savetxt('flyAllList_H0', flyall[NH>0], fmt='%s')
-n.savetxt('flyAllList_NH', flyall[(NN>0)&(NH>0)], fmt='%s')
+n.savetxt(top_dir+'/flyAllList', flyall, fmt='%s')
+n.savetxt(top_dir+'/flyAllList_N0', flyall[NN>0], fmt='%s')
+n.savetxt(top_dir+'/flyAllList_H0', flyall[(NN>0)&(NH==640)], fmt='%s')
+n.savetxt(top_dir+'/flyAllList_NH', flyall[(NN<=0)&(NH!=640)], fmt='%s')
 
