da68f8a5ef9d417d073d33c5309c13118d621123
JohanComparat <johan.comparat@gmail.com>
2018-02-22 09:15:26 +0100

    delta mag

--- spm/bin_SMF/create_table_delta_mag.py
+++ spm/bin_SMF/create_table_delta_mag.py
@@ -85,7 +85,7 @@ tpps = []
 for pg in sourcetypes:
 	sel_all = sel_st(pg)
 	n_all = length( sel_all )  *1.
-	if n_all > 0 :
+	if n_all > 100 :
 		#print(pg, n_all)
 		all_galaxies.append(n_all)
 		all_out = []
