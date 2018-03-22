d561b3f6952c147ea92fecca14fc9c812e659f22
JohanComparat <johan.comparat@gmail.com>
2018-03-22 19:06:55 +0100

    make summary files

---
+++ spm/bin_SMF/summary_table_merge_firefly_MAG.py
@@ -0,0 +1,23 @@
+from astropy.io import fits
+import numpy as n
+import glob
+import os
+import sys
+
+vv = sys.argv[1]
+
+#plates = n.array(os.listdir("/home/comparat/SDSS/26/stellarpop"))
+top_dir = "/home/comparat/SDSS/"+vv
+cat_dir = top_dir+"/catalogs"
+cat_file  = cat_dir+'/FireFly.fits'
+
+mag_file = "/home/comparat/SDSS/specphotalldr14_comparat.fit"
+
+cm_3d = lambda f1, f2, out : """stilts tmatch2 in1="""+f1+""" ifmt1=fits in2="""+f2+""" ifmt2=fits matcher=3d values1="PLATE MJD FIBERID" values2="PLATE MJD FIBERID" params=0.0001 join=all1 find=best omode=out ofmt=fits out="""+out
+
+out = cat_dir+'/FireFly_mag.fits'
+
+c1 = cm_3d(cat_file, mag_file, out)
+print(c1)
+os.system(c1)
+
