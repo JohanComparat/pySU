79972027713443df4c0bec75ca5c6be29e24e971
JohanComparat <johan.comparat@gmail.com>
2018-09-05 00:33:00 +0200

    stacks

--- galaxy/bin_eBOSS_ELG/plot_UV_stack.py
+++ galaxy/bin_eBOSS_ELG/plot_UV_stack.py
@@ -15,6 +15,10 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as p
 
+line_list_abs = n.array([2249.88, 2260.78, 2344.21, 2374.46, 2382.76, 2576.88, 2586.65, 2594.50, 2600.17, 2606.46, 2796.35, 2803.53, 2852.96])
+line_list_abs_names = n.array(['FeII', 'FeII', 'FeII', 'FeII', 'FeII', 'MnII', 'FeII', 'MnII','FeII', 'MnII', 'MgII','MgII','MgI'])
+line_list_em = n.array([2327, 2365.55, 2396.36, 2612.65,2626.45])
+line_list_em_names = n.array(['CII]', 'FeII*', 'FeII*', 'FeII*', 'FeII*'])
 
 def plot_me(qty):
 	# create all stacks
@@ -27,13 +31,23 @@ def plot_me(qty):
 		bn = os.path.basename(specList)[10:-8]
 		dd=fits.open(specList)[1].data
 		wl=dd['wavelength'         ]
-		s1 = (wl>2000)&(wl<3700)&(dd['NspectraPerPixel'   ]>0.5*n.max(dd['NspectraPerPixel'   ]))
+		s1 = (wl>2200)&(wl<2900)&(dd['NspectraPerPixel'   ]>0.5*n.max(dd['NspectraPerPixel'   ]))
 		p.plot(dd['wavelength'         ][s1], dd['medianStack'        ][s1], label= bn )
 		#dd['meanStack'          ][s1]
 		#dd['meanWeightedStack'  ][s1]
 		#dd['jackknifeSpectra'   ][s1]
 		#dd['jackknifStackErrors'][s1]
 		#dd['NspectraPerPixel'   ][s1]
+
+
+	for xx, nn in zip(line_list_abs, line_list_abs_names ):
+		p.axvline(xx, ls='dashed', color='k')
+		p.text(xx,0.3,nn,rotation=90)
+
+	for xx, nn in zip(line_list_em, line_list_em_names ):
+		p.axvline(xx, ls='dashed', color='=g')
+		p.text(xx,1.1,nn,rotation=90, color='g')
+
 
 	p.legend(frameon=False)
 	p.tight_layout()
