e98fa3d60eaa85838440e86b5be2ea32a47abfb3
JohanComparat <johan.comparat@gmail.com>
2018-11-07 08:39:08 +0100

    v51010

--- galaxy/bin_eBOSS_ELG/stack_spectra_UV.py
+++ galaxy/bin_eBOSS_ELG/stack_spectra_UV.py
@@ -9,28 +9,33 @@ from os.path import join
 import glob
 import numpy as n
 import SpectraStackingEBOSS as sse
-
+import time
+t0=time.time()
 # create all stacks
 dataList = n.array(glob.glob(join(os.environ['HOME'],"SDSS/lss/catalogs/3/stacks_v1", "eboss-elg_*.asc")))
 dataList.sort()
 
 for specList in dataList:
-	print( specList )
+	print('considers', specList, time.time()-t0)
 	outfile = join(os.environ['HOME'], "stacks", os.path.basename(specList)[:-4]+".stack")
 	#outfile = join(os.environ['HOME'], "SDSS", "stacks", os.path.basename(specList)[:-4]+".stack")
 	if os.path.isfile(outfile)==False:
+		print('starts working on', outfile, time.time()-t0)
 		stack=sse.SpectraStackingEBOSS(specList, outfile)
-		print(outfile)
 		if os.path.isfile(outfile+'.specMatrix.dat')==False:
+			print('creates matrix', time.time()-t0)
 			stack.createStackMatrix()
+		print('stacks', time.time()-t0)
 		stack.stackSpectra()
 
 	outfile = join(os.environ['HOME'], "stacks", os.path.basename(specList)[:-4]+".UVstack")
 	#outfile = join(os.environ['HOME'],"SDSS", "stacks", os.path.basename(specList)[:-4]+".UVstack")
 	if os.path.isfile(outfile)==False:
+		print('starts working on', outfile, time.time()-t0)
 		stack=sse.SpectraStackingEBOSS(specList, outfile)
-		print(outfile)
 		if os.path.isfile(outfile+'.specMatrix.dat')==False:
+			print('creates matrix', time.time()-t0)
 			stack.createStackMatrix_UVnormed()
+		print('stacks', time.time()-t0)
 		stack.stackSpectra()
 		
