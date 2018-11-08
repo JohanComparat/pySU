301e4b5e6fe01d43a216520927530427ff576fa4
JohanComparat <johan.comparat@gmail.com>
2018-11-08 08:18:57 +0100

    update plotting

--- galaxy/bin_eBOSS_ELG/plot_UV_stack.py
+++ galaxy/bin_eBOSS_ELG/plot_UV_stack.py
@@ -16,7 +16,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as p
 
 
-agn_file = join(os.environ['HOME'],"SDSS", "stacks", "xagn_0.38.stack")
+agn_file = join(os.environ['HOME'],"SDSS/stacks/v1", "xagn_0.38.stack")
 agn=fits.open(agn_file)[1].data
 s1 = (agn['wavelength'         ]>2200)&(agn['wavelength'         ]<2900)
 xA, yA = agn['wavelength'         ][s1], agn['medianStack'        ][s1]
@@ -26,16 +26,22 @@ agn=fits.open("/home/comparat/data-4most/opsim_data/survey_team_inputs/Xgal/temp
 s1 = (agn['LAMBDA']/(1.15)>2200)&(agn['LAMBDA']/(1.15)<3800)
 xA, yA = agn['LAMBDA'][s1]/(1.15), agn['FLUX'][s1]/(3.5950675388804168e-16)
 
+ELG=fits.open("/home/comparat/stacks/zhu_2015/eBOSS_ELG_NUV_composite.fits")[1].data
+ELG_a=fits.open("/home/comparat/stacks/zhu_2015/eBOSS_ELG_composite.fits")[1].data
+#eBOSS_ELG_composite.fits  
+
 
 line_list_abs = n.array([2249.88, 2260.78, 2344.21, 2374.46, 2382.76, 2576.88, 2586.65, 2594.50, 2600.17, 2606.46, 2796.35, 2803.53, 2852.96])
 line_list_abs_names = n.array(['FeII', 'FeII', 'FeII', 'FeII', 'FeII', 'MnII', 'FeII', 'MnII','FeII', 'MnII', 'MgII','MgII','MgI'])
 line_list_em = n.array([2327, 2365.55, 2396.36, 2612.65,2626.45])
 line_list_em_names = n.array(['CII]', 'FeII*', 'FeII*', 'FeII*', 'FeII*'])
 
-def plot_me(qty):
+def plot_me(qty='O2EW'):
 	# create all stacks
-	dataList_UV = n.array(glob.glob(join(os.environ['HOME'], "SDSS/stacks", "eboss-elg_0.8883838383838384_z_0.9493067632850242_*_"+qty+"_*.UVstack")))
-	dataList = n.array(glob.glob(join(os.environ['HOME'], "SDSS/stacks", "eboss-elg_0.8883838383838384_z_0.9493067632850242_*_"+qty+"_*.stack")))
+	# O2EW
+	# O2lum
+	dataList_UV = n.array(glob.glob(join(os.environ['HOME'], "stacks", "eboss-elg_0.6_1.2_*_"+qty+"_*.UVstack")))
+	dataList = n.array([el[:-7]+"stack" for el in dataList_UV])
 	
 	fig=p.figure(0,(7.2, 13.7), frameon=False)
 	
@@ -44,12 +50,12 @@ def plot_me(qty):
 	for specList in dataList_UV:
 		bn = os.path.basename(specList)[10:-8].split('_')
 		print(bn)
-		if qty == 'fast_lmass':
-			bnl = str(n.round(float(bn[0]),3))+'<z<'+str(n.round(float(bn[2]),3))+', '+str(n.round(float(bn[3]),3))+'<'+qty+'<'+str(n.round(float(bn[6]),3))
-		else:
-			bnl = str(n.round(float(bn[0]),3))+'<z<'+str(n.round(float(bn[2]),3))+', '+str(n.round(float(bn[3]),3))+'<'+qty+'<'+str(n.round(float(bn[5]),3))
+		#if qty == 'fast_lmass':
+			#bnl = str(n.round(float(bn[0]),3))+'<z<'+str(n.round(float(bn[2]),3))+', '+str(n.round(float(bn[3]),3))+'<'+qty+'<'+str(n.round(float(bn[6]),3))
+		#else:
+		bnl = str(n.round(float(bn[0]),3))+'<z<'+str(n.round(float(bn[2]),3))+', '+str(n.round(float(bn[3]),3))+'<'+qty+'<'+str(n.round(float(bn[5]),3))
 		dd=fits.open(specList)[1].data
-		wl=dd['wavelength'         ]
+		wl=dd['wavelength']
 		s1 = (wl>2200)&(wl<2900)&(dd['NspectraPerPixel'   ]>0.5*n.max(dd['NspectraPerPixel'   ]))
 		if float(bn[0])>0.5:
 			p.plot(dd['wavelength'         ][s1], dd['medianStack'        ][s1], label= bnl, lw=0.7 )
@@ -61,6 +67,9 @@ def plot_me(qty):
 	for xx, nn in zip(line_list_em, line_list_em_names ):
 		p.plot(n.array([xx,xx]),n.array([1,2]), ls='dashed', color='g', lw=0.5)
 		p.text(xx,1.4,nn,rotation=90, color='g')
+	
+	ok = (ELG['WAVE']>2200)&(ELG['WAVE']<2900)
+	p.plot(ELG['WAVE'][ok], ELG['FLUXMEDIAN'][ok] , label='Zhu15')
 
 	p.legend(frameon=False)
 	p.grid()
@@ -83,6 +92,8 @@ def plot_me(qty):
 		p.plot(n.array([xx,xx]),n.array([1,2]), ls='dashed', color='g', lw=0.5)
 		p.text(xx,1.4,nn,rotation=90, color='g')
 	p.grid()
+	ok = (ELG['WAVE']>2200)&(ELG['WAVE']<2900)
+	p.plot(ELG['WAVE'][ok], ELG['FLUXMEDIAN'][ok] , label='Zhu15')
 
 
 	fig.add_subplot(313, xlabel='wavelength [Angstrom, rest frame]', xlim=((2780, 2870)), ylim=((0,2)))
@@ -104,9 +115,11 @@ def plot_me(qty):
 		p.text(xx,1.4,nn,rotation=90, color='g')
 	
 	p.grid()
+	ok = (ELG['WAVE']>2200)&(ELG['WAVE']<2900)
+	p.plot(ELG['WAVE'][ok], ELG['FLUXMEDIAN'][ok] , label='Zhu15')
 
 	p.tight_layout()
-	p.savefig(join(os.environ['HOME'], "SDSS/stacks", "eboss-elg_"+qty+".UVstack")+".png")
+	p.savefig(join(os.environ['HOME'], "stacks", "eboss-elg_"+qty+".UVstack")+".png")
 	p.clf()
 
 	p.figure(1,(9,5))
@@ -127,20 +140,23 @@ def plot_me(qty):
 		#dd['jackknifeSpectra'   ][s1]
 		#dd['jackknifStackErrors'][s1]
 		#dd['NspectraPerPixel'   ][s1]
-	
+	p.plot(ELG['WAVE'], ELG['FLUXMEDIAN'], label='Zhu15')
 	p.grid()
 	p.xlim((2300,3800))
 	p.plot(xA,yA,'k',lw=0.5, label='NLAGN')
 	p.yscale('log')
 	p.legend(frameon=False)
 	p.tight_layout()
-	p.savefig(join(os.environ['HOME'], "SDSS/stacks", "eboss-elg_"+qty+".stack")+".png")
+	p.savefig(join(os.environ['HOME'], "stacks", "eboss-elg_"+qty+".stack")+".png")
 	p.clf()
+	
+plot_me(qty = 'O2EW' )
+plot_me(qty = 'O2lum' )
 
-plot_me(qty = 'mass' )
-plot_me(qty = 'g'          )
-plot_me(qty = 'gr'         )
-plot_me(qty = 'rz'         )
-plot_me(qty = 'rw1'         )
-
-os.system("cp -r ~/SDSS/stacks/*.png ~/wwwDir/sdss/elg/stacks/")
+#plot_me(qty = 'mass' )
+#plot_me(qty = 'g'          )
+#plot_me(qty = 'gr'         )
+#plot_me(qty = 'rz'         )
+#plot_me(qty = 'rw1'         )
+os.system("rm ~/wwwDir/sdss/elg/stacks/*.png")
+os.system("cp -r ~/stacks/*.png ~/wwwDir/sdss/elg/stacks/")
