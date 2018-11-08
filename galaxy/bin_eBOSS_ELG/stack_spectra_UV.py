15792ea65b6c5a95f081bd1a66ff7cbb251c3b35
JohanComparat <johan.comparat@gmail.com>
2018-11-08 10:45:42 +0100

    update plotting

--- galaxy/bin_eBOSS_ELG/stack_spectra_UV.py
+++ galaxy/bin_eBOSS_ELG/stack_spectra_UV.py
@@ -16,54 +16,62 @@ t0=time.time()
 dataList = n.array(glob.glob(join(os.environ['HOME'],"SDSS/stacks", "eboss-elg_*.asc")))
 dataList.sort()
 
-#for specList in dataList:
-
-specList = dataList[-1]
-print('considers', specList, time.time()-t0)
-outfile = join(os.environ['HOME'], "SDSS/stacks", os.path.basename(specList)[:-4]+".stack")
-#outfile = join(os.environ['HOME'], "SDSS", "stacks", os.path.basename(specList)[:-4]+".stack")
-#if os.path.isfile(outfile)==False:
-print('starts working on', outfile, time.time()-t0)
-stack=sse.SpectraStackingEBOSS(specList, outfile, dLambda = 0.0002, dV=-9999.99, l_start=3.35, l_end=3.579)
-if os.path.isfile(outfile+'.specMatrix.dat')==False:
-	print('creates matrix', time.time()-t0)
-	stack.createStackMatrix()
-print('stacks', time.time()-t0)
-stack.stackSpectra()
-
-hdu = fits.open(outfile)
-dd=hdu[1].data
-wl=dd['wavelength'         ]
-s1 = (dd['NspectraPerPixel'   ]>0.5*n.max(dd['NspectraPerPixel'   ]))
-x, y= dd['wavelength'         ][s1], dd['medianStack'        ][s1]
-yerr = dd['jackknifStackErrors'][s1]
-pfit = stack.fit_UV_continuum(x,y,yerr,degree=3)
-Fcont = n.polyval(pfit, dd['wavelength'         ])
-spec_UV.append(dd['medianStack'        ]/Fcont)
-spec_UV_Err.append(dd['jackknifStackErrors'        ]/Fcont)
-
-
-
-outfile = join(os.environ['HOME'], "SDSS/stacks", os.path.basename(specList)[:-4]+".UVstack")
-
-
-if os.path.isfile(self.out_file):
-	os.remove(self.out_file)
-print( "stack written to", self.out_file )
-thdulist.writeto(self.out_file)
-
-
-	outfile = join(os.environ['HOME'], "SDSS/stacks", os.path.basename(specList)[:-4]+".UVstack")
-	#outfile = join(os.environ['HOME'],"SDSS", "stacks", os.path.basename(specList)[:-4]+".UVstack")
-	#if os.path.isfile(outfile)==False:
-	print('starts working on', outfile, time.time()-t0)
-	stack=sse.SpectraStackingEBOSS(specList, outfile, dLambda = 0.0002, dV=-9999.99, l_start=3.34, l_end=3.556)
-	if os.path.isfile(outfile+'.specMatrix.dat')==False:
+for specList in dataList:
+	print('considers', specList, time.time()-t0)
+	out_file = join(os.environ['HOME'], "SDSS/stacks", os.path.basename(specList)[:-4]+".stack")
+	#out_file = join(os.environ['HOME'], "SDSS", "stacks", os.path.basename(specList)[:-4]+".stack")
+	#if os.path.isfile(out_file)==False:
+	print('starts working on', out_file, time.time()-t0)
+	stack=sse.SpectraStackingEBOSS(specList, out_file, dLambda = 0.0002, dV=-9999.99, l_start=3.35, l_end=3.579)
+	if os.path.isfile(out_file+'.specMatrix.dat')==False:
 		print('creates matrix', time.time()-t0)
-		stack.createStackMatrix_UVnormed()
+		stack.createStackMatrix()
 	print('stacks', time.time()-t0)
-	stack.stackSpectra()
+	#stack.stackSpectra()
 
-#specMatrix = n.loadtxt(outfile+'.specMatrix.dat')
-#specMatrixErr = n.loadtxt(outfile+'.specMatrixErr.dat')
-#specMatrixWeight = n.loadtxt(outfile+'.specMatrixWeight.dat')
+	hdu = fits.open(out_file)
+	dd=hdu[1].data
+	wl=dd['wavelength'         ]
+	s1 = (dd['NspectraPerPixel'   ]>0.5*n.max(dd['NspectraPerPixel'   ]))
+	x, y= dd['wavelength'         ][s1], dd['medianStack'        ][s1]
+	yerr = dd['jackknifStackErrors'][s1]
+	pfit = stack.fit_UV_continuum(x,y,yerr,degree=3)
+	Fcont = n.polyval(pfit, dd['wavelength'         ])
+	spec_UV = dd['medianStack'        ]/Fcont
+	spec_UV_Err = dd['jackknifStackErrors'        ]/Fcont
+
+	c1 = fits.Column(name="medianStack_UVnormed",format="D", unit="erg/s/cm2/Angstrom", array= spec_UV)
+	c2 = fits.Column(name="jackknifStackErrors_UVnormed",format="D", unit="erg/s/cm2/Angstrom", array= spec_UV_Err)
+	cols_2 = fits.ColDefs([c1, c2])
+
+	tbhdu = fits.BinTableHDU.from_columns(hdu[1].columns + cols_2)
+	prihdr = fits.Header()
+	prihdr['author'] = "JC"
+	prihdr['survey'] = hdu[0].header['SURVEY']
+	prihdr['in_file'] = os.path.basename(specList)[:-4]
+	prihdr['Nspec'] = hdu[0].header['Nspec']
+	prihdu = fits.PrimaryHDU(header=prihdr)
+	thdulist = fits.HDUList([prihdu, tbhdu])
+
+	out_file = join(os.environ['HOME'], "SDSS/stacks", os.path.basename(specList)[:-4]+".UVstack")
+
+	if os.path.isfile(out_file):
+		os.remove(out_file)
+	print( "stack written to", out_file )
+	thdulist.writeto(out_file)
+
+
+#specMatrix = n.loadtxt(out_file+'.specMatrix.dat')
+#specMatrixErr = n.loadtxt(out_file+'.specMatrixErr.dat')
+#specMatrixWeight = n.loadtxt(out_file+'.specMatrixWeight.dat')
+
+#out_file = join(os.environ['HOME'], "SDSS/stacks", os.path.basename(specList)[:-4]+".UVstack")
+##out_file = join(os.environ['HOME'],"SDSS", "stacks", os.path.basename(specList)[:-4]+".UVstack")
+##if os.path.isfile(out_file)==False:
+#print('starts working on', out_file, time.time()-t0)
+#stack=sse.SpectraStackingEBOSS(specList, out_file, dLambda = 0.0002, dV=-9999.99, l_start=3.34, l_end=3.556)
+#if os.path.isfile(out_file+'.specMatrix.dat')==False:
+	#print('creates matrix', time.time()-t0)
+	#stack.createStackMatrix_UVnormed()
+#print('stacks', time.time()-t0)
+#stack.stackSpectra()
