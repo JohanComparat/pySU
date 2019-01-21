bb7113f469ced8bdbb210c13f11f1606c361d687
JohanComparat <johan.comparat@gmail.com>
2019-01-21 22:09:03 +0100

    KZ stack

--- galaxy/bin_eBOSS_ELG/stack_spectra_ELG.py
+++ galaxy/bin_eBOSS_ELG/stack_spectra_ELG.py
@@ -12,14 +12,14 @@ import SpectraStackingEBOSS as sse
 
 spec_dir = join(os.environ['HOME'],"wwwDir/sdss/elg/stacks/KZ/")
 
-def stack_it(specList = join(spec_dir, "catalog_AGN.csv") ):
-	outfile = join(spec_dir, os.path.basename(specList)[:-4]+".stack")
+def stack_it(specList = join(spec_dir, "catalog_AGN.ascii") ):
+	outfile = join(spec_dir, os.path.basename(specList)[:-6]+".stack")
 	stack=sse.SpectraStackingEBOSS(specList, outfile)
 	print(outfile)
 	stack.stackSpectra()
 
-stack_it(join(spec_dir, "catalog_AGN.csv") )
-stack_it(join(spec_dir, "catalog_comp.csv") )
-stack_it(join(spec_dir, "catalog_liner.csv") )
-stack_it(join(spec_dir, "catalog_sf.csv") )
+stack_it(join(spec_dir, "catalog_AGN.ascii") )
+stack_it(join(spec_dir, "catalog_comp.ascii") )
+stack_it(join(spec_dir, "catalog_liner.ascii") )
+stack_it(join(spec_dir, "catalog_sf.ascii") )
 
