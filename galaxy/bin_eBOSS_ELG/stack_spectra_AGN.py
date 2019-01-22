ec8b58543084c6a0ef16b2ab8da49dfbc1a720e3
JohanComparat <johan.comparat@gmail.com>
2019-01-22 11:07:12 +0100

    AGN bug correstion

--- galaxy/bin_eBOSS_ELG/stack_spectra_AGN.py
+++ galaxy/bin_eBOSS_ELG/stack_spectra_AGN.py
@@ -14,7 +14,7 @@ import SpectraStackingEBOSS as sse
 spec_dir = join(os.environ['HOME'],"wwwDir/sdss/agn/stacks")
 
 def stack_it(specList = join(spec_dir, "catalog.ascii") ):
-	outfile = join(spec_dir, os.path.basename(specList)[:-6]+".stack")
+	outfile = join(spec_dir, os.path.basename(specList)[:-4]+".stack")
 	stack=sse.SpectraStackingEBOSS(specList, outfile)
 	stack.createStackMatrix()
 	print(outfile)
