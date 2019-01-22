60879b245603e52517ba33fdef71b93921eb6c8a
JohanComparat <johan.comparat@gmail.com>
2019-01-22 11:46:31 +0100

    stacks red

--- galaxy/bin_eBOSS_ELG/stack_spectra_AGN.py
+++ galaxy/bin_eBOSS_ELG/stack_spectra_AGN.py
@@ -14,8 +14,8 @@ import SpectraStackingEBOSS as sse
 spec_dir = join(os.environ['HOME'],"wwwDir/sdss/agn/stacks")
 
 def stack_it(specList = join(spec_dir, "catalog.ascii") ):
-	outfile = join(spec_dir, os.path.basename(specList)[:-4]+".stack")
-	stack=sse.SpectraStackingEBOSS(specList, outfile)
+	outfile = join(spec_dir, os.path.basename(specList)[:-4]+"red.stack")
+	stack=sse.SpectraStackingEBOSS(specList, outfile,l_end=3.9)
 	stack.createStackMatrix()
 	print(outfile)
 	stack.stackSpectra()
