be23b3f4f54d7979fcc74d6181e5446a336ea528
JohanComparat <johan.comparat@gmail.com>
2019-04-29 21:37:45 +0200

    highZ stack

---
+++ galaxy/bin_eBOSS_ELG/stack_spectra_KZ_highZ.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+"""
+This script produces the stacks for samples defined by a list of spectra identifiers.
+
+nohup python3 stack_spectra_ELG.py > stack_spectra_ELG.log &
+
+"""
+import sys
+import os 
+from os.path import join
+import glob
+import numpy as n
+import SpectraStackingEBOSS as sse
+
+spec_dir = join(os.environ['HOME'],"wwwDir/sdss/elg/stacks/KZ/high_z/")
+
+def stack_it(specList = join(spec_dir, "catalog_AGN.ascii") ):
+	outfile = join(spec_dir, os.path.basename(specList)[:-4]+".stack")
+	print(specList, outfile)
+	stack=sse.SpectraStackingEBOSS(specList, outfile, KZ_input=True, l_start=3.3, l_end=3.9 )
+	stack.createStackMatrix()
+	stack.stackSpectra()
+
+stack_it(join(spec_dir, 'catalog_AGN.ascii'   ) )
+stack_it(join(spec_dir, 'catalog_liner.ascii' ) )
+stack_it(join(spec_dir, 'catalog_comp.ascii'  ) )
+stack_it(join(spec_dir, 'catalog_sf.ascii'    ) )
+
+
+
+
+
