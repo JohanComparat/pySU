d858a7a4de8c218e034679fbec793da5d9ce2783
comparat <comparat@mpe.mpg.de>
2021-10-08 13:57:36 +0200

    QMOST stacks finalzing the T1 templates

--- galaxy/bin_eBOSS_ELG/plot_qmost_stack.py
+++ galaxy/bin_eBOSS_ELG/plot_qmost_stack.py
@@ -94,22 +94,22 @@ abs_line_list = [
 #stack_dir = join( os.environ['HOME'], "SDSS/stacks/v2" )
 stack_dir = join( os.environ['HOME'], "SDSS/stacks" )
 file_out =  join(stack_dir,"X_AGN", "DR16_ELG-stitched-stack.fits")
-file_out =  join(stack_dir,"X_AGN", "ROSAT_AGNT1-stitched-stack.fits")
-file_out =  join(stack_dir,"X_AGN", "ROSAT_AGNT2-stitched-stack.fits")
 
 def plot_spec( p_2_stack = file_out ):
 	print('plots', p_2_stack)
-	fig=p.figure(7, (14.0, 14.0), frameon=False)
-	fig.add_subplot(411, ylabel=r'F$_\lambda$')
+	# fig=p.figure(7, (14.0, 14.0), frameon=False)
+	# fig.add_subplot(411, ylabel=r'F$_\lambda$')
+	fig=p.figure(5, (14.0, 8.0))#, frameon=False)
+	fig.add_subplot(111, ylabel=r'F$_\lambda$',  xlabel='Wavelength rest-frame [Angstrom]')
 	stack = fits.open(p_2_stack)[1].data
 	s1 = (stack['wavelength']>0)
 	stack = stack[s1]
 	y_min = n.min(stack['medianStack'])
 	y_max = n.max(stack['medianStack'])
 	delta_y = y_max - y_min
-	p.xlim((n.min(stack['wavelength']), n.max(stack['wavelength'])))
+	p.xlim((n.min(stack['wavelength']), 9500 )) # n.max(stack['wavelength'])))
 	p.ylim((y_min - delta_y * 0.2 , y_max + delta_y * 0.2 ))
-	# p.yscale('log')
+	#p.xscale('log')
 	# lines above
 	for elem in em_line_list:
 		print(elem)
@@ -130,33 +130,33 @@ def plot_spec( p_2_stack = file_out ):
 	p.grid()
 	p.tight_layout()
 	#
-	print('standard deviation')
-	fig.add_subplot(412, ylabel=r'per cent')
-	stack = fits.open(p_2_stack)[1].data
-	s1 = (stack['wavelength']>0)
-	stack = stack[s1]
-	y_min = n.min( [ stack['jackknifStackErrors'], stack['NspectraPerPixel']**-0.5 ] )
-	y_max = n.max( [ stack['jackknifStackErrors'], stack['NspectraPerPixel']**-0.5 ] )
-	p.xlim((n.min(stack['wavelength']), n.max(stack['wavelength'])))
-	p.ylim(( y_min/1.1 , y_max*1.1 ))
-	p.plot(stack['wavelength'], stack['jackknifStackErrors']/stack['medianStack'], lw=0.7, label=r'$\sigma^{var}_{JK}$')
-	p.plot(stack['wavelength'], stack['NspectraPerPixel']**-0.5, lw=2, label=r'$1/\sqrt{N}$')
-	p.grid()
-	p.legend()
-	p.yscale('log')
-	p.tight_layout()
-	print('correlation coefficient')
-	fig.add_subplot(212, ylabel='Wavelength rest-frame [Angstrom]',  xlabel='Wavelength rest-frame [Angstrom]')
-	CR = n.corrcoef(stack['jackknifeSpectra'])
-	WLa = n.array([ stack['wavelength'] for el in stack['wavelength'] ])
-	WLb = WLa.T
-	highCorr_sel = ( abs(CR) > 0.8 ) & (CR>0)
-	xx = WLa[highCorr_sel]
-	yy = WLb[highCorr_sel]
-	cr_val = CR[highCorr_sel]
-	p.scatter(xx, yy, c=cr_val, s=1, rasterized = True)
-	p.colorbar(shrink=0.8)
-	p.tight_layout()
+	# print('standard deviation')
+	# fig.add_subplot(412, ylabel=r'per cent')
+	# stack = fits.open(p_2_stack)[1].data
+	# s1 = (stack['wavelength']>0)
+	# stack = stack[s1]
+	# y_min = n.min( [ stack['jackknifStackErrors'], stack['NspectraPerPixel']**-0.5 ] )
+	# y_max = n.max( [ stack['jackknifStackErrors'], stack['NspectraPerPixel']**-0.5 ] )
+	# p.xlim((n.min(stack['wavelength']), n.max(stack['wavelength'])))
+	# p.ylim(( y_min/1.1 , y_max*1.1 ))
+	# p.plot(stack['wavelength'], stack['jackknifStackErrors']/stack['medianStack'], lw=0.7, label=r'$\sigma^{var}_{JK}$')
+	# p.plot(stack['wavelength'], stack['NspectraPerPixel']**-0.5, lw=2, label=r'$1/\sqrt{N}$')
+	# p.grid()
+	# p.legend()
+	# p.yscale('log')
+	# p.tight_layout()
+	# print('correlation coefficient')
+	# fig.add_subplot(212, ylabel='Wavelength rest-frame [Angstrom]',  xlabel='Wavelength rest-frame [Angstrom]')
+	# CR = n.corrcoef(stack['jackknifeSpectra'])
+	# WLa = n.array([ stack['wavelength'] for el in stack['wavelength'] ])
+	# WLb = WLa.T
+	# highCorr_sel = ( abs(CR) > 0.8 ) & (CR>0)
+	# xx = WLa[highCorr_sel]
+	# yy = WLb[highCorr_sel]
+	# cr_val = CR[highCorr_sel]
+	# p.scatter(xx, yy, c=cr_val, s=1, rasterized = True)
+	# p.colorbar(shrink=0.8)
+	# p.tight_layout()
 	p.savefig(p_2_stack+".png")
 	p.clf()
 
@@ -164,128 +164,6 @@ plot_spec(  join(stack_dir,"X_AGN", "ROSAT_AGNT1-stitched-stack.fits") )
 
 plot_spec(  join(stack_dir,"X_AGN", "ROSAT_AGNT2-stitched-stack.fits") )
 plot_spec(  join(stack_dir,"X_AGN", "DR16_ELG-stitched-stack.fits") )
+plot_spec(  join(output1_directory,"X_AGN", "ROSAT_AGNT1-DR16QSO-stitched-stack.fits") )
+plot_spec(  join(output1_directory,"X_AGN", "DR16LRG-stitched-stack.fits") )
 
-
-sys.exit()
-
-p.figure(1,(9,5))
-p.title(qty)
-#p.plot(xA,yA/n.median(yA),'k',lw=0.5, label='Narrow Line AGN')
-ok = (ELG_a['WAVE']>2300)&(ELG_a['WAVE']<3800)
-
-p.plot(ELG_a['WAVE'][ok], ELG_a['FLUXMEDIAN'][ok]/ELG_a['FLUXMEDIAN_ERR'][ok], lw=0.3, label='Zhu15 SNR pilot study')
-for specList in dataList_UV:
-	bn = os.path.basename(specList)[10:-8].split('_')
-	bnl = str(n.round(float(bn[0]),3))+'<z<'+str(n.round(float(bn[2]),3))+', '+str(n.round(float(bn[3]),3))+'<'+qty+'<'+str(n.round(float(bn[5]),3))
-	dd=fits.open(specList)[1].data
-	wl=dd['wavelength']
-	s1 = (dd['NspectraPerPixel'   ]>0.5*n.max(dd['NspectraPerPixel'   ]))
-	p.plot(dd['wavelength'][s1], 200*dd['medianStack'        ][s1]/n.median(dd['medianStack'        ][s1]), label= bnl )
-	#dd['meanStack'          ][s1]
-	#dd['meanWeightedStack'  ][s1]
-	#dd['jackknifeSpectra'   ][s1]
-	#dd['jackknifStackErrors'][s1]
-	#dd['NspectraPerPixel'   ][s1]
-
-p.plot(dd['wavelength'][s1], dd['medianStack'][s1]/dd['jackknifStackErrors'][s1], lw=0.3, label='SNR full eBOSS')
-p.grid()
-p.xlim((2300,3800))
-p.yscale('log')
-p.ylabel('S/N per pixel')
-p.xlabel('wavelength [A]')
-p.legend(frameon=False)
-p.tight_layout()
-p.savefig(p_2_stack + ".png")
-p.clf()
-
-
-p.figure(1,(9,5))
-p.title(qty)
-#p.plot(xA,yA/n.median(yA),'k',lw=0.5, label='Narrow Line AGN')
-for specList in dataList_UV:
-	bn = os.path.basename(specList)[10:-8].split('_')
-	bnl = str(n.round(float(bn[0]),3))+'<z<'+str(n.round(float(bn[2]),3))+', '+str(n.round(float(bn[3]),3))+'<'+qty+'<'+str(n.round(float(bn[5]),3))
-	dd=fits.open(specList)[1].data
-	wl=dd['wavelength']
-	s1 = (dd['NspectraPerPixel'   ]>0.5*n.max(dd['NspectraPerPixel'   ]))
-	p.plot(dd['wavelength'][s1], dd['medianStack_UVnormed'        ][s1], label= bnl )
-	#dd['meanStack'          ][s1]
-	#dd['meanWeightedStack'  ][s1]
-	#dd['jackknifeSpectra'   ][s1]
-	#dd['jackknifStackErrors'][s1]
-	#dd['NspectraPerPixel'   ][s1]
-
-#ok = (ELG_a['WAVE']>2300)&(ELG_a['WAVE']<3800)
-#p.plot(ELG_a['WAVE'][ok], ELG_a['FLUXMEDIAN'][ok]/n.median(ELG_a['FLUXMEDIAN'][ok]), label='Zhu15')
-p.grid()
-p.xlim((2240,2400))
-p.ylim((0,2))
-p.legend(frameon=False)
-p.tight_layout()
-p.savefig(join(stack_dir, "eboss-elg-2240-2400_"+qty+".stack")+".png")
-p.clf()
-
-
-p.figure(1,(9,5))
-p.title(qty)
-#p.plot(xA,yA/n.median(yA),'k',lw=0.5, label='Narrow Line AGN')
-for specList in dataList_UV:
-	bn = os.path.basename(specList)[10:-8].split('_')
-	bnl = str(n.round(float(bn[0]),3))+'<z<'+str(n.round(float(bn[2]),3))+', '+str(n.round(float(bn[3]),3))+'<'+qty+'<'+str(n.round(float(bn[5]),3))
-	dd=fits.open(specList)[1].data
-	wl=dd['wavelength']
-	s1 = (dd['NspectraPerPixel'   ]>0.5*n.max(dd['NspectraPerPixel'   ]))
-	p.plot(dd['wavelength'][s1], dd['medianStack_UVnormed'        ][s1], label= bnl )
-	#dd['meanStack'          ][s1]
-	#dd['meanWeightedStack'  ][s1]
-	#dd['jackknifeSpectra'   ][s1]
-	#dd['jackknifStackErrors'][s1]
-	#dd['NspectraPerPixel'   ][s1]
-
-#ok = (ELG_a['WAVE']>2300)&(ELG_a['WAVE']<3800)
-#p.plot(ELG_a['WAVE'][ok], ELG_a['FLUXMEDIAN'][ok]/n.median(ELG_a['FLUXMEDIAN'][ok]), label='Zhu15')
-p.grid()
-p.xlim((2570,2640))
-p.ylim((0,2))
-p.legend(frameon=False)
-p.tight_layout()
-p.savefig(join(stack_dir, "eboss-elg-2570-2640_"+qty+".stack")+".png")
-p.clf()
-
-
-p.figure(1,(9,5))
-p.title(qty)
-#p.plot(xA,yA/n.median(yA),'k',lw=0.5, label='Narrow Line AGN')
-for specList in dataList_UV:
-	bn = os.path.basename(specList)[10:-8].split('_')
-	bnl = str(n.round(float(bn[0]),3))+'<z<'+str(n.round(float(bn[2]),3))+', '+str(n.round(float(bn[3]),3))+'<'+qty+'<'+str(n.round(float(bn[5]),3))
-	dd=fits.open(specList)[1].data
-	wl=dd['wavelength']
-	s1 = (dd['NspectraPerPixel'   ]>0.5*n.max(dd['NspectraPerPixel'   ]))
-	p.plot(dd['wavelength'][s1], dd['medianStack_UVnormed'        ][s1], label= bnl )
-	#dd['meanStack'          ][s1]
-	#dd['meanWeightedStack'  ][s1]
-	#dd['jackknifeSpectra'   ][s1]
-	#dd['jackknifStackErrors'][s1]
-	#dd['NspectraPerPixel'   ][s1]
-
-#ok = (ELG_a['WAVE']>2300)&(ELG_a['WAVE']<3800)
-#p.plot(ELG_a['WAVE'][ok], ELG_a['FLUXMEDIAN'][ok]/n.median(ELG_a['FLUXMEDIAN'][ok]), label='Zhu15')
-p.grid()
-p.xlim((2780,2870))
-p.ylim((0,2))
-p.legend(frameon=False)
-p.tight_layout()
-p.savefig(join(stack_dir, "eboss-elg-2780-2870_"+qty+".stack")+".png")
-p.clf()
-
-plot_me(qty = 'O2EW' )
-#plot_me(qty = 'O2lum' )
-
-#plot_me(qty = 'mass' )
-#plot_me(qty = 'g'          )
-#plot_me(qty = 'gr'         )
-#plot_me(qty = 'rz'         )
-#plot_me(qty = 'rw1'         )
-os.system("rm ~/wwwDir/sdss/elg/stacks/*.png")
-os.system("cp -r "+stack_dir+"/*.png ~/wwwDir/sdss/elg/stacks/")
